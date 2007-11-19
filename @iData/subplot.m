084423a1308d688ef80341a842ee92a1e87822ab
farhi <farhi@ill.fr>
2007-11-19 16:59:24 +0000

    Subplot function

---
+++ @iData/subplot.m
@@ -0,0 +1,33 @@
+function h=subplot(s, varargin)
+% subplot(s) : plot iData array as subplots
+%
+%   @iData/subplot plot each iData element in a subplot
+%
+% input:  s: object or array (iData)
+% output: h: plot handles (double)
+% ex:     subplot([ a a ])
+%
+% See also iData, iData/plot
+
+% EF 23/11/07 iData implementation
+
+if length(a(:)) == 1
+  h=plot(a, varargin);
+  return
+end
+
+a = squeeze(a); % remove singleton dimensions
+
+if length(size(a)) == 2 & all(size(a) > 1)
+  n = size(a,1); m = size(a,2);
+else
+  p = length(a(:));
+  n = floor(sqrt(p));
+  m = ceil(p/n);
+end
+
+h=[];
+for index=1:length(a(:))
+  subplot(m,n,index);
+  h = [ h plot(a(index)) ];
+end
