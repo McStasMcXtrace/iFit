2ad8bfbf7f88edfc88d31744392c8875163d0d9b
farhi <farhi@ill.fr>
2007-12-03 16:15:09 +0000

    colon (a:b) uses linspace

--- @iData/colon.m
+++ @iData/colon.m
@@ -1,4 +1,4 @@
-function b = mean(a)
+function c = colon(a,d,b)
 % b = mean(s) : mean value of iData object
 %
 %   @iData/mean function to compute the mean value of objects
@@ -9,11 +9,18 @@ function b = mean(a)
 %
 % See also iData, iData/floor, iData/ceil, iData/round, iData/combine
 
-if length(a) > 1
-  a = combine(a);
-  return
+if nargin == 2
+  b = d;
+  d = 0;
 end
 
-c = double(a);
-b = mean(c(:));
+as = round(mean(a));
+bs = round(mean(b));
+if d > 0, n = abs(bs-as)/d;
+else      n = abs(bs-as); end
+
+if n == 1, c = a;  return; end
+if n <= 0, c = a; return; end
+
+c = linspace(a,b,n);
 
