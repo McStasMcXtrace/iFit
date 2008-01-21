9abf40d5c1982114a2a87fa40e10976c5b6de3df
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2008-01-21 09:04:09 +0000

    Update of optimization routines. Includes iteration process info. (options.Display='iter')

---
+++ trunk/iOptim/private/localChar.m
@@ -0,0 +1,16 @@
+function strfcn = localChar(fcn)
+% Convert the fcn to a string for printing
+
+if ischar(fcn)
+    strfcn = fcn;
+elseif isa(fcn,'inline')
+    strfcn = char(fcn);
+elseif isa(fcn,'function_handle')
+    strfcn = func2str(fcn);
+else
+    try
+        strfcn = char(fcn);
+    catch
+        strfcn = '(name not printable)';
+    end
+end
