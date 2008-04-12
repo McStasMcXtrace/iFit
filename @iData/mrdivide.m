92d666fd389318ad968b2c6f737d2263bcd1a592
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2008-04-12 21:55:26 +0000

    Update * and / operations to apply on matrices as well

---
+++ @iData/mrdivide.m
@@ -0,0 +1,18 @@
+function c = mrdivide(a,b)
+% c = mrdivide(a,b) : computes the matrix division of iData objects
+%
+%   @iData/mrdivide function to compute the matrix division of data sets=a/b
+%
+% input:  a: object or array (iData or numeric)
+%         b: object or array (iData or numeric)
+% output: c: object or array (iData)
+% ex:     c=a/2;
+%
+% See also iData, iData/minus, iData/plus, iData/times, iData/rdivide, iData/power
+
+if isscalar(a) | isscalar(b)
+  c = iData_private_binary(a, b, 'rdivide');
+else
+  iData_private_error(mfilename,[ 'Matrix iData matrix division not supported yet' ]);
+end
+
