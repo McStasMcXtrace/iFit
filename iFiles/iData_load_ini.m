26c9962a9db9c22933f39766b87dd122e5c1658a
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2008-04-20 14:09:01 +0000

    Use fast binary import for specified importation methods (e.g. 2D are slow)

--- iFiles/iData_load_ini.m
+++ iFiles/iData_load_ini.m
@@ -46,32 +46,36 @@ function loaders = iData_load_ini
     
     format5.name       ='ILL TAS Data (polarized)';
     format5.patterns   ={'POSQE:','PARAM:','DATA_:','LOCAL:','USER_:','PAL'};
-    format5.options    ='--headers --section=PARAM --section=VARIA --section=ZEROS --section=DATA -section=POLAN --metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE --metadata=INSTR --metadata=COMND';
+    format5.options    =['--fast --binary --headers ' ...
+                        '--section=PARAM --section=VARIA --section=ZEROS --section=DATA -section=POLAN ' ...
+                        '--metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE --metadata=INSTR --metadata=COMND'];
     format5.method     ='looktxt';
     format5.postprocess='load_ill_tas'; % load_ill_tas
     
     format6.name       ='ILL TAS Data';
     format6.patterns   ={'POSQE:','PARAM:','DATA_:','LOCAL:','USER_:'};
-    format6.options    ='--headers --section=PARAM --section=VARIA --section=ZEROS --section=DATA --metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE --metadata=INSTR --metadata=COMND';
+    format6.options    =['--fast --binary --headers ' ...
+                        '--section=PARAM --section=VARIA --section=ZEROS --section=DATA ' ...
+                        '--metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE --metadata=INSTR --metadata=COMND'];
     format6.method     ='looktxt';
     format6.postprocess='load_ill_tas'; % load_ill_tas
     
     format7.name       ='SPEC';
     format7.patterns   ={'#F','#D','#S'};
-    format7.options    ='--headers --metadata="#S " --comment= ';
+    format7.options    ='--fast --binary --headers --metadata="#S " --comment= ';
     format7.method     ='looktxt';
     format7.postprocess='';
     
     format8.name       ='McStas Scan output';
     format8.patterns   ={'# Numpoints:','# variables:','# title: Scan of'};
-    format8.options    =['--headers --comment= --metadata=variables ' ...
+    format8.options    =['--fast --binary --headers --comment= --metadata=variables ' ...
                          '--metadata=xlabel --metadata=ylabel'];
     format8.method     ='looktxt';
     format8.postprocess='load_mcstas_scan';
     
     format9.name       ='McStas 2D monitor';
     format9.patterns   ={'Format: McStas with text headers file.','# type: array_2d'};
-    format9.options    = ['--headers --comment= --metadata=variables ' ...
+    format9.options    = ['--fast --binary --headers --comment= --metadata=variables ' ...
 		    '--metadata=Errors --metadata=Events --metadata=xlabel ' ...
 		    '--metadata=ylabel --metadata=zlabel --metadata=xylimits'];
     format9.method     ='looktxt';
@@ -79,19 +83,19 @@ function loaders = iData_load_ini
     
     format10.name       ='McStas 1D monitor';
     format10.patterns   ={'Format: McStas with text headers file.','# type: array_1d'};
-    format10.options    ='--headers --comment= --metadata=variables --metadata=xlabel --metadata=ylabel';
+    format10.options    ='--fast --binary --headers --comment= --metadata=variables --metadata=xlabel --metadata=ylabel';
     format10.method     ='looktxt';
     format10.postprocess='load_mcstas_1d';
     
     format11.name       ='McStas sim file';
     format11.patterns   ={'begin simulation','  Format: McStas'};
-    format11.options    ='--headers --comment=';
+    format11.options    ='--fast --binary --headers --comment=';
     format11.method     ='looktxt';
     format11.postprocess='load_mcstas_sim';
     
     format12.name       ='ILL TAS Data (light)';
     format12.patterns   ={'POSQE:','PARAM:','DATA_:','LOCAL:','USER_:'};
-    format12.options    ='--headers --section=PARAM --section=DATA --metadata=DATA';
+    format12.options    ='--fast --binary--headers --section=PARAM --section=DATA --metadata=DATA';
     format12.method     ='looktxt';
     format12.postprocess='';
     
