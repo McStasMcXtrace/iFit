b89f63d917641a23a4c4d6503920d332582f2369
farhi <farhi@ill.fr>
2008-04-23 15:54:55 +0000

    sum, mean and median return iData objects on axes or projection. Update of plot

--- @iData/sum.m
+++ @iData/sum.m
@@ -1,16 +1,19 @@
 function s = sum(a,dim)
-% s = sum(a,dim) : computes the sum of iData objects elements
+% s = sum(a,dim) : computes the sum/projection of iData objects elements
 %
 %   @iData/sum function to compute the sum of the elements of the data set
 %     sum(a,dim) accumulates along axis of rank dim. If dim=0, sum is done
-%       on all axes.
+%       on all axes and the total is returned as a scalar value. 
+%       sum(a,1) accumulates on first dimension (columns)
+%     sum(a,-dim) accumulates on all axes except the dimension specified, i.e.
+%       the result is the projection of a along dimension dim.
 %
-% input:  a: object or array (iData or numeric)
+% input:  a: object or array (iData/array of)
 %         dim: dimension to accumulate (int)
-% output: s: sum of elements (double)
+% output: s: sum of elements (iData/scalar)
 % ex:     c=sum(a);
 %
-% See also iData, iData/plus, iData/prod, iData/cumsum
+% See also iData, iData/plus, iData/prod, iData/cumsum, iData/mean
 
 if ~isa(a, 'iData')
   iData_private_error(mfilename,['syntax is sum(iData, dim)']);
@@ -28,11 +31,33 @@ if length(a(:)) > 1
 end
 
 s=get(a,'Signal');
+[link, label]          = getalias(a, 'Signal');
+b=a;
+setaxis(b, [], getaxis(b)); % delete all axes
 if dim > 0
   s = sum(s, dim);
-else
+  ax_index=1;
+  for index=1:ndims(a)
+    if index ~= dim
+      setaxis(b, ax_index, getaxis(a, num2str(index)));
+      ax_index = ax_index+1;
+    end
+  end
+  setalias(b,'Signal', s, [mfilename ' of ' label ]);     % Store Signal
+elseif dim == 0
   for index=1:ndims(a)
     s = sum(s, index);
   end
+  return  % scalar
+else  % dim < 0
+  % accumulates on all axes except the rank specified
+  for index=1:ndims(a)
+    if index~=-dim, s = sum(s,index); end
+  end
+  setaxis(b, 1, getaxis(a, num2str(-dim)));
+  setalias(b,'Signal', s, [ 'projection of ' label ]);     % Store Signal
 end
+
+b = iData_private_history(b, mfilename, b, dim);
+s = b;
 
