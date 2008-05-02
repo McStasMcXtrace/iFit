dd9c01174d31ab2958c2a86e6eca3c54e16b7d9c
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2008-05-02 11:49:36 +0000

    Added catenate method

--- trunk/@iData/union.m
+++ trunk/@iData/union.m
@@ -36,35 +36,8 @@ end
 % return if using identical axes: no need to interpolate. retain axes and data.
 if all_identical_axes, ai=a; bi=[]; return; end
 
-% initiate new axes
-for index=1:ndims(a(1))
-  c_step{index} =  Inf;
-  c_min{index}  =  Inf;
-  c_max{index}  = -Inf;
-  c_len{index}  =  0;
-end
-
-% loop on all iData to find intersection area
-for index=1:length(a)
-  if ndims(a(index)) ~= ndims(a(1))
-    iData_private_error(mfilename, [ 'Object intersection requires same dimensionality.\n\tobject ' inputname(1) ' ' a(1).Tag ' is ' num2str(ndims(a(1))) ' but object ' a(index).Tag ' is ' num2str(ndims(a(index))) ]);
-  end
-  for j_ax = 1:ndims(a(index))  % for each dimension
-    x = getaxis(a(index), j_ax); x=unique(x(:));    % extract axis, and remove duplicates. diff > 0
-    c_step{j_ax} = min(min(diff(x)), c_step{j_ax}); % smallest step
-    c_min{j_ax}  = min(min(x), c_min{j_ax});        % lowest min
-    c_max{j_ax}  = max(max(x), c_max{j_ax});        % highest max
-    c_len{j_ax}  = c_len{j_ax} + length(x);         % cumulated axes length
-  end
-end
-
-% build new axes
-for j_ax = 1:ndims(a(index))  % for each dimension
-  c_len{j_ax} = c_len{j_ax}/length(a);                  % mean axis length from original data
-  len         = (c_max{j_ax}-c_min{j_ax})/c_step{j_ax}; % theoretical axis length
-  c_len{j_ax} = min(len+1, 10*c_len{j_ax});             % can not extend axes more than 10 times
-  c_axis{j_ax} = linspace(c_min{j_ax}, c_max{j_ax}, c_len{j_ax});
-end
+% compute common axes
+c_axis = iData_private_caxis(a);
 
 % loop on all iData to interpolate
 ai = a; bi=[];
