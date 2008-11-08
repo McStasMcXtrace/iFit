68bee9be344ee3f13fc0a178edeb3471d57ed1c0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2008-11-08 09:36:37 +0000

    added Toggle error bars for 1D data

--- @iData/plot.m
+++ @iData/plot.m
@@ -21,7 +21,7 @@ function h=plot(a, method)
 %   fscatter3: Felix Morsdorf, Jan 2003, Remote Sensing Laboratory Zuerich
 %   vol3d:     Joe Conti, 2004
 %
-% Version: $Revision: 1.24 $
+% Version: $Revision: 1.25 $
 % See also iData, interp1, interpn, ndgrid, plot, iData/setaxis, iData/getaxis
 %          iData/xlabel, iData/ylabel, iData/zlabel, iData/clabel, iData/title
 
@@ -31,11 +31,13 @@ function h=plot(a, method)
 if nargin == 1, method=''; end
 if length(a) > 1
   h = cell(size(a));
+  ih = ishold;
   for index=1:length(a(:))
     h{index} = plot(a(index), method);
     hold on
   end
   h = reshape(h, size(a));
+  if ih == 1, hold on; else hold off; end
   return
 end
 
@@ -197,6 +199,9 @@ uimenu(uicm, 'Label', [ 'Source: <' S '>' ]);
 uimenu(uicm, 'Label', [ 'Cmd: ' cmd ]);
 uimenu(uicm, 'Label', [ 'User: ' a.User ]);
 uimenu(uicm, 'Separator','on','Label','Toggle grid', 'Callback','grid');
+if ndims(a) == 1
+  uimenu(uicm, 'Label','Toggle error bars', 'Callback','tmp_h=get(gco,''children''); if strcmp(get(tmp_h(2),''visible''),''off''), tmp_v=''on''; else tmp_v=''off''; end; set(tmp_h(2),''visible'',tmp_v); clear tmp_h tmp_v');
+end
 if ndims(a) >= 2
   uimenu(uicm, 'Label','Reset Flat/3D View', 'Callback','[tmp_a,tmp_e]=view; if (tmp_a==0 & tmp_e==90) view(3); else view(2); end; clear tmp_a, tmp_e; lighting none;alpha(1);shading flat;');
   uimenu(uicm, 'Label','Smooth View','Callback', 'shading interp;');
