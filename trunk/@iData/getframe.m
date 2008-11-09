8aadbcb4ab7d48b9b093dc31620efb347f4456cb
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2008-11-09 16:56:07 +0000

    fixed capture that may fail without JVM and hidden figure

--- trunk/@iData/getframe.m
+++ trunk/@iData/getframe.m
@@ -3,18 +3,19 @@ function frame = getframe(a, dim)
 %
 %   @iData/getframe function to create iData frames/thumbnails.
 %   Such thumbnails may be used to create icons on GUIs
-%   Note: a small figure will flicker on screen.
+%   Note: a figure will flicker on screen.
 %
 % input:  s:   object or array (iData)
-%         dim: dimension of the thumbnail. Default is 64 pixels.
+%         dim: dimension of the thumbnail. When specified, the frame has no labels, ticks, ...
+%              to serve as a thumbnail
 %
 % output: frame: frame/thumbnail
 % ex:     f=getframe(a); image(f.cdata);
 %
-% Version: $Revision: 1.1 $
+% Version: $Revision: 1.2 $
 % See also iData, iData/plot, getframe, image, imwrite
 
-if nargin < 2, dim=64; end
+if nargin < 2, dim=0; end
 if length(a) > 1
   frame = cell(size(a));
   for index=1:length(a(:))
@@ -23,34 +24,38 @@ if length(a) > 1
   return
 end
 
-f=figure('visible','off');
-% resample a with a coarse grid if needed
-need_resample=0;
-a_axes = cell(1,ndims(a));
-for index=1:ndims(a)
-  s = size(a,index);  % loads object axes, or 1:end if not defined 
-  if s > dim
-    a_axes{index} = round(linspace(1,s,dim));
-    need_resample = 1;
-  else
-    a_axes{index} = 1:s;
+f=figure;
+if dim
+  % resample a with a coarse grid if needed
+  need_resample=0;
+  a_axes = cell(1,ndims(a));
+  for index=1:ndims(a)
+    s = size(a,index);  % loads object axes, or 1:end if not defined 
+    if s > dim
+      a_axes{index} = round(linspace(1,s,dim));
+      need_resample = 1;
+    else
+      a_axes{index} = 1:s;
+    end
   end
-end
-% fast resampling only using indexes
-if need_resample
-  S.type='()';
-  S.subs=a_axes;
-  a = subsref(a, S);
+  % fast resampling only using indexes
+  if need_resample
+    S.type='()';
+    S.subs=a_axes;
+    a = subsref(a, S);
+  end
 end
 
 % plot the data
 plot(a); view(2);
-legend off;
-xlabel(''); ylabel('')
-set(gcf,'Position',[50,50,dim,dim]);
-set(gca,'xtick',[]);
-set(gca,'ytick',[]) ;
-axis tight;
+if dim
+  legend off;
+  xlabel(''); ylabel('')
+  set(gcf,'Position',[50,50,dim,dim]);
+  set(gca,'xtick',[]);
+  set(gca,'ytick',[]) ;
+  axis tight;
+end
 % extract frame
 frame=getframe(f); 
 delete(f);
