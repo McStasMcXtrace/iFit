b37fe4d8dc680a2d6df5bc657a829baba18f7f3b
farhi <farhi@ill.fr>
2008-11-20 13:06:30 +0000

    support ILL TAS with MULTI detector section, and polarized states

--- iFiles/load_ill_tas.m
+++ iFiles/load_ill_tas.m
@@ -1,12 +1,19 @@
 function a=load_ill_tas(a)
 % function a=load_ill_tas(a)
 %
-% Simple postprocessing for ILL/TAS files
+% Simple postprocessing for ILL/TAS files.
+% Supports ILL TAS files, including those with multidetectors.
 %
 
 a=iData(a);
-% get the main data block name
-DataBlock        = a.Signal;
+
+% re-assign DATA in case the file has more than one data block (MULTI, ...)
+try
+  DATA  = a.MetaData.DATA; 
+  setalias(a,'Signal',DATA,[ 'Data CNTS' ]);
+catch
+  DATA  = a.Signal;
+end
 
 try
   STEPS = a.Data.STEPS;
@@ -15,12 +22,20 @@ catch
 end
 
 % get the main data block header
-columns_header   = findstr(a, 'DATA_:','case');
+[columns_header, data]   = findstr(a, 'DATA_:','case');
 if isempty(columns_header), 
   iData_private_warning(mfilename, [ 'The loaded data set ' a.Tag ' is not an ILL TAS data format' ]);
   return; 
 end
 columns_header   = columns_header{1};
+
+% detect if this is a MULTI detector file
+try
+  MULTI  = a.MetaData.MULTI; 
+  setalias(a, 'MULTI', 'this.Data.MetaData.MULTI', 'Multidetector');
+catch
+  MULTI=[];
+end
 
 % Find spaces and determine proper aliases for the columns
 columns = strread(columns_header,'%s','delimiter',' ');
@@ -46,9 +61,10 @@ end
 index_hkle=[]; % index of QH QK QL EN columns
 index_m12 =[]; % index of M1 M2 monitors
 index_temp=[]; % index for temperatures
+index_pal =[]; % index for polarization analysis
 Variance = zeros(1,length(columns));
 for j=1:length(columns)
-  setalias(a,columns{j},a.Signal(:,j));
+  setalias(a,columns{j},a.Signal(:,j)); % create an alias for each column
   if (isempty(STEPS) & ~isempty(strmatch(columns{j}, {'QH','QK','QL','EN'},'exact'))) | ...
     (~isempty(STEPS) & ~isempty(strmatch(columns{j}, STEPS, 'exact')))
     index_hkle = [ index_hkle j ];
@@ -58,6 +74,9 @@ for j=1:length(columns)
   end
   if strmatch(columns{j}, {'TT','TRT'},'exact')
     index_temp= [ index_temp j ];
+  end
+  if strmatch(columns{j}, {'PAL'},'exact')
+    index_pal= [ index_pal j ];
   end
   if isempty(strmatch(columns{j},{'PNT','CNTS','TI'}, 'exact'))
     if length(a.Signal(:,j))
@@ -131,15 +150,11 @@ try
 catch
   DATE='';
 end
-try
-  COMND = a.Headers.MetaData.COMND; COMND=deblank(COMND(7:end));
-catch
-  COMND='';
-end
+
 setalias(a, 'COMND', 'this.Data.Headers.MetaData.COMND', 'TAS command');
 setalias(a, 'INSTR', 'this.Data.Headers.MetaData.INSTR', 'Instrument used');
 setalias(a, 'EXPNO', 'this.Data.Headers.MetaData.EXPNO', 'Experiment number');
-setalias(a, 'TITL', 'this.Data.Headers.MetaData.TITLE', 'Dataset title');
+setalias(a, 'TITL',  'this.Data.Headers.MetaData.TITLE', 'Dataset title');
 % update object
 if ~isempty(DATE), a.Date = DATE; end
 a.User = [ EXPNO ' ' USER '/' LOCAL '@' INSTR ];
@@ -147,11 +162,20 @@ if isempty(TITLE), a.Title= [ COMND ';' a.Title ];
 else a.Title= [ TITLE ';' a.Title ]; end
 for i=1:2, a.Title=strrep(a.Title, '  ',' '); end
 
-% make up Signal label
 
 % set Signal and default axis
-setalias(a,'Signal','CNTS',[ 'Data CNTS' ]);
-setaxis(a,1,columns{index});
+if isempty(MULTI)
+  setalias(a,'Signal','CNTS',[ 'Data CNTS' ]);  % has been defined in DATA_ columns
+  setaxis(a,1,columns{index});
+else
+  setalias(a,'Signal', 'MULTI',[ 'Data CNTS' ]);  % has been defined from MULTI_
+  setalias(a,'Channel',1:size(MULTI,2), 'Detector channel');
+  setaxis(a,1,columns{index});
+  setaxis(a,2,'Channel');
+  setalias(a,'Monitor', get(a, columns{index_m12}) * 1:size(MULTI,2));
+end
+
+% make up Signal label
 xl = xlabel(a);
 if ~isempty(TT) & isnumeric(TT), xl = sprintf('%s T=%.2f K',xl,TT); end
 if ~isempty(FX) & isnumeric(FX), 
@@ -163,3 +187,19 @@ if ~isempty(FX) & isnumeric(FX),
 end
 xlabel(a, xl);
 
+% handle polarization analysis files
+if ~isempty(index_pal)
+  % get number of PAL states
+  pal = unique(DATA(:,index_pal(1)));
+  b = [];
+  for j=1:length(pal)
+    % create one iData per PAL state
+    index_rows = find( DATA(:,index_pal(1)) == pal(j) );
+    this_b = a(index_rows, :);
+    title(this_b, [ title(this_b) ' [PAL=' num2str(j) ']' ]);
+    this_b.Title = [ 'PAL=' num2str(j) ';' this_b.Title  ];
+    b = [ b this_b ];
+  end
+  a = b;
+end
+  
