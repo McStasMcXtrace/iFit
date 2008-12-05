78446b16c05c30b90a3f2ffe8c272d7c9317557b
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2008-12-05 15:48:02 +0000

    Use silent mode for looktxt import

--- trunk/iFiles/iLoad_ini.m
+++ trunk/iFiles/iLoad_ini.m
@@ -23,27 +23,27 @@ function loaders = iLoad_ini
 
     format1.name       ='ILL Data (normal integers)';
     format1.patterns   ={'RRRR','AAAA','FFFF','IIII'};
-    format1.options    ='--headers --fortran --catenate --fast --binary --makerows=IIII --makerows=FFFF --silent';
+    format1.options    ='--headers --fortran --catenate --fast --binary --makerows=IIII --makerows=FFFF';
     format1.method     ='looktxt';
     
     format2.name       ='ILL Data (large integers)';
     format2.patterns   ={'RRRR','AAAA','FFFF','JJJJ'};
-    format2.options    ='--headers --fortran --catenate --fast --binary --makerows=JJJJ --makerows=FFFF --silent';
+    format2.options    ='--headers --fortran --catenate --fast --binary --makerows=JJJJ --makerows=FFFF';
     format2.method     ='looktxt';
     
     format3.name       ='ILL Data (floats only)';
     format3.patterns   ={'RRRR','AAAA','FFFF'};
-    format3.options    ='--headers --fortran --catenate --fast --binary --makerows=FFFF --silent';
+    format3.options    ='--headers --fortran --catenate --fast --binary --makerows=FFFF';
     format3.method     ='looktxt';
     
     format4.name       ='ILL Data (general)';
     format4.patterns   ={'SSSS'};
-    format4.options    ='--headers --fortran --catenate --fast --binary --makerows=FFFF --makerows=JJJJ --makerows=IIII --silent';
+    format4.options    ='--headers --fortran --catenate --fast --binary --makerows=FFFF --makerows=JJJJ --makerows=IIII';
     format4.method     ='looktxt';
     
     format5.name       ='ILL TAS Data (polarized)';
     format5.patterns   ={'POSQE:','PARAM:','DATA_:','LOCAL:','USER_:','PAL'};
-    format5.options    =['--fast --binary --headers --silent ' ...
+    format5.options    =['--fast --binary --headers ' ...
                         '--section=PARAM --section=VARIA --section=ZEROS --section=DATA ' ...
                         '--section=POLAN --section=STEPS ' ...
                         '--metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE ' ...
@@ -54,7 +54,7 @@ function loaders = iLoad_ini
     
     format6.name       ='ILL TAS Data';
     format6.patterns   ={'POSQE:','PARAM:','DATA_:','LOCAL:','USER_:'};
-    format6.options    =['--fast --binary --headers  --silent ' ...
+    format6.options    =['--fast --binary --headers  ' ...
                         '--section=PARAM --section=VARIA --section=ZEROS --section=DATA ' ...
                         '--section=STEPS ' ...
                         '--metadata=LOCAL --metadata=USER --metadata=EXPNO --metadata=DATE --metadata=DATA ' ...
@@ -71,14 +71,14 @@ function loaders = iLoad_ini
     
     format8.name       ='McStas Scan output';
     format8.patterns   ={'# Numpoints:','# variables:','# title: Scan of'};
-    format8.options    =['--fast --binary --headers --comment= --metadata=variables  --silent ' ...
+    format8.options    =['--fast --binary --headers --comment= --metadata=variables  ' ...
                          '--metadata=xlabel --metadata=ylabel'];
     format8.method     ='looktxt';
     format8.postprocess='load_mcstas_scan';
     
     format9.name       ='McStas 2D monitor';
     format9.patterns   ={'Format: McStas with text headers file.','# type: array_2d'};
-    format9.options    = ['--fast --binary --headers --comment= --metadata=variables  --silent ' ...
+    format9.options    = ['--fast --binary --headers --comment= --metadata=variables  ' ...
 		    '--metadata=Errors --metadata=Events --metadata=xlabel ' ...
 		    '--metadata=ylabel --metadata=zlabel --metadata=xylimits'];
     format9.method     ='looktxt';
@@ -86,26 +86,26 @@ function loaders = iLoad_ini
     
     format10.name       ='McStas 1D monitor';
     format10.patterns   ={'Format: McStas with text headers file.','# type: array_1d'};
-    format10.options    ='--fast --binary --headers --comment= --metadata=variables --metadata=xlabel --metadata=ylabel --silent ';
+    format10.options    ='--fast --binary --headers --comment= --metadata=variables --metadata=xlabel --metadata=ylabel ';
     format10.method     ='looktxt';
     format10.postprocess='load_mcstas_1d';
     
     format11.name       ='McStas sim file';
     format11.extension  ='sim';
     format11.patterns   ={'begin simulation','  Format: McStas'};
-    format11.options    ='--fast --binary --headers  --silent --comment=';
+    format11.options    ='--fast --binary --headers  --comment=';
     format11.method     ='looktxt';
     format11.postprocess='load_mcstas_sim';
     
     format13.name       ='McStas Sqw table';
     format13.patterns   ={'Sqw data file for Isotropic_Sqw'};
-    format13.options    ='--fast --binary  --silent --headers';
+    format13.options    ='--fast --binary  --headers';
     format13.method     ='looktxt';
     format13.postprocess='load_mcstas_sqw';
     format13.extension  ='sqw';
     
     format14.name       ='INX tof data';
-    format14.options    ='--headers --fortran  --silent --catenate --fast --binary';
+    format14.options    ='--headers --fortran  --catenate --fast --binary';
     format14.method     ='looktxt';
     format14.postprocess='load_ill_inx';
     format14.patterns   ={'INX'};
