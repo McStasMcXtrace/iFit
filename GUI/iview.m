d151d83873d141bd08ca45a49be199389a01287b
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2008-12-16 15:42:56 +0000

    Second functional version of iView: objects can be dragged to another window, using RIGHT mouse button. Will be improved.

--- GUI/iview.m
+++ GUI/iview.m
@@ -12,6 +12,7 @@ function iview(varargin)
 
 
 % ARGUMENTS and ACTIONS
+% new, load, save_data, resize open_data, close, exit, about, mouse_down, mouse_up, mouse_drag
 
 % handling of arguments
 if nargin==0, instance=[]; else instance=varargin{1}; end
@@ -20,7 +21,8 @@ if nargin<=2, object  =[]; else object  =varargin{3}; end
 % check that instance is a single numeric figure handle
 if ~isnumeric(instance)
   % shift arg to action
-  if isempty(action), action=instance; end
+  if isempty(object), object=action; end
+  action=instance;
   instance=[];
 end
 if ~ischar(action)
@@ -29,42 +31,78 @@ if ~ischar(action)
   action='load';
 end
 
+% the interface is built in iView_private_create (menus, ...)
 % create or raise interface
 instance=iView_private_create(instance);
 
 switch action
-case 'new'
-  instance=figure;
-  instance=iView_private_create(instance);
-  return
-case 'load'
-  if isempty(object), object= iData(''); end
-  iView_private_icon(instance, 'load', object)
-case 'resize'
-  iView_private_icon(instance, 'resize', []);
-  return
-otherwise
-  if ~isempty(action)
-    disp([' Unknown action ' action ' in ' mfilename ]);
-  end
+  case 'new'
+    % open a new iView instance
+    instance=figure;
+    instance=iView_private_create(instance);
+  case 'load'
+    % open file selector to open data files
+    set(instance, 'Pointer', 'watch');
+    if isempty(object), object= iData(''); else object=iData(object); end
+    iView_private_icon(instance, 'load', object);
+    iView_private_icon(instance, 'documents', []);
+    set(instance, 'Pointer', 'arrow');
+  case 'save_data'
+    % open file selector to save data sets
+    if ~isempty(object)
+      set(instance, 'Pointer', 'watch');
+      iView_private_icon(instance, 'saveas', object);
+      set(instance, 'Pointer', 'arrow');
+    end
+  case 'save_config'
+    iView_private_config(instance, 'save');
+  case 'resize'
+    % resize instance
+    iView_private_icon(instance, 'resize', []);
+  case 'open_data'
+    % open data set (plot it)
+    iView_private_icon(instance, 'open', object);
+  case 'close_data'
+    % close (delete) data se
+    iView_private_icon(instance, 'delete', object);
+  case 'close'
+    % close this instance
+    close(instance);
+  case 'exit'
+    % close all instances
+    % save configuration first
+    iview(gcf, 'save_config');
+    % find all existing iView instances
+    iLoad('','save config');
+    instance_list=findall(0,'Tag','iView_instance');
+    close(instance_list);
+    disp([ '% ' datestr(now) ' Exiting iView' ]);
+  case 'about'
+    % dialog box about iView
+    helpstr = {'iView 0.1: This program enables to',...
+	  '1- load data sets',...
+	  '2- view/zoom/select data',...
+	  '3- fit with any function or function set',...
+	  '4- choose the fit method',...
+	  '5- save your fit results and data',...
+	  '6- configure many features at users choice',...
+	  ' ',...
+	  'Authors : E. Farhi <farhi@ill.fr>',...
+	  '*** iView comes with ABSOLUTELY NO WARRANTY',...
+	  'This is free software, and you are welcome',...
+	  'to redistribute it under certain conditions',...
+	  'as specified in Licence files.'};
+     helpdlg(helpstr,'iView About');
+  case 'mouse_down'
+    % handle mouse events (drag'n drop, move, select, ...)
+    iView_private_mouse(instance, 'down', object);
+  case 'mouse_drag'
+    iView_private_mouse(instance, 'drag', object);
+  case 'mouse_up'
+    iView_private_mouse(instance, 'up', object);
+  otherwise
+    if ~isempty(action)
+      disp([' Unknown action ' action ' in ' mfilename ]);
+    end
 end
-% iview actions:
-%   build         build empty interface
-%   load_config   read INI file
-%   save_config   write INI file (config only, not the data)
-%   load          send argument to iData, import and create/update icons
-%   save          save config and all data sets into INI/M script
-%   delete        remove data set and icon
-%   print         print interface or selected elements
-%   select        toggle icon selection state
-%   update        check all data sets and update icons/menus
-%   align         re-arrange all icons and align them on a grid
-%   exit          save configuration (if needed), clear memory and destroy interface
-%   copy          identify data set and store it into clipboard
-%   paste         paste data set
-%   duplicate     copy+paste
-%   edit          edit iView configuration or data sets (if selection is active)
-
-% INTERFACE
-
 
--- GUI/private/iView_private_config.m
+++ GUI/private/iView_private_config.m
@@ -1,8 +1,16 @@
-function config=iView_private_config(instance, action)
+function config=iView_private_config(instance, action, config)
+% handle iView instance config
+% action: load, save
 
 switch action
 case 'load'
-  config=iView_private_config_load(instance);
+  config=iView_private_config_load(0);
+case 'save'
+  if nargin<3, config=[]; end
+  if isempty(config), config=iView_private_config_load(0); end
+  config=iView_private_config_save(config);
+  setappdata(0, 'iView_Config', config);
+  disp([ '% Saved iView configuration into ' config.FileName ]);
 otherwise
   disp([' Unknown action ' action ' in ' mfilename ]);
 end
@@ -10,40 +18,69 @@ end
 % =============================================================================
 %                    INLINE functions
 % =============================================================================
+% iView_private_config_load
+% iView_private_config_save
+% iView_private_config_save_rec
+
 function config=iView_private_config_load(instance)
-config=[];
-
-% is there an existing config ? If yes, re-use it
-if ishandle(instance)
-  if isappdata(instance, 'Config')
-    config=getappdata(instance, 'Config');
-  end
-else % iView instance does not exist, search for an other instance to get config
-  % find all existing iView instances
-  instance_list=findall(0,'Tag','iView_instance');
-  if ~isempty(instance_list)
-    instance=instance_list(1);  % use first existing instance
-    config=getappdata(instance, 'Config');
-  end
-end
-
-if isempty(config) % doen when building first iView instance
-  % else get default default configuration
-  config.FileName         = 'iView';
-  config.PaperUnits       = 'centimeters';
-  config.PaperOrientation = 'landscape';
-  config.PaperPosition    = [5 5 20 15];
-  config.Color            = [1 1 1]; % white
-  config.IconSize         = 64;
-
-  % customized menus 
-  % Label=cell of {'Menu/Item','callback'...} with associated callbacks
-  % To insert a Separator, use 'Menu/Item'='Menu/Separator'
-  config.Menu= {
-  'Tools/Custom entry','disp(''hello'')', ...
-  };
-
-  % and overrides by any local configuration file
+% iView_private_config_load load configuration from config file, or existing window, or default
+  config=[];
   
-end
+  % search config file
+  if exist('iView_ini')
+    config = iView_ini;
+  elseif exist(fullfile(prefdir, 'iView.ini'), 'file')
+    % there is an iLoad_ini in the Matlab preferences directory: read it
+    config = fullfile(prefdir, 'iView.ini');
+    fid = fopen(config, 'r');
+    content = fread(fid, Inf, 'uint8=>char');
+    fclose(fid);
+    % evaluate content of file
+    config=[]; eval(content(:)'); % this make a 'config' variable
+  end
 
+  if isempty(config)
+    % is there an existing config ? If yes, re-use it
+    if ishandle(instance)
+      if isappdata(instance, 'iView_Config')
+        config=getappdata(instance, 'iView_Config');
+      end
+    else % iView instance does not exist, search for an other instance to get config
+      % find all existing iView instances
+      instance_list=findall(0,'Tag','iView_instance');
+      if ~isempty(instance_list)
+        instance=instance_list(1);  % use first existing instance
+        config=getappdata(instance, 'iView_Config');
+      end
+    end
+  end
+  
+  % make sure some fields exist and apply default if required
+  if ~isfield(config,'FileName'),         config.FileName         = 'iView.ini'; end
+  if ~isfield(config,'PaperUnits'),       config.PaperUnits       = 'centimeters'; end
+  if ~isfield(config,'PaperOrientation'), config.PaperOrientation = 'landscape'; end
+  if ~isfield(config,'PaperPosition'),    config.PaperPosition    = [5 5 20 15]; end
+  if ~isfield(config,'Color'),            config.Color            = [1 1 1];  end
+  if ~isfield(config,'IconSize'),         config.IconSize         = 64; end
+  if ~isfield(config,'Menu'),             config.Menu             = {}; end
+  if ~isfield(config,'ExitConfirm'),      config.ExitConfirm      = 1; end
+
+function config=iView_private_config_save(config)
+  % check config file name to use
+  filename=fullfile(prefdir, 'iView.ini'); % store preferences in PrefDir (Matlab)
+  config.FileName = filename;
+  str = [ '% iView configuration script file ' sprintf('\n') ...
+          '%' sprintf('\n') ...
+          '% Matlab ' version ' m-file ' filename ' saved on ' datestr(now) ' with iview(''save_config'')' sprintf('\n') ...
+          '%' sprintf('\n') ...
+          '% NOTE: The resulting structure must be named "config"' sprintf('\n') ...
+          '%' sprintf('\n') ...
+          class2str('config', config) ];
+  [fid, message]=fopen(filename,'w+');
+  if fid == -1
+    warning(['Error opening file ' filename ' to save iView configuration.' ]);
+  else
+    fprintf(fid, '%s', str);
+    fclose(fid);
+  end
+  
