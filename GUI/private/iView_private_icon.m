895cdfee6055828dd2ab527d42e3fcff04ab2b0c
farhi <farhi@ill.fr>
2008-12-16 15:42:56 +0000

    Second functional version of iView: objects can be dragged to another window, using RIGHT mouse button. Will be improved.

--- GUI/private/iView_private_icon.m
+++ GUI/private/iView_private_icon.m
@@ -1,55 +1,108 @@
-function iView_private_icon(instance, action, object)
+function hIcon=iView_private_icon(instance, action, object)
 % iView_private_icon_new Add a new data set inside the iView instance
 % The object may be an iData, or any other type, which is then sent to iData
-% before importation
+% before importation.
+% action: load, check, resize, documents, open
 
+hIcon=[];
 if nargin < 3, return; end
 
 if isempty(action), action='check'; end
 
-if strcmp(action, 'resize')
+if strcmp(action, 'resize')     % re-arrange icons in instance window
   iView_private_icon_resize(instance);
+  return
+end
+if strcmp(action, 'documents')  % populate the Documents menu
+  iView_private_icon_documents(instance);
   return
 end
 
 if isempty(object), return; end
 
+Data = getappdata(instance, 'Data');
 % make sure we have an iData
-if ~isempty(object) & ~isa(object, 'iData')
-  object=iData(object);
+if ~isempty(object) & ischar(object)
+  object=cellstr(object);
+end
+if ~isempty(object) & iscellstr(object)
+  new_object=[];
+  for index=1:length(object)
+    this = findobj(Data, 'Tag', object{index});
+    if isempty(this)
+      % object has no corresponding iData
+      % remove uicontrol and exit
+      hIcon = findobj(instance, 'Tag', object{index});
+      delete(hIcon);
+    else
+      new_object= [new_object this ];
+    end
+  end
+  object=new_object;
+  if isempty(object), return; end
+end
+if ~isa(object, 'iData')
+  object=iData(object); % will also popup FileSelector from iLoad/uigetfiles if object=''
 end
 
 % handle iData arrays
 if length(object) > 1
   for index=1:length(object(:))
-    iView_private_icon(instance, action, object(index));
+    hIcon= [hIcon iView_private_icon(instance, action, object(index))];
   end
   return
 end
 
-% check if the element already exists, add it if no
-Data = getappdata(instance, 'Data');
-config = getappdata(instance, 'Config');
-index = get(Data, 'Tag');
+config = iView_private_config(instance, 'load');
+index  = strmatch(object.Tag, get(Data, 'Tag'), 'exact');
 
-index = strmatch(object.Tag, index, 'exact');
-
-if isempty(index) | strcmp(action, 'load')
+switch action
+case 'check'
+  hIcon = findobj(instance, 'Tag', object.Tag);
+  if ~isempty(hIcon) & isempty(index)
+    delete(hIcon);
+  end
+case 'open'
+  hIcon=subplot(object);
+  return
+case 'delete'
   if ~isempty(index)
+    % remove iData and corresponding uicontrol
+    if length(Data) > 1, 
+      Data(index) = [];
+      Data=Data(find(~isempty(Data)));
+    else Data=[]; end
+    setappdata(instance, 'Data', Data);
+    iView_private_icon(instance, 'check', object);
+  end
+case 'saveas'
+  if ~isempty(object)
+    hIcon=saveas(object, 'gui');
+    if ~isempty(hIcon)
+      fprintf(1, 'Object %s\n  saved into file %s\n', char(object), hIcon);
+    end
+  end
+  return
+case 'load'
+  % check if the element already exists, add it if no
+  if ~isempty(index) % already exists 
     % make a copy of the object
     object = copyobj(object);
   end
   Data = [ Data object ]; % store new object
   setappdata(instance, 'Data', Data);
   index= length(Data);
+otherwise
 end
+
+if isempty(index), return; end
 
 % determine position of icon: we use the icon size from the Config
 % position is set from iData index (end of array), icon size and window width
 [iRow,iColumn]  = iView_private_icon_position(instance, index);
 
 NL = sprintf('\n');
-tooltip = [ object.Title NL object.Source NL 'Data size [' num2str(size(object)) ']' NL ];
+tooltip = [ object.Title NL object.Source NL 'Tag ' object.Tag '; Data size [' num2str(size(object)) ']' NL ];
 for ax_index = 0:ndims(object)
   [axisdef, lab] = getaxis(object, num2str(ax_index));
   lab=deblank(lab);
@@ -66,6 +119,7 @@ if strcmp(action, 'load')
   hIcon = uicontrol(instance,'Style','togglebutton',...
                   'Tag', object.Tag, ...
                   'Value',0, ...
+                  'Callback', 'iview(gcf, ''mouse_down'', gco);', ...
                   'CData', frame.cdata);
 else
   hIcon = findobj(instance, 'Tag', object.Tag);
@@ -97,46 +151,79 @@ set(hIcon, ...
 %   properties
 %   edit (file)
 %   edit (axes, signal, title, ...)
+figure(instance);
+cmenu = uicontextmenu('Parent',instance);
+set(hIcon, 'UIContextMenu', cmenu);
+uimenu(cmenu, 'Label', object.Title);
+uimenu(cmenu, 'Label', 'Open', 'Separator','on', 'Callback', [ 'iview(gcf, ''open_data'', ''' object.Tag ''');' ]);
+uimenu(cmenu, 'Label', 'Save as...', 'Callback', [ 'iview(gcf, ''save_data'', ''' object.Tag ''');' ]);
+uimenu(cmenu, 'Label', 'Close', 'Callback', [ 'iview(gcf, ''close_data'', ''' object.Tag ''');' ]);
 
 % =============================================================================
 %                    INLINE functions
 % =============================================================================
 % iView_private_icon_position
+% iView_private_icon_resize
+% iView_private_icon_documents
 
 function [iRow,iColumn]  = iView_private_icon_position(instance, index)
 % iView_private_icon_position determine position of the indexed element in an 
 % iView instance. 
 
-hIcon = [];
+  hIcon = [];
 
-config = getappdata(instance, 'Config');
+  config = iView_private_config(instance, 'load');
 
-% size of Icon is [ 2*config.IconSize config.IconSize ]
+  % size of Icon is [ 2*config.IconSize config.IconSize ]
 
-% number of elements per row
-instancePosition = get(instance, 'Position');
-instanceWidth    = instancePosition(3);
-nbElementsPerRow = ceil(instanceWidth/(3*config.IconSize));
-
-if instanceWidth < 2.25*config.IconSize
-  instancePosition(3) = 2.25*config.IconSize;
-  set(instance, 'Position', instancePosition);
+  % number of elements per row
+  instancePosition = get(instance, 'Position');
   instanceWidth    = instancePosition(3);
-end
+  nbElementsPerRow = max(1,floor(instanceWidth/(2.25*config.IconSize)));
 
-% get icon index
-iColumn = mod(index-1, nbElementsPerRow);
-iRow    = floor((index-1)/nbElementsPerRow);
-% make it pixels in the window (with a gap of 1/4 between elements, and 1/2 shift)
-iRow    = iRow    *(1.25*config.IconSize) + config.IconSize/4;
-iColumn = iColumn *(2.25*config.IconSize) + config.IconSize/4;
-% fill in from the top (revert iRow)
-iRow = instancePosition(4) - iRow - config.IconSize;
+  if instanceWidth < 2.25*config.IconSize
+    instancePosition(3) = 2.25*config.IconSize;
+    set(instance, 'Position', instancePosition);
+    instanceWidth    = instancePosition(3);
+    figureslider(instance, 'resize');
+  end
+
+  % get icon index
+  iColumn = mod(index-1, nbElementsPerRow);
+  iRow    = floor((index-1)/nbElementsPerRow);
+  % make it pixels in the window (with a gap of 1/4 between elements, and 1/2 shift)
+  iRow    = iRow    *(1.25*config.IconSize) + config.IconSize/4;
+  iColumn = iColumn *(2.25*config.IconSize) + config.IconSize/4;
+  % fill in from the top (revert iRow)
+  iRow = instancePosition(4) - iRow - config.IconSize;
 
 function iView_private_icon_resize(instance)
-% reposition icons inside iView instance
+% iView_private_icon_resize reposition icons inside iView instance
 
-Data = getappdata(instance, 'Data');
-iView_private_icon(instance, 'check', Data);
+  Data = getappdata(instance, 'Data');
+  iView_private_icon(instance, 'check', Data);
 
+function iView_private_icon_documents(instance)
+% iView_private_icon_documents Update Documents menu with list of loaded iData objects
 
+  documents = findobj(instance, 'Tag', 'Documents');
+  items     = get(documents, 'Children');
+  
+  % clear Static handles from the list (not from menu)
+  index     = strmatch('Static', get(items, 'Tag'), 'exact');
+  items(index)=[];
+  % delete other entries (as we shall rebuild menu items)
+  delete(items);
+  
+  % populate Documents
+  Data = getappdata(instance, 'Data');
+  
+  for index=1:length(Data)
+    if length(Data) == 1, this=Data; else this=Data(index); end
+    if index==1
+      uimenu(documents, 'Label', Data(index).Title, 'Callback', [ 'iview(gcf, ''open_data'', ''' this.Tag ''');' ], 'Separator', 'on');
+    else
+      uimenu(documents, 'Label', Data(index).Title, 'Callback', [ 'iview(gcf, ''open_data'', ''' this.Tag ''');' ]);
+    end
+  end
+  
---
+++ GUI/private/iView_private_mouse.m
@@ -0,0 +1,148 @@
+function hIcon=iView_private_mouse(instance, action, object)
+% iView_private_icon_mouse Handle mouse/keyboard events in window/icons
+% action: down, drag, up
+
+if isempty(action), return; end
+
+obj.type  =get(gco,'type');  
+if strcmp(obj.type, 'uicontrol')
+  obj.type =get(gco,'style');  
+end
+
+obj.tag   =get(gco,'tag');
+press.type=get(gcf,'SelectionType'); % normal, extend(+shift), alternate(+ctrl), open(double click)
+
+if isempty(obj.type) | isempty(obj.tag), return; end
+
+% only acts on buttons (but not the 'center' one from the sliders)
+if ~isempty(strmatch(obj.tag, {'Slider_v','Slider_h','Center'},'exact'))
+  return;
+end
+if strcmp(action, 'down')
+  obj
+press
+action
+  switch obj.type
+  case {'togglebutton','checkbox'}
+    % action on an icon (open, drag'n drop)
+    if strcmp(press.type, 'open')
+      iview(gcf,'open',obj.tag);
+    elseif strcmp(press.type, 'normal') | strcmp(press.type, 'alt')
+      % handle movement of icon
+      set(gcf,'Pointer','Fleur');                                  % Cursor to fleur
+      %set(gcf,'WindowButtonMotionFcn',[ 'iview(' num2str(gcf) ',''mouse_drag'',''' obj.tag ''');' ]); % Drag when moved
+      set(gcf,'WindowButtonUpFcn',[ 'iview(' num2str(gcf) ',''mouse_up'',''' obj.tag ''');' ]);  % end of movement
+      setappdata(gcf,'dragplot',gco);
+    end
+  case 'figure'
+    % action on the figure background (select rectangle)
+    % sets WindowButtonMotionFcn and WindowButtonUpFcn
+  end
+elseif strcmp(action, 'drag')
+  % do nothing yet. We just wait for end of movement. Could display here a message as 'Dragging object(s) so and so'
+    obj
+press
+action
+elseif strcmp(action, 'up')
+  obj
+press
+action
+  f = get(0,'pointerwindow');           % determine which figure window it's over (target). gcf=original
+  h = getappdata(gcf,'dragplot');       % Retrieve the handle to the dragged object.
+  o = gcf;
+  
+  if f > 0 & strcmp(get(f, 'Tag'),'iView_instance') % If it's a valid iView figure window,
+    Data0  = getappdata(gcf, 'Data');     % data in original window
+    Data1  = getappdata(f, 'Data');       % data in target window
+    object = findobj(Data0, 'Tag', get(h,'Tag'));          % find iData which has same Tag as pushbutton
+    p = get(f,'currentpoint');          % then determine the current position (target),
+    t = get(gcf,'selectiontype');     % determine whether the CTRL key was pressed too.
+    if iscell(object), object = object{1}; end
+    index = iView_private_icon_index(gcf, p); % index of target position in the iData array
+    index0= strmatch(object.Tag, get(Data0,'Tag'),'exact');
+    if strcmp(t,'alt') & f~=gcf               % If CTRL was pressed (or right button),
+      % make a copy of object (drag+ctrl)
+      object = copyobj(object);
+      iView_private_icon(f, 'load', object);  % add object at the end of all items (in target)
+      % copy object to the target position
+      if index > 1,              p1 = Data1(1:(index-1)); 
+      else 
+        if length(Data1) == 1, p1=Data1; 
+        else p1= [] ; end
+      end
+      if index <= length(Data1) & length(Data1)>1, p2 = Data1(index:end);   else p2 = []; end
+      Data1 = [ p1 object p2];
+      setappdata(f, 'Data', Data1);
+      % re-order icons
+      iView_private_icon(f, 'resize', []);
+    else
+      if f==gcf 
+        % move in same figure
+        % index of original object
+        if length(Data0)>1 & index ~= index0
+          index0= strmatch(object.Tag, get(Data0,'Tag'),'exact');
+          Data0(index0) = [];  % remove original position
+          if index > 1,              p1 = Data0(1:(index-1)); 
+          else 
+            if length(Data0) == 1, p1=Data0; 
+            else p1= [] ; end
+          end
+          if index <= length(Data0) & length(Data0)>1, p2 = Data0(index:end);   else p2 = []; end
+          Data0 = [ p1 object p2];
+          setappdata(f, 'Data', Data0);
+          % re-order icons
+          iView_private_icon(f, 'resize', []);
+        end
+      else
+        % get object, copy it to target window, remove original
+        % make a copy of object (drag+ctrl)
+        iView_private_icon(f, action, object);  % add object at the end of all items (in target)
+        % copy object to the target position
+        if index > 1,              p1 = Data1(1:(index-1)); 
+        else 
+          if length(Data1) == 1, p1=Data1; 
+          else p1= [] ; end
+        end
+        if index <= length(Data1) & length(Data1)>1, p2 = Data1(index:end);   else p2 = []; end
+        Data1 = [ p1 object p2];
+        setappdata(f, 'Data', Data1);
+        % re-order icons
+        iView_private_icon(f, 'resize', []);  % target window is updated
+        if length(Data0) > 1,
+          Data0(index0) = [];  % remove original position
+        else
+          Data0=[];
+        end
+        setappdata(gcf, 'Data', Data0);
+        iView_private_icon(f, 'resize', []);  % target window is updated
+        iView_private_icon(f, 'check', []);   % target window is updated
+      end
+    end
+  end
+  
+  set(gcf,'WindowButtonMotionFcn','');
+  set(gcf,'Pointer','arrow');
+  set(gcf,'WindowButtonUpFcn','');
+end
+
+
+function index = iView_private_icon_index(instance, position)
+  config = iView_private_config(instance, 'load');
+
+  % size of Icon is [ 2*config.IconSize config.IconSize ]
+
+  % number of elements per row
+  instancePosition = get(instance, 'Position');
+  instanceWidth    = instancePosition(3);
+  nbElementsPerRow = max(1,floor(instanceWidth/(2.25*config.IconSize)));
+  
+  % determine col/row indexes from position of pointer
+  iColumn = position(1) - config.IconSize/4;
+  iColumn = max(1,floor(iColumn / (2.25*config.IconSize)));
+  iRow    = position(2) - config.IconSize/4;
+  iRow    = max(1,floor(iRow/ (1.25*config.IconSize)));
+  
+  index   = ((iRow-1)*nbElementsPerRow+(iColumn-1))+1;
+  index   = min(length(getappdata(instance, 'Data')), index);
+
+
