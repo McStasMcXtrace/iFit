9cf26a9a4f84113d85137c911db03b9de16f055e
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2009-03-20 15:20:33 +0000

    ind2sub(array, 1) returns array(1). ind2sub(single,1) returns single.Signal(1)

--- trunk/@iData/ind2sub.m
+++ trunk/@iData/ind2sub.m
@@ -1,17 +1,17 @@
 function data = ind2sub(data, indexes)
 % ind2sub(s,index) : get indexed element in an iData array
 %
-%   @iData/ind2sub is equivalent to accessing directly the indexed element,
+%   @iData/ind2sub is equivalent to accessing directly the indexed element in arrays,
 %         except when the array is of lenght 1.
-%         When length(s) is 1, s(1) would return the first element of s,
-%         whereas ind2sub(s,1) returns s (first element of the array).
+%         When length(s) is 1, s(1) would return s itself,
+%         whereas ind2sub(s,1) returns the first element of its 'Signal',
 %
 % input:  s:     object or array (iData)
 %         index: index in array
 % output: s(index)
 % ex :    ind2sub(s, 1)
 %
-% Version: $Revision: 1.1 $
+% Version: $Revision: 1.2 $
 % See also iData, iData/disp, iData/get, iData/size
 
 % EF 23/09/07 iData implementation
@@ -23,5 +23,8 @@ function data = ind2sub(data, indexes)
     data=data(indexes);
   else
     if length(data) == 0, data=[]; 
+    else 
+    	S = data.Signal;
+    	data = S(1);
     end
   end    
