bf7e5c251f376c1ae8da43ac11a298c6e87fc4f1
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2009-03-22 15:04:04 +0000

    a([]) returns []

--- trunk/@iData/subsref.m
+++ trunk/@iData/subsref.m
@@ -5,7 +5,7 @@ function b = subsref(a,S)
 %   such as a(1:2) or a.field.
 %   The special syntax a{0} where a is a single iData returns the signal, and a{n} returns the axis of rank n.
 %
-% Version: $Revision: 1.7 $
+% Version: $Revision: 1.8 $
 % See also iData, iData/subsasgn
 
 % This implementation is very general, except for a few lines
@@ -34,6 +34,7 @@ for i = 1:length(S)     % can handle multiple index levels
       end
       % this is where specific class structure is taken into account
       if ischar(s.subs{1}), b=get(b, s.subs{:}); return; end
+      if isempty(s.subs{:}), b=[]; return; end
       if length(s.subs{:}) == 1 && s.subs{:} == 1, return; end
       d=get(b,'Signal'); d=d(s.subs{:});  b=set(b,'Signal', d);
 
