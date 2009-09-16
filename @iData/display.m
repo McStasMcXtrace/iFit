6d79ad78b16f775f5ad2a972725afb87084e5806
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2009-09-16 21:43:24 +0000

    display empty iData as empty...

--- @iData/display.m
+++ @iData/display.m
@@ -9,7 +9,7 @@ function d = display(s_in)
 % output: d: string to display (char)
 % ex:     'display(iData)' or 'iData'
 %
-% Version: $Revision: 1.4 $
+% Version: $Revision: 1.5 $
 % See also iData, iData/disp, iData/get
 
 % EF 27/07/00 creation
@@ -19,38 +19,43 @@ d = [ sprintf('%s = ',inputname(1)) ];
 if length(s_in) > 1
   d = [ d sprintf(' array [%s]',num2str(size(s_in))) ];
 end
-d = [ d sprintf(' iData object:\n\n') ];
+if length(s_in) == 0
+    d = [ d sprintf(' iData object: empty\n') ];
+else
+    d = [ d sprintf(' iData object:\n\n') ];
+    s_in = s_in(:);
+    if length(s_in) > 1
+      d = [ d sprintf('Index ') ];
+    end
+    d = [ d sprintf('    [Tag]     [Dimension]                           [Title] [Last command]          [Label]\n') ];
 
-s_in = s_in(:);
-if length(s_in) > 1
-  d = [ d sprintf('Index ') ];
-end
-d = [ d sprintf('    [Tag]     [Dimension]                           [Title] [Last command]          [Label]\n') ];
+    % now build the output string
+    for index=1:length(s_in)
+      s = s_in(index);
+      if length(s_in) > 1
+        d = [ d sprintf('%5i ',index) ];                        % index
+      end
+      if isempty(s.Tag)
+        d = [ d sprintf('%9s ','<nul>') ];                      % Tag
+      else
+        d = [ d sprintf('%9s ',s.Tag) ];
+      end
+      d = [ d sprintf('%15s ', ['[' num2str(size(s)) ']' ]) ];  % size
+      t = cellstr(s.Title); t = deblank(t{1});
+      if length(t) > 31, t = [ t(1:28) '...' ]; end             % title
+      d = [ d sprintf('%33s ', [ '''' t '''' ]) ];
+      h = cellstr(s.Command); h=deblank(h{end});
+      if length(h) > 23, h = [ h(1:20) '...' ]; end             % last command
+      d = [ d sprintf('%s ', h) ];
+      h = cellstr(s.Label); h=deblank(h{end});
+      if length(h) > 13, h = [ h(1:10) '...' ]; end             % Label
+      d = [ d sprintf('%s ', h) ];
 
-% now build the output string
-for index=1:length(s_in)
-  s = s_in(index);
-  if length(s_in) > 1
-    d = [ d sprintf('%5i ',index) ];                        % index
-  end
-  if isempty(s.Tag)
-    d = [ d sprintf('%9s ','<nul>') ];                      % Tag
-  else
-    d = [ d sprintf('%9s ',s.Tag) ];
-  end
-  d = [ d sprintf('%15s ', ['[' num2str(size(s)) ']' ]) ];  % size
-  t = cellstr(s.Title); t = deblank(t{1});
-  if length(t) > 31, t = [ t(1:28) '...' ]; end             % title
-  d = [ d sprintf('%33s ', [ '''' t '''' ]) ];
-  h = cellstr(s.Command); h=deblank(h{end});
-  if length(h) > 23, h = [ h(1:20) '...' ]; end             % last command
-  d = [ d sprintf('%s ', h) ];
-  h = cellstr(s.Label); h=deblank(h{end});
-  if length(h) > 13, h = [ h(1:10) '...' ]; end             % Label
-  d = [ d sprintf('%s ', h) ];
-  
-  d = [ d sprintf('\n') ];
+      d = [ d sprintf('\n') ];
+    end
 end
+
+
 
 if nargout == 0
   fprintf(1,d);
