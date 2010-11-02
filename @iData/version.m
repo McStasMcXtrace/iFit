7da10c028408fb1945c58a0c9d3712d9fff02eea
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2010-11-02 09:50:14 +0000

    cosmetics

--- @iData/version.m
+++ @iData/version.m
@@ -3,12 +3,12 @@ function b = version(a)
 %
 %   @iData/version: version of the iData class library
 %
-% Version: $Revision: 1.4 $
+% Version: $Revision: 1.5 $
 
 % EF 23/09/07 iData impementation
 
-vers = '$Revision: 1.4 $';
-date = '12 Jun 2008';
+vers = '1.0';
+date = 'Oct 2010';
 auth = 'E.Farhi, P. Willendrup and Y.Debab, (c) ILL/DS/CS <farhi@ill.eu>';
 b = [ 'Matlab/iData version ' vers ' (' date ') by ' auth ];
 
