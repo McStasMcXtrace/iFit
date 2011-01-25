1cdf593f5f1df2c454d9962978f1306d383b6167
farhi <farhi@ill.fr>
2011-01-25 13:01:22 +0000

    Cosmetics and better support for non double array import

--- @iData/version.m
+++ @iData/version.m
@@ -3,12 +3,12 @@ function b = version(a)
 %
 %   @iData/version: version of the iData class library
 %
-% Version: $Revision: 1.5 $
+% Version: $Revision: 1.6 $
 
 % EF 23/09/07 iData impementation
 
 vers = '1.0';
-date = 'Oct 2010';
+date = 'Jan 2011';
 auth = 'E.Farhi, P. Willendrup and Y.Debab, (c) ILL/DS/CS <farhi@ill.eu>';
 b = [ 'Matlab/iData version ' vers ' (' date ') by ' auth ];
 
