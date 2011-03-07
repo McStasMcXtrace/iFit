8d229ea4a430c4176e8cdf8e8e9d3e3c8bb12dd7
farhi <farhi@ill.fr>
2011-03-07 20:28:27 +0000

    iLoad: Fixed import of multiple files. mkdist: added automatic version change when creating dist Docs: added Changes, visible from index.html

--- @iData/version.m
+++ @iData/version.m
@@ -3,12 +3,12 @@ function b = version(a)
 %
 %   @iData/version: version of the iData class library
 %
-% Version: $Revision: 1.9 $
+% Version: $Revision: 1.10 $
 
 % EF 23/09/07 iData impementation
 
-vers = '1.0-RC1';
-date = 'Feb 2011';
+vers = '@IFIT_VERSION@';
+date = '@IFIT_DATE@';
 auth = 'E.Farhi, P. Willendrup and Y.Debab, (c) ILL/DS/CS <farhi@ill.eu> GPL2';
 b = [ 'Matlab/iData version ' vers ' (' date ') by ' auth ];
 
