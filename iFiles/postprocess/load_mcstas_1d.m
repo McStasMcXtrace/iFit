dd83a4a1531a8b0f236446535bff3f6b4e424df6
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2011-03-31 14:44:55 +0000

    iLoad: now transparently interpret XYE files and associate columns. The McStas 1D postprocess is also used for default text import.

--- iFiles/postprocess/load_mcstas_1d.m
+++ iFiles/postprocess/load_mcstas_1d.m
@@ -1,20 +1,29 @@
 function a=load_mcstas_1d(a)
 % function a=load_mcstas_1d(a)
 %
-% Returns an iData style dataset from a McStas 1d monitor file
+% Returns an iData style dataset from a McStas 1d monitor file, or even simple XYE files
+% Some labels are also searched.
 %
 
 % Find proper labels for Signal and Axis
 a=iData(a);
 
 if ~isempty(findfield(a, 'xlabel')) 
-  xlabel = a.Data.Headers.MetaData.xlabel;
-  xlabel(1:length('# xlabel: '))='';
-else xlabel=''; end
+  xlab = a.Data.Headers.MetaData.xlabel;
+  xlab(1:max(strfind(xlab,'xlabel')+6))='';
+elseif ~isempty(findfield(a, 'x_label')) 
+  xlab = a.Data.Headers.MetaData.x_label;
+  xlab(1:max(strfind(xlab,'x_label'))+6)='';
+else xlab=''; end
+
 if ~isempty(findfield(a, 'ylabel')) 
-  ylabel = a.Data.Headers.MetaData.ylabel;
-  ylabel(1:length('# ylabel: '))='';
-else ylabel=''; end
+  ylab = a.Data.Headers.MetaData.ylabel;
+  ylab(1:max(strfind(ylab,'ylabel')+6))='';
+elseif ~isempty(findfield(a, 'y_label')) 
+  ylab = a.Data.Headers.MetaData.y_label;
+  ylab(1:max(strfind(ylab,'y_label')+6))='';
+else ylab=''; end
+
 if ~isempty(findfield(a, 'component')) 
   label = strtrim(a.Data.Headers.MetaData.component);
   label(1:length('# component: '))='';
@@ -23,19 +32,26 @@ if ~isempty(findfield(a, 'component'))
   setalias(a, 'Component', 'Data.Component','Component name');
 end
 
-Datablock = ['this.' getalias(a,'Signal')];
+% special case for McStas files and XYE (3 columns) files
+if (size(a,2) == 3 && size(a,1) >= 5) || strfind(a.Title,'McStas 1D monitor')
 
-% First column is the scan parm, we denote that 'x'
-setalias(a,'x',[Datablock '(:,1)'],xlabel);
-setalias(a,'Signal',[Datablock '(:,2)'],ylabel);
-try
-  setalias(a,'Error',[Datablock '(:,3)']);
-catch
-  setalias(a,'Error',0);
+  Datablock = ['this.' getalias(a,'Signal')];
+
+  % First column is the scan parm, we denote that 'x'
+  setalias(a,'x',[Datablock '(:,1)'],xlab);
+  setalias(a,'Signal',[Datablock '(:,2)'],ylab);
+  try
+    setalias(a,'Error',[Datablock '(:,3)']);
+  catch
+    setalias(a,'Error',0);
+  end
+  setalias(a,'E','Error');
+  if ~isempty(findfield(a, 'Error')) 
+    setalias(a,'N',[Datablock '(:,4)'],'# Events');
+  end
+  setaxis(a,1,'x');
+else
+  xlabel(a, xlab);
+  ylabel(a, ylab);
 end
-setalias(a,'E','Error');
-if ~isempty(findfield(a, 'Error')) 
-  setalias(a,'N',[Datablock '(:,3)'],'# Events');
-end
-setaxis(a,1,'x');
 
