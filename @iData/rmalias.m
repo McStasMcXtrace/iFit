c0fcab0cfe5d5182731019473e132b4ddcfecda6
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2011-04-14 09:57:52 +0000

    iFit: pack now removes unused iData parts (to save memory), getaxis and rmalias bug fixed, mkdist removes existing mex.

--- @iData/rmalias.m
+++ @iData/rmalias.m
@@ -11,13 +11,13 @@ function s_out = rmalias(a_in,names)
 % output: s: array (iData)
 % ex:     rmalias(iData,'Temperature')
 %
-% Version: $Revision: 1.2 $
+% Version: $Revision: 1.3 $
 % See also iData, iData/getalias, iData/get, iData/set, iData/setalias
 
 % EF 27/07/00 creation
 % EF 23/09/07 iData implementation
 if nargin == 1
-  names=[];
+  names='';
 end
 
 s_out = setalias(a_in, names);
