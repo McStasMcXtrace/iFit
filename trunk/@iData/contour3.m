746592c37bc41842fb8ddde692428b092cc702c1
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2011-07-26 21:13:54 +0000

    iData/plot: added/updated plotting methods

---
+++ trunk/@iData/contour3.m
@@ -0,0 +1,25 @@
+function a = contour3(a, option)
+% h = contour3(s,option) : Plot a 2D/3D object as 3D contour
+%
+%   @iData/contour3 function to plot a 2D or 3D object
+%     2D objects are shown as a 3D contour
+%
+% input:  s: object or array (iData)
+%         option: global option for 2D and 3D plots: 
+%                 flat, interp, faceted (for shading)
+%                 transparent, light, clabel, colorbar, hide_axes
+%                 axis tight, axis auto, view2, view3
+%                 painters (bitmap drawing), zbuffer (vectorial drawing)
+% output: h: graphics object handles (cell)
+% ex:     contour3(iData(peaks)); contour3(iData(flow));
+%
+% Version: $Revision: 1.1 $
+% See also iData, iData/plot
+
+if nargin ==1
+	option='';
+end
+h = plot(a, [ 'contour3 ' option ]);
+
+
+
--- trunk/@iData/ylim.m
+++ trunk/@iData/ylim.m
@@ -1,7 +1,7 @@
 function a = ylim(a, lims)
 % b = ylim(s,[ ymin ymax ]) : Reduce iData Y axis limits
 %
-%   @iData/ylim function to reduce the Y axis (rank 2, rows) limits
+%   @iData/ylim function to reduce the Y axis (rank 1, rows) limits
 %     ylim(s) returns the current Y axis limits. 
 %
 % input:  s: object or array (iData)
@@ -9,10 +9,10 @@ function a = ylim(a, lims)
 % output: b: object or array (iData)
 % ex:     b=ylim(a);
 %
-% Version: $Revision: 1.4 $
+% Version: $Revision: 1.5 $
 % See also iData, iData/plot, iData/ylabel
 
-axisvalues = getaxis(a, 2);
+axisvalues = getaxis(a, 1);
 if isempty(axisvalues), return; end
 if nargin == 1
   a=[ min(axisvalues) max(axisvalues) ]; 
@@ -21,7 +21,11 @@ end
 
 index=find(lims(1) <= axisvalues & axisvalues <= lims(2));
 s.type='()';
-s.subs={ ':', index };
+if ndims(a) > 1
+  s.subs={ index, ':' };
+else
+  s.subs={ index };
+end
 cmd=a.Command;
 a = subsref(a,s);
 a.Command=cmd;
