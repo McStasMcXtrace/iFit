bda54493b3efaf196717dbbc8ba83797f0466379
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2011-07-26 21:17:48 +0000

    iData: minor fixes

--- trunk/@iData/clim.m
+++ trunk/@iData/clim.m
@@ -9,7 +9,7 @@ function a = clim(a, lims)
 % output: b: object or array (iData)
 % ex:     b=clim(a);
 %
-% Version: $Revision: 1.4 $
+% Version: $Revision: 1.5 $
 % See also iData, iData/plot, iData/ylabel
 
 axisvalues = getaxis(a, 4);
@@ -21,7 +21,7 @@ end
 
 index=find(lims(1) <= axisvalues & axisvalues <= lims(2));
 s.type='()';
-s.subs={ ':', ':', index };
+s.subs={ ':', ':', ':', index };
 cmd=a.Command;
 a = subsref(a,s);
 a.Command=cmd;
