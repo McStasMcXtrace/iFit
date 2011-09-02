21bf39a48e8ab9d5c759979d32ed070d2561896d
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2011-09-02 12:52:14 +0000

    iLoad/image: reads EXIF data when available.

--- iFiles/private/mimread.m
+++ iFiles/private/mimread.m
@@ -1,6 +1,9 @@
 function s = mimread(filename)
 % mimread Wrapper to imfinfo/imread which reconstructs the image structure
 
-s      = imfinfo(filename);
-s.data = imread(filename);
+s       = imfinfo(filename);
+s.image = imread(filename);
+try
+  s.EXIF = exifread(filename);
+end
 
