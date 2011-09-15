b05533c4511de0e537beca3a879794a3c2eb37d0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2011-09-15 08:46:10 +0000

    iData: fft can now operate on all or a single dimension

--- @iData/ifft.m
+++ @iData/ifft.m
@@ -1,4 +1,4 @@
-function b = ifft(a)
+function b = ifft(a, varargin)
 % c = ifft(a) : computes the inverse Discrete Fourier transform of iData objects
 %
 %   @iData/ifft function to compute the inverse Discrete Fourier transform of data sets
@@ -10,8 +10,8 @@ function b = ifft(a)
 %         a=iData(t,0.7*sin(2*pi*50*t)+sin(2*pi*120*t)+2*randn(size(t)));
 %         c=fft(a); d=ifft(c); plot([ a d ])
 %
-% Version: $Revision: 1.3 $
+% Version: $Revision: 1.4 $
 % See also iData, iData/fft, iData/conv, FFT, IFFT
 
-b = fft(a, 'ifft');
+b = fft(a, 'ifft', varargin{:});
 
