9dc61a5b3a1fa0ad4c19db17ecc2898b1c93ef3e
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2011-10-05 14:19:35 +0000

    iFit: improved speed/efficiency of code for import/get. Added PBD file reader.

--- trunk/@iData/set.m
+++ trunk/@iData/set.m
@@ -13,11 +13,13 @@ function s_out = set(a_in,varargin)
 %
 % ex      : set(iData,'Title','A nice Title')
 %
-% Version: $Revision: 1.8 $
+% Version: $Revision: 1.9 $
 % See also iData, iData/get, iData/setalias, iData/setaxis
 
 % EF 27/07/00 creation
 % EF 23/09/07 iData implementation
+
+% calls: setalias
 
 if nargin == 1
   disp('iData object properties:');
@@ -50,14 +52,14 @@ for index = 1:length(s_out)
   
   while i1<=length(varargin)     % first parse fields and values
     if ischar(varargin{i1})      % normal 'PropertyName', Propertyvalue
-      prop_names  = { varargin{i1} };         % get single PropertyName
-      prop_values = { varargin{i1+1} };       % get single Propertyvalue
+      prop_names  = varargin(i1);         % get single PropertyName
+      prop_values = varargin(i1+1);       % get single Propertyvalue
       i1 = i1+2;
     elseif isstruct(varargin{i1})       % import structure
       prop_names = fieldnames(varargin{i1});         % get PropertyNames
       prop_values = struct2cell(varargin{i1});
       i1 = i1+1;
-    elseif iscell(varargin{i1}) & i1 < length(varargin) % import from 2 cells
+    elseif iscell(varargin{i1}) && i1 < length(varargin) % import from 2 cells
       prop_names = varargin{i1};         % get PropertyNames
       prop_values = varargin{i1+1};      % get Propertyvalues
       i1 = i1+2;
@@ -69,7 +71,7 @@ for index = 1:length(s_out)
       prop_value = prop_values{j1};
       % check for aliases
       alias_names = a.Alias.Names; % this is a cellstr of Alias names
-      alias_num   = strmatch(lower(prop_name), lower(alias_names), 'exact');
+      alias_num   = find(strcmpi(prop_name, alias_names));
       if length(alias_num) >= 1
         alias_values = a.Alias.Values;
         name = alias_names{alias_num(1)};
@@ -77,8 +79,8 @@ for index = 1:length(s_out)
         if ~ischar(prop_name), prop_name=[]; end
         % check if prop_name exists, and use it'
         try
-          if (isempty(prop_name) & (isnumeric(prop_value) | islogical(prop_value))) ...
-            | isnumeric(prop_name) | strcmp(prop_name, prop_value)
+          if (isempty(prop_name) && (isnumeric(prop_value) || islogical(prop_value))) ...
+            || isnumeric(prop_name) || strcmp(prop_name, prop_value)
             setalias(a, name, prop_value);
           else
             setalias(a, prop_name, prop_value);
@@ -92,19 +94,19 @@ for index = 1:length(s_out)
           iData_private_warning(mfilename, sprintf('can not set Property Alias %s=%s in object %s.', name, prop_name, [ inputname(1) ' '  a.Tag ]))
         end
       else
-        if strncmp(lower(prop_name), 'filename', 8)
+        if strncmpi(prop_name, 'filename', 8)
           prop_name = 'Source';
         end
-        if strncmp(lower(prop_name), 'alias', 5)
+        if strncmpi(prop_name, 'alias', 5)
           iData_private_warning(mfilename, 'to set Aliases, use the setalias function');
-        elseif strncmp(lower(prop_name), 'axis', 4)
+        elseif strncmpi(prop_name, 'axis', 4)
           iData_private_warning(mfilename, 'to set Axis, use the setaxis function, or the syntax iData{index}.');
         else
           try
             eval(['a.' prop_name '= prop_value;' ]);
           catch
             iData_private_warning(mfilename, sprintf('can not find Property %s in object %s. Setting it.', prop_name, [ inputname(1) ' '  a.Tag ]))
-            if ~isempty(prop_value) | isnumeric(prop_value), setalias(a, prop_name, prop_value); end
+            if ~isempty(prop_value) || isnumeric(prop_value), setalias(a, prop_name, prop_value); end
           end
         end
       end
@@ -120,6 +122,6 @@ if length(s_out) > 1
   s_out = reshape(s_out,size(a_in));
 end
 
-if nargout == 0 & length(inputname(1))
+if nargout == 0 && ~isempty(inputname(1))
   assignin('caller',inputname(1),s_out);
 end
