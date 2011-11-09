f2a4dc2493dbb019f11e8254937556fbb0507ac4
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2011-11-09 09:36:24 +0000

    iFiles: added open* functions for direct drag-drop -> open files to matlab

---
+++ trunk/iFiles/opensim.m
@@ -0,0 +1,10 @@
+function out = opensim(filename)
+%OPENSIM Open a McStas SIM file, display it
+%        and set the 'ans' variable to an iData object with its content
+
+out = iData(filename);
+plot(out);
+
+if ~isdeployed
+  assignin('base','ans',out);
+end
