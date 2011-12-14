ffd92dd2b4eb441deec97b37c6e3d95768a6751b
farhi <farhi@ill.fr>
2011-12-14 12:44:57 +0000

    iData/get: allocate empty return argument when nothing happened. Was left undefined. GUI/mcstas: display current monitors taking part in the criteria at each plot refresh. iFiles/netcdf1: redefine aliases only when new data is larger. iFiles/texmex: do not free memory blocks, as this creates segv. looktxt is 100% clean in alloc/free.

--- @iData/get.m
+++ @iData/get.m
@@ -13,7 +13,7 @@ function [varargout] = get(this,varargin)
 % output: property: property value in 's' (cell)
 % ex :    get(iData) or get(iData,'Title')
 %
-% Version: $Revision: 1.29 $
+% Version: $Revision: 1.30 $
 % See also iData, iData/set, iData/getalias, iData/getaxis, iData/findobj
 
 % EF 27/07/00 creation
@@ -63,3 +63,6 @@ for index=1:length(varargin)
   end
 end
 
+if isempty(varargout)
+  varargout={[]};
+end
