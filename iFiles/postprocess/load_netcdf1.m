0f9ae7285f879a894a4f67d7878aacaa223c365f
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2011-12-14 12:44:57 +0000

    iData/get: allocate empty return argument when nothing happened. Was left undefined. GUI/mcstas: display current monitors taking part in the criteria at each plot refresh. iFiles/netcdf1: redefine aliases only when new data is larger. iFiles/texmex: do not free memory blocks, as this creates segv. looktxt is 100% clean in alloc/free.

--- iFiles/postprocess/load_netcdf1.m
+++ iFiles/postprocess/load_netcdf1.m
@@ -3,7 +3,7 @@ function b=load_netcdf1(a)
 %
 % Returns an iData style dataset from a NetCDF 1.0 file
 %
-% Version: $Revision: 1.2 $
+% Version: $Revision: 1.3 $
 % See also: iData/load, iLoad, save, iData/saveas
 
 % handle input iData arrays
@@ -12,12 +12,6 @@ if length(a(:)) > 1
     a(index) = feval(mfilename, a(index));
   end
   return
-end
-
-for index=1:length(a.Data.VarArray)
-  this = a.Data.VarArray(index);
-  name = this.Str(sort([find(isstrprop(this.Str,'alphanum')) find(this.Str == '_')]));
-  setalias(a, genvarname(strtrim(name)), [ 'Data.VarArray(' num2str(index) ').Data' ]);
 end
 
 for index=1:length(a.Data.AttArray)
@@ -30,6 +24,16 @@ for index=1:length(a.Data.DimArray)
   this = a.Data.DimArray(index);
   name = this.Str(sort([find(isstrprop(this.Str,'alphanum')) find(this.Str == '_')]));
   setalias(a, genvarname(strtrim(name)), [ 'Data.DimArray(' num2str(index) ').Dim' ]);
+end
+
+for index=1:length(a.Data.VarArray)
+  this = a.Data.VarArray(index);
+  name = this.Str(sort([find(isstrprop(this.Str,'alphanum')) find(this.Str == '_')]));
+  name = genvarname(strtrim(name));
+  if isempty(getalias(a, name)) || ...
+    (~isempty(getalias(a, name)) && numel(this.Data) > numel(get(a, name)))
+    setalias(a, name, [ 'Data.VarArray(' num2str(index) ').Data' ]);
+  end
 end
 
 b = a;
