980d0043885407d743c6a554e5a39c027933cd7e
pkwi <pkwi@fysik.dtu.dk>
2012-01-17 13:54:46 +0000

    Added support for Analyze volume data, frequently used in medical imaging. Load-functions provided by Claus Svarer <csvarer@nru.dk>.
    Example:
      a=iData('/path/to/iFit/cyno_atlas.4dint.hdr');
      slice(a)
    
    Write of volumes pending

--- iFiles/iLoad_ini.m
+++ iFiles/iLoad_ini.m
@@ -139,9 +139,16 @@ function config = iLoad_ini
     PDB.method          ='PDBRead';
     PDB.patterns        ={'HEADER','TITLE','COMPND','SOURCE','EXPDTA','AUTHOR','REVDAT','SEQRES','MASTER','END'};
     
+    Analyze.name       ='Analyze volume data';
+    Analyze.options    ='';
+    Analyze.method     ='ReadAnalyze';
+    Analyze.postprocess='load_analyze';
+    Analyze.extension  ='hdr';
+    
+    
 % definition of configuration
     config.loaders =  { ILL_normal, ILL_integers, ILL_float, ILL_general, ILL_TAS_pol, ILL_TAS, ...
-	       spec, mcstas_scan, mcstas_2D, mcstas_1D, mcstas_sim, mcstas_sqw, ISIS_spe, ILL_inx, ESRF_edf, Matlab_FIG, PDB };
+	       spec, mcstas_scan, mcstas_2D, mcstas_1D, mcstas_sim, mcstas_sqw, ISIS_spe, ILL_inx, ESRF_edf, Matlab_FIG, PDB, Analyze};
 	       
 	  config.UseSystemDialogs = 'yes'; % no: use uigetfiles, else defaults to 'uigetfile'
 	  config.FileName         = [ mfilename ' (default configuration from ' which(mfilename) ')' ];
