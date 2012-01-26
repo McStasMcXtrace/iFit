1a43856d034221f7183ee4fc1639d68ca2d41ffa
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2012-01-26 11:27:57 +0000

    iFuncs: added sigmoid which is about the same as doserep.

---
+++ iFuncs/sigmoid.m
@@ -0,0 +1,104 @@
+function y=sigmoid(p, x, y)
+% y = sigmoid(p, x, [y]) : Sigmoidal
+%
+%   iFunc/sigmoid Sigmoidal S-shaped
+%     y  = A0+(A1-A0)./(1+exp(-(x-x0)/w))
+%   This is a sigmoid S-shaped curve, aka logistic.
+%   The function called with a char argument performs specific actions.
+%   You may create new fit functions with the 'ifitmakefunc' tool.
+%
+% input:  p: Sigmoidal model parameters (double)
+%            p = [ Amplitude Center Width BackGround ]
+%          or action e.g. 'identify', 'guess', 'plot' (char)
+%         x: axis (double)
+%         y: when values are given, a guess of the parameters is performed (double)
+% output: y: model value or information structure (guess, identify)
+% ex:     y=sigmoid([1 0 1 1], -10:10); or y=sigmoid('identify') or p=sigmoid('guess',x,y);
+%
+% Version: $Revision: 1.1 $
+% See also iData, ifitmakefunc
+
+% 1D function template:
+% Please retain the function definition structure as defined below
+% in most cases, just fill-in the information when HERE is indicated
+
+  if nargin >= 2 && isnumeric(p) && ~isempty(p) && isnumeric(x) && ~isempty(x)
+  %   evaluate: model(p,x, ...)
+    y = evaluate(p, x);
+  elseif nargin == 3 && isnumeric(x) && isnumeric(y) && ~isempty(x) && ~isempty(y)
+  %   guess: model('guess', x,y)
+  %   guess: model(p,       x,y)
+    y = guess(x,y);
+  elseif nargin == 2 && isnumeric(p) && isnumeric(x) && numel(p) == numel(x)
+  %   guess: model(x,y) with numel(x)==numel(y)
+    y = guess(p,x);
+  elseif nargin == 2 && isnumeric(p) && ~isempty(p) && isempty(x)
+  %   evaluate: model(p,[])
+    y = feval(mfilename, p);
+  elseif nargin == 2 && isempty(p) && isnumeric(x) && ~isempty(x)
+  %   identify: model([],x)
+    y = identify; x=x(:);
+    % HERE default parameters when only axes are given <<<<<<<<<<<<<<<<<<<<<<<<<
+    y.Guess  = [1 mean(x) std(x)/3 .1];
+    y.Axes   = { x };
+    y.Values = evaluate(y.Guess, y.Axes{:});
+  elseif nargin == 1 && isnumeric(p) && ~isempty(p) 
+  %   identify: model(p)
+    y = identify;
+    y.Guess  = p;
+    % HERE default axes to represent the model when parameters are given <<<<<<<
+    y.Axes   =  { linspace(p(2)-3*p(3),p(2)+3*p(3), 100) };
+    y.Values = evaluate(y.Guess, y.Axes{:});
+  elseif nargin == 1 && ischar(p) && strcmp(p, 'plot') % only works for 1D
+    y = feval(mfilename, [], linspace(0,2, 100));
+    if y.Dimension == 1
+      plot(y.Axes{1}, y.Values);
+    elseif y.Dimension == 2
+      surf(y.Axes{1}, y.Axes{2}, y.Values);
+    end
+    title(mfilename);
+  elseif nargin == 0
+    y = feval(mfilename, [], linspace(0,2, 100));
+  else
+    y = identify;
+  end
+
+end
+% end of model main
+% ------------------------------------------------------------------------------
+
+% inline: evaluate: compute the model values
+function y = evaluate(p, x)
+  sx = size(x); x=x(:);
+  if isempty(x) | isempty(p), y=[]; return; end
+  
+  % HERE is the model evaluation <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  y  = p(4)+p(1)./(1+exp(-(x-p(2))/p(3)));
+  
+  y = reshape(y, sx);
+end
+
+% inline: identify: return a structure which identifies the model
+function y =identify()
+  % HERE are the parameter names
+  parameter_names = {'Amplitude','Center','Width','Background'};
+  %
+  y.Type           = 'iFit fitting function';
+  y.Name           = [ 'Sigmoidal curve (1D) [' mfilename ']' ];
+  y.Parameters     = parameter_names;
+  y.Dimension      = 1;         % dimensionality of input space (axes) and result
+  y.Guess          = [];        % default parameters
+  y.Axes           = {};        % the axes used to get the values
+  y.Values         = [];        % default model values=f(p)
+  y.function       = mfilename;
+end
+
+% inline: guess: guess some starting parameter values and return a structure
+function info=guess(x,y)
+  info       = identify;  % create identification structure
+  info.Axes  = { x };
+  % fill guessed information
+  info.Guess = [ max(y)-min(y) mean(x) std(x)/3 min(y) ];
+  info.Values= evaluate(info.Guess, info.Axes{:});
+end
+
