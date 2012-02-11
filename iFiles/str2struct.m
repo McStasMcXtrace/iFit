41dea8075c288085c1aee49c8caca51533304351
farhi <farhi@ill.fr>
2012-02-11 02:27:25 +0000

    Docs: Fit/iOptim/McStas: updated documentation regarding parameters and options given as strings. Monitors can be given as expressions.

--- iFiles/str2struct.m
+++ iFiles/str2struct.m
@@ -1,6 +1,8 @@
 function s=str2struct(string)
 % s=str2struct(string) Create a structure from string lines
 %   This function creates a structure from string containing <name> <value> pairs
+%   Structure member must be separated by ';' or end-of-lines.
+%   The member assignation can be specified with spaces, '=' and ':'
 %
 % input arguments:
 %   string: string from which the structure should be extracted
@@ -8,12 +10,12 @@ function s=str2struct(string)
 % output variables:
 %   s: structure which contains the named values
 %
-% example: str=str2struct('Temperature: 200');
+% example: str=str2struct('Temperature: 200; RV=3; comment something nice');
 %          
 % See also: mat2str, num2str, eval, sprintf, class2str
 %
 % Part of: iFiles utilities (ILL library)
-% Author:  E. Farhi <farhi@ill.fr>. $Revision: 1.2 $
+% Author:  E. Farhi <farhi@ill.fr>. $Revision: 1.3 $
 
 s={};
 if nargin ==0, return; end
@@ -41,7 +43,7 @@ for index=1:numel(cellstring)
   [name, line] = strtok(this, sprintf('=: \t'));
   if isempty(name), continue; end
   if name(1)=='#' || name(1)=='%' || strncmp(name, '//', 2) || name(1) == '!'
-    continue; % skipp comment lines
+    continue; % skip comment lines
   end
   nextline = min(find(isstrprop(line, 'alphanum')));
   startline=line(1:nextline);
@@ -53,7 +55,12 @@ for index=1:numel(cellstring)
   name = strrep(name, '.', '_');
   name = strrep(name, '-', '_');
   name = genvarname(name);
-  if isempty(value), value=comment; end
+  if isempty(value), 
+      value=comment;
+      if ~isempty(str2num(value))
+          value=str2num(value);
+      end
+  end
   if ~isempty(value), s.(name) = value; end
 end
 
