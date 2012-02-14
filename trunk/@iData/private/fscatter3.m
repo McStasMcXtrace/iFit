b06a6df83689a5b37f58ae0d0d241c75d1d450c4
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2012-02-14 22:38:10 +0000

    iData: updated scatter3 method to allow additional argument 'filled' and 'bubble'

--- trunk/@iData/private/fscatter3.m
+++ trunk/@iData/private/fscatter3.m
@@ -13,15 +13,25 @@ if nargin == 3
   C = Z;
 end
 if nargin <= 4
-  numclass = 256; % Number of color classes
+  cmap = [];
+end
+filled = 0;
+if strfind(cmap,'filled')
+  cmap=[];
+  filled = 1;
+elseif strfind(cmap,'bubble')
+  cmap=[];
+  filled = 2;
+elseif ischar(cmap)
+  cmap='';
+end
+if isempty(cmap)
   cmap = hsv(256);
 end
-if nargin == 5
-  numclass = max(size(cmap));
-  if numclass == 1
-    cmap = hsv(256);
-    numclass = 256;
-  end  
+numclass = max(size(cmap));
+if numclass == 1
+  cmap = hsv(256);
+  numclass = 256;
 end
 
 % avoid too many calculations
@@ -46,15 +56,25 @@ colormap(cmap);
 
 % plot each color class in a loop
 
-marker = max(4, 10-log10(length(X(:))));
+if ~filled
+  marker = max(2, 10-log10(length(X(:))));
+end
 
 k = 0;
 for j = 1:numclass
   jj = find(ii == j);
   if ~isempty(jj)
     k = k + 1;
-    h(k) = plot3(X(jj),Y(jj),Z(jj),'.','color',col(j,:), ...
+    if filled
+      marker = ceil(20*sqrt((C(jj(1))-mins)/(maxs-mins))+2);
+    end
+    if filled == 2
+      h(k) = plot3(X(jj),Y(jj),Z(jj),'o','color',col(j,:), ...
+		 'markersize',marker/2);
+	  else
+      h(k) = plot3(X(jj),Y(jj),Z(jj),'.','color',col(j,:), ...
 		 'markersize',marker);
+	  end
   end  
 end
 hold off
