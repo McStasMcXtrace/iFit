69e780dc91c4a2994f1a99fdfc12ea9993203ac0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2012-03-06 15:38:33 +0000

    Doc/iData: addedhyperlinks in matlab disp/display. Fix plot with iData vector.

--- @iData/doc.m
+++ @iData/doc.m
@@ -3,13 +3,13 @@ function d = doc(a)
 %
 %   @iData/doc: web page documentation
 %
-% Version: $Revision: 1.2 $
+% Version: $Revision: 1.3 $
 
 % EF 23/10/10 iData impementation
 
 d = [ fileparts(which('iData/version')) filesep '..' filesep 'Docs' filesep 'index.html' ];
 disp(version(iData))
 disp('Opening iData documentation from ')
-disp(d)
+disp([ '  <a href="matlab:web ' d '">web ' d '</a>' ])
 web(d);
 
