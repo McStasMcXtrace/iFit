d4b514cbad8c667ba623b610c44bb8aa1704b725
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2012-03-20 13:19:02 +0000

    iFiles/iData load/save: support for STL geometry files. Will add OFF and PLY.

--- trunk/iFiles/postprocess/load_netcdf1.m
+++ trunk/iFiles/postprocess/load_netcdf1.m
@@ -3,7 +3,7 @@ function b=load_netcdf1(a)
 %
 % Returns an iData style dataset from a NetCDF 1.0 file
 %
-% Version: $Revision: 1.4 $
+% Version: $Revision: 1.5 $
 % See also: iData/load, iLoad, save, iData/saveas
 
 % handle input iData arrays
@@ -11,6 +11,11 @@ if length(a(:)) > 1
   for index=1:length(a(:))
     a(index) = feval(mfilename, a(index));
   end
+  return
+end
+
+if ~isfield(a.Data,'VarArray') ||~isfield(a.Data,'AttArray') || ~isfield(a.Data,'DimArray') 
+  warning([ mfilename ': The loaded data set ' a.Tag ' from ' a.Source ' is not a NetCDF1 data format.' ]); 
   return
 end
 
