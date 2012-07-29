a2e15e19f6fbc339c00046b6c491a0be5ee8dcef
farhi <farhi@ill.fr>
2012-07-29 21:15:11 +0000

    iFit: standalone: prepare debian package creation

--- Applications/GUI/standalone/make.sh
+++ Applications/GUI/standalone/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # create standalone ifit distribution
 #
-# usage: make.sh <<target>
+# usage: make.sh <target>
 
 echo usage: make.sh target
 
@@ -11,10 +11,11 @@ export m=$PWD
 export p=`realpath $PWD/../../..`
 mv edit.m.org edit.m
 mv web.m.org  web.m
-cd $*
+mkdir -p $1
+cd $1
 echo Creating the stand-alone version 
 echo from $p 
-echo into $* 
+echo into $1
 mcc -m ifit -a $p
 mv ifit run_ifit
 rm run_ifit.sh
