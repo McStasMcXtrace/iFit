7958c7b94156c4f8e3f14132acb1c14c78ec44c4
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2012-08-07 14:13:09 +0000

    iFit: final release stuff for 1.2. Building the packages...

--- Applications/standalone/make.sh
+++ Applications/standalone/make.sh
@@ -3,15 +3,18 @@
 #
 # usage: make.sh <target>
 
-echo usage: make.sh target
+# requires packages: realpath, Matlab Compiler
 
 # location of the make script
 export m=$PWD
 # location of the iFit directory
-export p=`realpath $PWD/../../..`
+export p=`realpath $PWD/../..`
+
+# create the help pages
+matlab -nojvm -nosplash -r "addpath(genpath('$p')); help_create; exit"
+
 mv edit.m.org edit.m
 mv web.m.org  web.m
-mv help.m.org help.m
 mkdir -p $1
 cd $1
 echo Creating the stand-alone version 
@@ -26,4 +29,3 @@ cp $p/COPYING .
 cd $m
 mv edit.m edit.m.org
 mv web.m  web.m.org
-mv help.m help.m.org
