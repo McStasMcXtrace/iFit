9cef4b214a4da59217bc5b6451496e0a0819acf1
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2012-08-07 11:39:58 +0000

    Move standalone to Apps

---
+++ Applications/standalone/make.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# create standalone ifit distribution
+#
+# usage: make.sh <target>
+
+echo usage: make.sh target
+
+# location of the make script
+export m=$PWD
+# location of the iFit directory
+export p=`realpath $PWD/../../..`
+mv edit.m.org edit.m
+mv web.m.org  web.m
+mv help.m.org help.m
+mkdir -p $1
+cd $1
+echo Creating the stand-alone version 
+echo from $p 
+echo into $1
+mcc -m ifit -a $p
+mv ifit run_ifit
+rm run_ifit.sh
+cp $m/ifit .
+cp $p/README.txt .
+cp $p/COPYING .
+cd $m
+mv edit.m edit.m.org
+mv web.m  web.m.org
+mv help.m help.m.org
