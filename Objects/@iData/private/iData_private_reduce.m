5503431d48b0f47780aecac7c5be17aac094d9f0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2012-09-04 20:17:08 +0000

    iData: prepare reducevolume

--- Objects/@iData/private/iData_private_reduce.m
+++ Objects/@iData/private/iData_private_reduce.m
@@ -1,37 +1,81 @@
-function a=iData_private_reduce(a, max_length)
-% rebinning object so that its number of elements is smaller than 1e6
+function s = reducevolume(a, R)
+% h = reducevolume(s, factor) : reduce an iData object size
+%
+% B = REDUCEVOLUME(A, [Rx Ry Rz ... ]) reduces the number
+%    of elements in the object by only keeping every Rx element in the x
+%    direction, every Ry element in the y direction, and every Rz element
+%    in the z direction and so on. 
+% B = REDUCEVOLUME(A, R)
+%    If a scalar R is used to indicate the amount or
+%    reduction instead of a vector, the reduction is assumed to
+%    be R on all axes. 
+%
+% B = REDUCEVOLUME(A)
+%    When omitted, the volume/size reduction is performed on bigger axes until
+%    the final object contains less than 1e6 elements.
+%
+% Version: $Revision: 1.0 $
+% See also iData, reducevolume,plot, iData/plot, iData/size
+
+% rebinning object so that its number of elements is smaller 
 
 if nargin == 1
-  % how many axes are 'big' ? if more than one, set max_length to 100
-  big_axes   = 0;
-  max_length = 1000;
-  for index=1:ndims(a)
-    if length(getaxis(a, index)) > max_length
-      big_axes = big_axes +1;
-    end
-  end
-  if big_axes > 1, max_length = 100;
-  else             max_length = 1000;
-  end
+  R = []; % will guess to reduce down to 1e6
 end
 
-S.type='()';
-S.subs=cell(1,ndims(a));
+% handle input iData arrays
+if numel(a) > 1
+  s = [];
+  for index=1:numel(a)
+    s = [ s feval(mfilename, a(index), R) ];
+  end
+  s = reshape(s, size(a));
+  return
+end
 
-% scan dimensions and reduce them to max_length elements per dimension
-for index=1:ndims(a)
-  lx = length(getaxis(a, index));
-  if lx > max_length
-    S.subs{index}=ceil(linspace(1,lx,max_length));
+% determine best reduction factor
+if ischar(R) || isempty(R)
+  S  = size(a);
+  R  = ones(size(S));
+  S0 = S;
+  
+  % loop until we get les than 1e6 elements
+  while prod(S) > 1e6
+    % identify the biggest axis, and reduce it by increasing R
+    for index=1:length(R)
+      [dummy, j] = sort(S);
+      % S(j(end)) is the biggest element
+      R(j(end)) = R(j(end))+1;
+      S = S0 ./ R;
+    end
+  end
+ 
+end
+
+% case of event data sets
+if isvector(a)
+  if length(R) > 1, R = max(R); end
+elseif isnumeric(R) && length(R) == 1
+  % R is scalar and object is not vectorial
+  R = R*ones(1, ndims(a));
+end
+
+s = copyobj(a);
+S = [];
+S.type='()';
+S.subs=cell(1,length(R));
+
+% scan dimensions and rebin them
+for index=1:length(R)
+  lx = length(getaxis(s, index));
+  if R(index) > 1
+    S.subs{index}=ceil(1:R(index):lx);
   else
     S.subs{index} = ':';
   end
 end
 
 % perform rebinning
-a = subsref(a, S);
+s = subsref(s, S);
 
-if nargout == 0 && ~isempty(inputname(1))
-  assignin('caller',inputname(1),a);
-end
 
