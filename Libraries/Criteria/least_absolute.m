f3111fc7e8a824dc327962452d708dc567b0a333
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2013-01-07 16:34:56 +0000

    Criteria: added max Likelihood estimator. Updated Docs.

--- Libraries/Criteria/least_absolute.m
+++ Libraries/Criteria/least_absolute.m
@@ -1,9 +1,12 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% <http://en.wikipedia.org/wiki/Least_absolute_deviation>
 function c=least_absolute(Signal, Error, Model)
 % weighted least absolute criteria
 % the return value is a vector, and most optimizers use its sum (except LM).
 % |Signal-Model|/Error
+%
+% A good fit corresponds with a criteria lower or equal to 1.
+%
+% <http://en.wikipedia.org/wiki/Least_absolute_deviation>
+
   if isempty(Error) || isscalar(Error) || all(Error == Error(end))
     index = find(isfinite(Model) & isfinite(Signal));
     c = abs(Signal(index)-Model(index)); % raw least absolute
--- Libraries/Criteria/least_max.m
+++ Libraries/Criteria/least_max.m
@@ -1,8 +1,10 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% <http://en.wikipedia.org/wiki/Absolute_deviation>
 function c=least_max(Signal, Error, Model)
 % weighted median absolute criteria
 % the return value is a scalar
 % median(|Signal-Model|/Error)
+%
+% A good fit corresponds with a criteria lower or equal to 1.
+%
+% <http://en.wikipedia.org/wiki/Absolute_deviation>
   c = max(least_absolute(Signal, Error, Model));
 end % least_max
