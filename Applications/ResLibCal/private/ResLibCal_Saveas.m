87d0b89c1c041f8d357736bbd39f15e4cde4a904
farhi <farhi@ill.fr>
2013-02-20 18:26:15 +0000

    Apps: adding ResLibCal for TAS resolution function computation.

---
+++ Applications/ResLibCal/private/ResLibCal_Saveas.m
@@ -0,0 +1,44 @@
+function filename = ResLibCal_Saveas(filename, EXP)
+% ResLibCal_Saveas(filename): save the GUI configuration to an EXP/ResLib file
+%
+% Return:
+%  filename: name of file written
+
+% Calls: ResLibCal_Compute, class2str
+
+  if nargin < 1, filename =''; end
+  if nargin < 2, EXP=''; end
+
+  if isempty(filename)
+    [filename, pathname] = uiputfile( ...
+         {'*.m',  'M-files (*.m)'; ...
+          '*.ini','ResLibCal configuration (*.ini)' ; ...
+          '*.*',  'All Files (*.*)'}, ...
+          'Save ResLibCal configuration as ...');
+    if isempty(filename) || all(filename == 0), return; end
+    filename = fullfile(pathname, filename);
+  end
+
+  if isempty(EXP)
+    EXP = ResLibCal_Compute; % collect current settings and compute
+  end
+  if ~isempty(EXP)
+    str = [ '% ResLibCal configuration script file ' sprintf('\n') ...
+            '%' sprintf('\n') ...
+            '% Matlab ' version ' m-file ' filename sprintf('\n') ...
+            '% generated automatically on ' datestr(now) ' with ' mfilename sprintf('\n') ...
+            '% The Resolution function is indicated as the "resolution.RMS" field ' sprintf('\n') ...
+            '% (in lattice rlu), "resolution.RM" is in [Angs-1] in [QxQyQzE].' sprintf('\n') ...
+            '% Modify "config" rather than "config.ResCal".' sprintf('\n') ...
+            class2str('config', EXP) ];
+    [fid, message]=fopen(filename,'w+');
+    if fid == -1
+      warning(['Error opening file ' filename ' to save ResLibCal configuration.' ]);
+      filename = [];
+    else
+      fprintf(fid, '%s', str);
+      fclose(fid);
+      disp([ '% Saved ResLibCal configuration into ' filename ]);
+    end
+  end
+% end ResLibCal_Saveas
