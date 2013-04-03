c990fb5d557184e7e1103490f7f6231cdda53632
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-04-03 12:47:11 +0000

    scripts/load: load_vitess_2d: make use of findfield('numeric')

--- trunk/Scripts/load/load_vitess_2d.m
+++ trunk/Scripts/load/load_vitess_2d.m
@@ -2,6 +2,8 @@ function a=load_vitess_2d(a)
 % function a=load_vitess_2d(a)
 %
 % Returns an iData style dataset from a VITESS 2d monitor file
+% These files have 1st row and 1st column defining the axes.
+% The element (1.1) is missing (row 1 has length shorter by 1 ele
 %
 % Version: $Revision$
 % See also: iData/load, iLoad, save, iData/saveas
@@ -25,16 +27,15 @@ a=iData(a);
 % * at least 2 numerical blocks, and we use the last two
 % * a first block with one column less that second block
 % * first block has one or 2 rows
-[match, types, nelements] = findfield(a);
-index = strmatch('double',types);
-if length(index) < 2, return; end
+[match, types, nelements] = findfield(a,'','numeric');
+if length(match) < 2, return; end
 
-f1 = get(a,match{index(end-1)});
-f2 = get(a,match{index(end)});
+f1 = get(a,match{2});
+f2 = get(a,match{1});
 
 if size(f1,2) ~= size(f2,2) -1, return; end % 2nd block has more columns that 1st
 if size(f1,1) > 2,              return; end % more than 2 lines
 
-a{0}=[ match{index(end)} '(:,2:end)' ];
-a{1}=[ match{index(end)} '(:,1)' ];
-a{2}=[ match{index(end-1)} '(1,:)' ];
+a{0}=[ match{1} '(:,2:end)' ];
+a{1}=[ match{1} '(:,1)' ];
+a{2}=[ match{2} ];
