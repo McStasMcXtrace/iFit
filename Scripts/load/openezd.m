d3687bd9585da1a74742f463d22b0d850daefabf
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2013-05-16 16:05:11 +0000

    Scripts: openXX opens a new figure before plotting.

--- Scripts/load/openezd.m
+++ Scripts/load/openezd.m
@@ -3,7 +3,7 @@ function out = openezd(filename)
 %        and set the 'ans' variable to an iData object with its content
 
 out = load(iData,filename, 'PLY');
-subplot(out);
+figure; subplot(out);
 
 if ~isdeployed
   assignin('base','ans',out);
