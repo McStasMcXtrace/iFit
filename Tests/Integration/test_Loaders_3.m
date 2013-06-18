a08d19ec850708a248254c848bcc570f93e3f636
farhi <farhi@ill.fr>
2013-06-18 12:49:00 +0000

    Tests: reorder tests

--- Tests/Integration/test_Loaders_3.m
+++
@@ -1,7 +0,0 @@
-function result = test_Loaders_3
-  a = iData(rand(10));
-  a = iData(struct('a',1,'b','a string'));
-  a = findobj(iData);
-  f = figure; peaks;
-  a = iData(f);
-  result = 'OK  iData(peaks); iData(gcf)';
