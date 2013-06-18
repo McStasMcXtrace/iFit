2f598a911588837ebe2b0a2c84c121b39e0b2e5d
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2013-06-18 12:49:00 +0000

    Tests: reorder tests

--- Tests/Integration/test_Fit_6_limits.m
+++
@@ -1,8 +0,0 @@
-function result = test_Fit_6_limits
-  a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
-  p=fits(a, 'gauss', [], 'fminimfil', [ 0.5 0.8 -1 0 ], [ 1 1.2 1 1 ]);
-  if abs(max(abs([ 0.62         1.0008      0.0035         0.0001 ])-abs(p))) < 0.01
-    result = 'OK  fits(a, ''gauss'', [], ''fminralg'', [ 0.5 0.8 0 0 ], [ 1 1.2 1 1 ]);';
-  else
-    result = 'FAILED';
-  end 
--- Tests/Integration/test_iData_1.m
+++
@@ -1,7 +0,0 @@
-function result = test_iData_1
-  a = iData([ ifitpath 'Data/ILL_IN6.dat']);
-  get(a);
-  a = iData(rand(10));
-  if ndims(a) == 2
-    result = 'OK  iData([ ifitpath ''Data/ILL_IN6.dat'']);';
-  else result = 'FAILED'; end
