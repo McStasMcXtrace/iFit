72f828b25ebea4185c5465e05824c3699c59a597
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2013-06-18 12:49:28 +0000

    Tests: reorder tests

---
+++ Tests/Unit/Objects/iData/test_iData_load_vars.m
@@ -0,0 +1,7 @@
+function result = test_Loaders_3
+  a = iData(rand(10));
+  a = iData(struct('a',1,'b','a string'));
+  a = findobj(iData);
+  f = figure; peaks;
+  a = iData(f);
+  result = 'OK  iData(peaks); iData(gcf)';
---
+++ Tests/Unit/Objects/iFunc/test_Fit_1.m
@@ -0,0 +1,8 @@
+function result = test_Fit_1
+  a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
+  p=fits(a,'','','fminimfil');
+  if abs(max(abs([ 0.61         1.0008      0.0035         0.0001 ])-abs(p))) < 0.01
+    result = 'OK  fits(a)';
+  else
+    result = 'FAILED';
+  end 
