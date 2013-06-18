977c3234943ce1281a256a8a40340b83113f909e
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-06-18 12:49:00 +0000

    Tests: reorder tests

--- trunk/Tests/Integration/test_Fit_5_fix.m
+++
@@ -1,9 +0,0 @@
-function result = test_Fit_5_fix
-  a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
-  p=fits(a, 'gauss', [], 'fminralg', [ 1 0 0 0 ]);
-  % p= 0.5936    1.0008   -0.0037    0.0002
-  if abs(max(abs([ 0.5936         1.0008      0.0035         0.0002 ])-abs(p))) < 0.01
-    result = 'OK  fits(a, ''gauss'', [], ''fminralg'', [ 1 0 0 0 ])';
-  else
-    result = 'FAILED';
-  end 
