0e1a195cc042c47a527d82154b72b5dab63bf1a1
farhi <farhi@ill.fr>
2013-06-18 13:27:43 +0000

    Tests: reorder iFunc/Models tests

--- Tests/Unit/Objects/iFunc/test_iFunc_fits.m
+++ Tests/Unit/Objects/iFunc/test_iFunc_fits.m
@@ -1,8 +1,10 @@
-function result = test_Fit_1
+function result = test_iFunc_fits
+
   a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
   p=fits(a,'','','fminimfil');
+  
   if abs(max(abs([ 0.61         1.0008      0.0035         0.0001 ])-abs(p))) < 0.01
-    result = 'OK  fits(a)';
+    result = [ 'OK     ' mfilename ];
   else
-    result = 'FAILED';
+    result = [ 'FAILED ' mfilename ];
   end 
--- Tests/Unit/Objects/iFunc/test_iFunc_fits_constraints.m
+++ Tests/Unit/Objects/iFunc/test_iFunc_fits_constraints.m
@@ -1,8 +1,10 @@
-function result = test_Fit_6_limits
+function result = test_iFunc_fits_constraints
+
   a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
   p=fits(a, 'gauss', [], 'fminimfil', [ 0.5 0.8 -1 0 ], [ 1 1.2 1 1 ]);
+  
   if abs(max(abs([ 0.62         1.0008      0.0035         0.0001 ])-abs(p))) < 0.01
-    result = 'OK  fits(a, ''gauss'', [], ''fminralg'', [ 0.5 0.8 0 0 ], [ 1 1.2 1 1 ]);';
+    result = [ 'OK     ' mfilename ];
   else
-    result = 'FAILED';
+    result = [ 'FAILED ' mfilename ];
   end 
