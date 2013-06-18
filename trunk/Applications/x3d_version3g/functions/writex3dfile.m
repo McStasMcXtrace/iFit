bc3bbc2045263ace77e469736ee13b3c21734390
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-06-18 10:18:49 +0000

    removed bugs after unit testing

--- trunk/Applications/x3d_version3g/functions/writex3dfile.m
+++ trunk/Applications/x3d_version3g/functions/writex3dfile.m
@@ -4,6 +4,10 @@ function writex3dfile(folder, filename, data)
 fid = fopen([folder filename '.x3d'],'w');
 fprintf(fid,'<?xml version="1.0" encoding="UTF-8"?>\n');
 fprintf(fid,'<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.1//EN" "http://www.web3d.org/specifications/x3d-3.1.dtd">\n');
-fprintf(fid,str);
+if iscell(str)
+  fprintf(fid,'%s', str{:});
+else
+  fprintf(fid,'%s', str);
+end
 fclose(fid);
 
