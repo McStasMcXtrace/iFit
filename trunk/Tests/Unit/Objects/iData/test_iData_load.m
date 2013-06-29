1a2d243aab4fca34619ac8dc47f4eed3bf542350
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-06-29 12:17:56 +0000

    Tests: new tests for iFunc, fixed some for iData. Fixed bugs after Tests/iFunc

--- trunk/Tests/Unit/Objects/iData/test_iData_load.m
+++ trunk/Tests/Unit/Objects/iData/test_iData_load.m
@@ -1,5 +1,9 @@
 function result=test_iData_load
 
+  tic;
+  a = iData([ ifitpath 'Data' ]); % also tests post-loaders scripts
+  toc
+  
   a=iData([ ifitpath 'Data/30dor.fits' ]);
   b=iData([ ifitpath 'Data/Diff_BananaTheta_1314088587.th' ]);
   
