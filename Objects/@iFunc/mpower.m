143484f62c5215ab1b2bb9dbe64f81134a8dd6c7
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2013-07-01 08:25:18 +0000

    Tests: added proper iFunc binary tests. Fixed revealed bugs.

--- Objects/@iFunc/mpower.m
+++ Objects/@iFunc/mpower.m
@@ -4,11 +4,13 @@ function c = mpower(a,b)
 %   @iFunc/mpower (^) function to compute the matrix-power of functions (orthogonal axes)
 %     when one of the argument is a character string, it is used as-is in the 
 %     operator expression. 
+%     when the second argument is an integer, the initial model is extended
+%     orthogonaly, creating a ndims(a)*b dimension model
 %
 % input:  a: object or array (iFunc or numeric)
 %         b: object or array (iFunc or numeric)
 % output: c: object or array (iFunc)
-% ex:     c=lorz^gauss;
+% ex:     c=lorz^gauss; c=gauss^3
 %
 % Version: $Revision$
 % See also iFunc, iFunc/minus, iFunc/plus, iFunc/times, iFunc/rdivide
