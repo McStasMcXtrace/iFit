ace64b940b6710ceaab3ce382b71a3a5779f4bd3
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-08-27 21:00:43 +0000

    iData: update kmeans (centroids)

--- trunk/Objects/@iData/kmeans.m
+++ trunk/Objects/@iData/kmeans.m
@@ -1,21 +1,17 @@
-function s = kmeans(a, k)
-% b = kmeans(X, k) : k-means clustering (segmentation into classes) of iData object
+function [s,c] = kmeans(a, k)
+% [b,c] = kmeans(X, k) : k-means clustering of iData object
 %
 %   @iData/kmeans function to partition the object X into k classes.
 %
 %   b = kmeans(a,k) partitions the points in the iData object X into k clusters.
 %   The resulting object Signal contains numbers from 1 to 'k' which are indices
-%   of the initial data segments.
-%
-%   For 1D and 2D objects, the Otsu method is used, for further dimensions, the
-%   legacy k-means is used (slower).
-%
-%   Otsu N., A threshold selection method from gray-level histogram, 
-%     IEEE Trans. Syst. Man Cybern. 9:62-66;1979 
+%   of segments/partitions.
+%   When no cluster can be found, the result is empty.
 %
 % input:  X: object or array (iData)
 %         k: number of partitions wanted (integer, default is 2)
 % output: b: object or array (iData)
+%         c: centroid locations of clusters
 % ex:     b=kmeans(a);
 %
 % Version: $Revision: 1035 $
@@ -27,21 +23,24 @@ end
 
 % handle input iData arrays
 if numel(a) > 1
-  s = zeros(iData, numel(a),1);
+  s = zeros(iData, size(a));
+  c = cell(size(a));
   parfor index=1:numel(a)
-    s(index) = feval(mfilename, a(index), k);
+    [ s(index), c{index} ] = feval(mfilename, a(index), k);
   end
-  s = reshape(s, size(a));
   return
 end
 
-% now call Otsu of fastCMeans depending on dimensionality
-X = subsref(a,struct('type','.','subs','Signal'));
-if ndims(a) <= 2
-  X = otsu(X, k);
-else
-  X = uint8(X/max(X(:))*2^8); % this is faster and requires much less memory that uint16
-  X = FastCMeans(X, k);
+s = []; c = [];
+
+% now call Otsu or FastCMeans depending on dimensionality
+S = subsref(a,struct('type','.','subs','Signal'));
+
+X = uint8(S/max(S(:))*2^8); % this is faster and requires much less memory that uint16
+X = FastCMeans(X, k);
+
+if all(X == 0)  % no cluster found
+  return
 end
 
 % create the final object
@@ -51,3 +50,14 @@ s=iData_private_history(s, mfilename, a, k);
 s=set(s, 'Signal', X, 'Error', 0);
 s=label(s, 0, 'Clusters/partitions');
 
+% compute centroids
+if nargout > 1 && any(X(:) > 0)
+  % removes warnings during interp
+  iData_private_warning('enter', mfilename);
+  for index=1:k
+    % use std method with background subtraction
+    [this_w, this_c] = std(a.*(X == index), -(1:ndims(a)));
+    c = [ c ; this_c ];
+  end
+  iData_private_warning('exit', mfilename);
+end
--- trunk/Tests/Unit/Objects/iData/test_iData_smooth.m
+++ trunk/Tests/Unit/Objects/iData/test_iData_smooth.m
@@ -1,6 +1,11 @@
 function result = test_iData_smooth
 
   a = iData([ ifitpath 'Data/Diff_BananaPSD_1314088587.th_y' ]);
+  a = a.*(1+0.1*randn(size(a)));
   b = smooth(a);
-
-  result = 1;
+  
+  if std(std(double(a-b))) < 0.03
+    result = 1;
+  else
+    result = 0;
+  end
