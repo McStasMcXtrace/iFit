25ec2fc161daf5b4945ff3ad30515791dd64e29b
farhi <farhi@5b86b127-4b86-4220-aadf-e89618e9c670>
2013-09-03 13:36:15 +0000

    iData: various fixes and upgrades for export to mantid, read_text, trapz

--- trunk/Tests/Unit/Objects/iData/test_iData_corrcoef.m
+++ trunk/Tests/Unit/Objects/iData/test_iData_corrcoef.m
@@ -2,7 +2,7 @@ function result=test_iData_corrcoef
 
 a = load(iData, [ ifitpath 'Data/sv1850.scn']);
 m = gauss;
-[p,dummy,dummy,o] = fits(a,m);
+[p,dummy,dummy,o] = fits(a,m, '', 'fminpowell');
 c = corrcoef(a,m);
 
 if abs(c(1,2) - o.corrcoef ) < 0.005
