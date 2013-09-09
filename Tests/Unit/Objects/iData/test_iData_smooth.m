325417b95bce51ffe55ddf48a1d8fd07e9d0d57e
farhi <farhi@ill.fr>
2013-09-09 22:16:23 +0000

    iData: smooth: added Savitsky-Golay method

--- Tests/Unit/Objects/iData/test_iData_smooth.m
+++ Tests/Unit/Objects/iData/test_iData_smooth.m
@@ -3,6 +3,7 @@ function result = test_iData_smooth
   a = iData([ ifitpath 'Data/Diff_BananaPSD_1314088587.th_y' ]);
   a = a.*(1+0.1*randn(size(a)));
   b = smooth(a);
+  c = smooth(a, 'sgolay');
   
   if std(std(double(a-b))) < 0.03
     result = 1;
