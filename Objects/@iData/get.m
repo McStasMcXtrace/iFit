f5094dd9acdd4aba9c3e5a587988b4385237b4a0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2014-05-22 15:17:21 +0000

    iData: comment out eval line (was set for testing in last commit)

--- Objects/@iData/get.m
+++ Objects/@iData/get.m
@@ -108,7 +108,7 @@ function this = get_eval(this, property)
           this = eval([ 'this.Data.' property ]);
       catch
           % disp([ 'Warning: failed evaluation of this.Data.' property ' in object ' this.Tag ' "' this.Title '".' ]);
-          %this = eval(property);              % this is a full expression: evaluate it...
+          this = eval(property);              % this is a full expression: evaluate it...
       end
   end
 
