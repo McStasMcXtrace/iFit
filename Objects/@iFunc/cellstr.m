048cf7bf5b94a8170904c7c06940ec2bef435c89
farhi <farhi@ill.fr>
2014-07-26 10:03:46 +0000

    iFunc: handle variable input args to iFunc models

--- Objects/@iFunc/cellstr.m
+++ Objects/@iFunc/cellstr.m
@@ -129,7 +129,12 @@ end
   
   % the Expression has to return a 'signal' value in the last line
   if isa(s.Expression ,'function_handle')
-    ret{end+1} = sprintf('signal = feval(%s, p, %s, varargin{:});', func2str(s.Expression), ax(1:(end-1)));
+    ax = [ 'p,' ax ];    % first arg should be 'p'g
+    if nargin(s.Expression) < 0
+        ret{end+1} = sprintf('signal = feval(%s, %s, varargin{:});', func2str(s.Expression), ax(1:(end-1)));
+    else
+        ret{end+1} = sprintf('signal = feval(%s, %s);', func2str(s.Expression), ax(1:(end-1)));
+    end
   else
     ret{end+1} = [ '% The Expression, computing signal from ''p'' and axes ' ax(1:(end-1)) ];
     e = s.Expression; 
