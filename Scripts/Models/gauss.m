5916946ff2fa89d20d6169e1ad7cc847dbdf025d
farhi <farhi@ill.fr>
2015-03-13 09:21:51 +0000

    updated iFunc model creators

--- Scripts/Models/gauss.m
+++ Scripts/Models/gauss.m
@@ -4,6 +4,9 @@ function y=gauss(varargin)
 %   iFunc/gauss Gaussian fitting function
 %     y = p(1)*exp(-0.5*((x-p(2))/p(3)).^2) + p(4);
 %
+% gauss(width)          creates a model with a specified width
+% gauss([ parameters ]) creates a model with specified model parameters
+%
 % input:  p: Gaussian model parameters (double)
 %            p = [ Amplitude Centre HalfWidth BackGround ]
 %          or 'guess'
@@ -16,9 +19,10 @@ function y=gauss(varargin)
 % See also iFunc, iFunc/fits, iFunc/plot
 
 y.Name      = [ 'Gaussian (1D) [' mfilename ']' ];
-y.Description='Single 1D Gaussian model';
+y.Description='1D Gaussian model';
 y.Parameters={'Amplitude','Centre','HalfWidth','Background'};
 y.Expression= @(p,x) p(1)*exp(-0.5*((x-p(2))/p(3)).^2) + p(4);
+
 % moments of distributions
 m1 = @(x,s) sum(s(:).*x(:))/sum(s(:));
 m2 = @(x,s) sqrt(abs( sum(x(:).*x(:).*s(:))/sum(s(:)) - m1(x,s).^2 ));
@@ -26,6 +30,13 @@ m2 = @(x,s) sqrt(abs( sum(x(:).*x(:).*s(:))/sum(s(:)) - m1(x,s).^2 ));
 y.Guess     = @(x,s) [ NaN m1(x, s-min(s(:))) m2(x, s-min(s(:))) NaN ];
 
 y = iFunc(y);
+
+%if nargin == 1 && isnumeric(varargin{1})
+  %if length(varargin{1}) == 1
+    %varargin = {[ 1 0 varargin{1} 0]};
+  %end
+  %y.ParameterValues = varargin{1};
+%end
 
 if length(varargin)
   y = y(varargin{:});
