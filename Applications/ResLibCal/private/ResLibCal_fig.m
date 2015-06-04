cef5169f78873ce9fb6bba43bc1593a1562e7c80
Emmanuel FARHI <farhi@ill.fr>
2015-06-04 21:02:26 +0200

    ResLibCal: finds handles quickly

---
+++ Applications/ResLibCal/private/ResLibCal_fig.m
@@ -0,0 +1,26 @@
+% ResLibCal_fig: finds current ResLibCal main GUI, as well as other
+%   controls.
+%   A cache is built when accessing the controls for the first time, and
+%   is re-used afterwards for faster access (e.g. Matlab >= R2014b)
+%
+%   ResLibCal_fig:      returns the main GUI handle
+%   ResLibCal_fig(tag): returns the 'Tag' specified
+function f=ResLibCal_fig(tag)
+
+  persistent fig handles
+
+  if ~ishandle(fig), fig=[]; end
+  if isempty(fig)
+    fig = findall(0, 'Tag','ResLibCal');
+    if length(fig) > 1, delete(fig(2:end)); end
+    handles = [];
+  end
+
+  if nargin == 0
+    f=fig;
+  else
+    if ~isfield(handles, tag) || ~ishandle(handles.(tag))
+      handles.(tag) = findobj(fig, 'Tag', tag);
+    end
+    f = handles.(tag);
+  end
