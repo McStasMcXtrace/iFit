9b874789820507565bb691bc71ff867f4cde52f4
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2015-08-02 11:28:19 +0200

    iData: 'fill' method more visible.

--- Objects/@iData/isinf.m
+++ Objects/@iData/isinf.m
@@ -4,6 +4,8 @@ function a = isinf(a)
 %   @iData/isinf function to return true for infinite elements
 %   of 's', i.e. that are +Inf or -Inf 
 %
+%   To remove nan's and inf's values use: fill(s)
+%
 % input:  s: object or array (iData)
 % output: b: object or array (iData)
 % ex:     b=isinf(a);
@@ -12,7 +14,6 @@ function a = isinf(a)
 % See also iData, iData/sign, iData/isreal, iData/isfinite, iData/isnan,
 %          iData/isinf, iData/isfloat, iData/isinterger,
 %          iData/isnumeric, iData/islogical, iData/isscalar, 
-%          iData/isvector, iData/issparse
-
+%          iData/isvector, iData/issparse, iData/fill
 a = iData_private_unary(a, 'isinf');
 
