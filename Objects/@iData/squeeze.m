a117dc1aa380859064b8ce6555c4aeef27ccebc2
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2015-08-23 19:00:48 +0200

    iData: swueeze also affects Monitor and Error. Moves scalar/unique axes to end

--- Objects/@iData/squeeze.m
+++ Objects/@iData/squeeze.m
@@ -14,11 +14,12 @@ function a = squeeze(a)
 % See also iData, iData/size, iData/resize, iData/permute, iData/reshape
 
 d = size(a);
-x = find(d <= 2);
 d = d(find(d > 1));
 d = [d ones(1,2-length(d))]; % Make sure siz is at least 2-D
 
 if numel(a) == 1
+  sz = isvector(a);
+  if ~sz, sz = length(size(a)); end
   % this is a single iData object
   if ndims(a) <= 2 && length(size(a)) ==2, return; end
   s = get(a,'Signal');
@@ -29,9 +30,37 @@ if numel(a) == 1
   if ~isequal(subsref(a,struct('type','.','subs','Signal')), sq)
     a = setalias(a, 'Signal', sq, [  'squeeze(' sl ')' ]);
   end
-  for index=1:length(x)
-    a=rmaxis(a, x(index));
+  % do the same on Error and Monitor
+  de=get(a,'Error'); 
+  if numel(de) > 1 && isnumeric(de) 
+    try % in case Error=sqrt(Signal), the Error is automatically changed when Signal is -> fail
+      d=squeeze(de); a=set(a,'Error', d); a = setalias(a, 'Error', d);
+    end
   end
+  clear de
+
+  dm=get(a,'Monitor');
+  if numel(dm) > 1 && isnumeric(dm)
+    d=squeeze(dm); a=set(a,'Monitor', d);  a = setalias(a, 'Monitor', d);
+  end
+  clear dm
+  
+  % move the un-used/scalar axes to the end (code from subsref)
+  
+  % check if the sub indices supress an axis: move it further
+  for index=1:(sz-1)
+    if index <= length(a.Alias.Axis)
+    index
+      % axes to be moved: scalar, or all unique
+      x = getaxis(a, index);
+      if isscalar(x) || isempty(find(x ~= x(1), 1))
+        % axis is constant, or scalar
+        a.Alias.Axis([index index+1])   = a.Alias.Axis([index+1 index]);
+        x = getaxis(a, index+1); a=setaxis(a, index+1,x(1));
+      end
+    end
+  end
+  
 else
   % this is an iData array
   a = reshape(a, d);
