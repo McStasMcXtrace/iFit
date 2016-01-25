00f86467201b3314af9624bf9aa103b60cbce714
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-01-25 12:05:41 +0100

    Load: better nMoldyn/MDanse support

--- Scripts/load/load_nmoldyn.m
+++ Scripts/load/load_nmoldyn.m
@@ -23,23 +23,36 @@ end
 
 % ======================================================================
 % searches for some 'known' nMoldyn symbols after import
-if isempty(findstr(a, 'nmoldyn')), return; end
-
-% 'jobinfo'
-s = findfield(a,{'GlobalAttributes.Value','jobinfo','header'});
-if ~isempty(s), a=setalias(a, 'jobinfo',s{1},'nMoldyn configuration');
-else return;  % not a nMoldyn stuff
+if isempty(findstr(a, 'nmoldyn')), 
+  return; 
 end
 
-% nMoldyn results
+% 'jobinfo'
+f = {'GlobalAttributes.Value','jobinfo','header'};
+s = findfield(a, f);
+for index=f
+  if isfield(a, index), s{end+1} = index; end
+end
+
+% add any other 'fields' as aliases
+if ~isempty(s), 
+  a=setalias(a, 'jobinfo',s{1},'nMoldyn configuration');
+else 
+  return;  % no nMoldyn JobInfo stuff
+end
+
+% nMoldyn results: last search defines the Signal and Axes: we prefer S(q,w)
 a = load_nmoldyn_signal_axes(a, 'pdf_total', 'r');
-a = load_nmoldyn_signal_axes(a, {'Sq_total','ssf_total'}, {'q','k'});
-a = load_nmoldyn_signal_axes(a, {'Sqw_total','s_q_f_total'}, 'frequency', {'q','k'});
 a = load_nmoldyn_signal_axes(a, 'msd_total', {'times','time'});
 a = load_nmoldyn_signal_axes(a, 'dos_total', 'frequency');
 a = load_nmoldyn_signal_axes(a, 'atomic_density', {'times','time'});
 a = load_nmoldyn_signal_axes(a, 'temperature', {'times','time'});
 a = load_nmoldyn_signal_axes(a, 'eisf_total', {'q','k'});
+a = load_nmoldyn_signal_axes(a, {'Sq_total','ssf_total'}, {'q','k'});
+a = load_nmoldyn_signal_axes(a, {'Fqt_total','f_q_t_total'},{'q','k'}, {'times','time'});
+a = load_nmoldyn_signal_axes(a, {'Sqw_total','s_q_f_total'}, {'q','k'}, 'frequency');
+
+% ==============================================================================
 
 function a = load_nmoldyn_signal_axes(a, signal, ax1, ax2)
   % locates items in signal, attach the first match to Signal
@@ -53,10 +66,7 @@ function a = load_nmoldyn_signal_axes(a, signal, ax1, ax2)
   [a,s] = load_nmoldyn_search_token(a, signal);
   if isempty(s), return; end
   a = setaxis(a, 0, s);  % assign 0-th axis=Signal to the alias found above
-  l = label(a, 0);
-  if isempty(l) || strcmp(l,'Data Signal')
-    label(a, 0, s);
-  end
+  label(a, 0, s);
 
   % assign 1st axis looking for member names in object
   [a,s] = load_nmoldyn_search_token(a, ax1);
