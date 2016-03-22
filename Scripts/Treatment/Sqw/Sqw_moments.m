6cc9d4931933df6bce6a1c55555f1490fda55ab7
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-03-22 17:37:00 +0100

    Treatment: Sqw: validate Sqw scripts.

--- Scripts/Treatment/Sqw/Sqw_moments.m
+++ Scripts/Treatment/Sqw/Sqw_moments.m
@@ -1,16 +1,71 @@
-function sigma=Sqw_moments(data, classical)
-% Sqw_moments: compute harmonic frequencies
+function sigma=Sqw_moments(data, M, classical, T)
+% moments=Sqw_moments(sqw, M, classical, T): compute Sqw moments (harmonic frequencies)
+%
+%   Compute the structure factor (moment 0), recoil energy (moment 1) and the
+%     collective, harmonic and longitudinal dispersions.
+%   S(q) = \int S(q,w) dw                            structure factor [moment 0]
+%   Er   = \int w*S(q,w) dw = h2q2/2M                   recoil energy [moment 1]
+%   Wc   = sqrt(2kT*Er/S(q))                    collective/isothermal dispersion
+%   Wl                                          harmonic/longitudinal excitation
+%   Wq   = 2q*sqrt(kT/S(q)/M)                               mean energy transfer
+%     
+%   collective:       Wc   = sqrt(2kT*Er/S(q));  isothermal dispersion
+%   harmonic:         Wl   = maxima wL(q) of the longitudinal current correlation
+%    S(q) structure factor';
+%    recoil E_r=h^2q^2/2M';
+%    w_c= collective dispersion';  
+%    
+%    w_q=q \surd kB T/m S(q) isothermal';
 %
 % input:
 %   data: iData object for S(q,w)
-%   classical: 0 for non symmetric S(q,w) [with Bose], 1 for symmetric (from MD)
+%   M: molar weight of the atom/molecule in [g/mol].
+%     when omitted, it is searched as sqw.Data.weight
+%   classical: 0 for non symmetric S(q,w) [with Bose, from exp.], 1 for symmetric (from MD)
+%     when omitted, this is guessed from the data set when possible
+%   T: when given, Temperature to use. When not given, the Temperature
+%      is searched in the object. The temperature is in [K]. 1 meV=11.605 K.
+%
+% Reference: 
+%   Helmut Schober, Journal of Neutron Research 17 (2014) pp. 109
+%   Lovesey p180 eq. 5.38 = w0
 
+
+
+  if nargin < 2, M = []; end
+  if nargin < 3, classical = []; end
+  if nargin < 4, T = []; end
+  
   data = Sqw_check(data);
   if isempty(data), sigma=[]; return; end
-
-  kT      = Sqw_getT(data)/11.604;   % kbT in meV;
-  M       = data.Data.weight;               % mass
   
+  % guess when omitted arguments
+  if isempty(classical) && (isfield(data,'classical') || ~isempty(findfield(data, 'classical')))
+    classical = data.classical;
+  end
+  if isempty(M) && isfield(data.Data, 'weight')
+    M       = data.Data.weight;               % mass
+  end
+  if isempty(T)
+    T = Sqw_getT(data);
+  end
+  
+  % check input parameters
+  if isempty(M)
+    disp([ mfilename ': WARNING: The data set ' s.Tag ' ' s.Title ' from ' s.Source ' does not provide information about the molar weight. Use Sqw_moments(data, M)' ]);
+    return
+  end
+  if isempty(classical)
+    disp([ mfilename ': WARNING: The data set ' s.Tag ' ' s.Title ' from ' s.Source ' does not provide information about classical/quantum data set. Use Sqw_moments(data, M, classical=0 or 1)' ]);
+    return
+  end
+  
+  if isempty(T) || T<=0
+    disp([ mfilename ': WARNING: The data set ' s.Tag ' ' s.Title ' from ' s.Source ' does not have any temperature defined. Use Sqw_moments(data, M, classical, T).' ]);
+    return
+  end
+  
+  kT      = Sqw_getT(data)/11.604;   % kbT in meV;
   q       = data{2};
   w       = data{1}; 
   
@@ -34,18 +89,24 @@ function sigma=Sqw_moments(data, classical)
   if classical
     % all odd moments are 0, even are to be multiplied by 2 when using S(q,w>0)
     % M2 = q.^2.*kT/M
-    wc      = sqrt(M2./M0); % sqrt(<w²S>/s(q)) == sqrt(q kT/M/s(q))
+    wc      = sqrt(M2./M0); % sqrt(<w²S>/s(q)) == q sqrt(kT/M/s(q)) collective/isothermal
     wl      = M3./M2; % maxima wL(q) of the longitudinal current correlation function ~ wl
   else
     wc      = sqrt(2*kT.*M1./M0); 
     wl      = sqrt(M3./M1); 
   end
-  wc.Label='wc collective dispersion';  wl.Label='wl harmonic excitation'; wq.Label='w_q=q \sqrt(k_B T/m S(q))';
+  
+  sq.Label='S(q) structure factor';
+  M1.Label='recoil E_r=h^2q^2/2M';
+  wc.Label='w_c collective/isothermal dispersion';  
+  wl.Label='w_l harmonic/longitudinal excitation'; 
+  wq.Label='w_q=q \surd kB T/m S(q) mean energy transfer';
+  
+  sigma =[ sq M1 wc wl wq ];
 
-  sigma.wc=wc;
-  sigma.wl=wl;
-  sigma.wq=wq;
   return
+  
+  
   % now fit gaussians for each q value...
   for index=1:length(q)
     this  = data(index,:);
