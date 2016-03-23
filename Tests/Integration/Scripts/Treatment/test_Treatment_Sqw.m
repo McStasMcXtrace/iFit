0e3b521769f0e0ea692a9fa728178f1ee0ebd5d8
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-03-23 17:24:04 +0100

    Tests: Sqw: added XS compared with manual integration

--- Tests/Integration/Scripts/Treatment/test_Treatment_Sqw.m
+++ Tests/Integration/Scripts/Treatment/test_Treatment_Sqw.m
@@ -5,11 +5,14 @@ function result = test_Treatment_Sqw
   SqwT  = Sqw_Bosify(Sqw2, 1250);
   Sqw25 = Sqw_dynamic_range(SqwT, 25);
   moments=Sqw_moments(SqwT, 72.64);
-
+  XS    = Sqw_scatt_xs(SqwT, 25); % 25 meV -> Ki=3.47
+  
+  % total XS should be equal to \int(q S(q))/2Ki^2
   
   if abs(mean(moments(1))-0.45) < 0.01 && abs(mean(moments(2))-1.26) < 0.01 ...
   && abs(mean(moments(3))-23.6) < 1    && abs(mean(moments(4))-5.4) < 0.1 ...
-  && abs(mean(moments(5))-19.5) < 0.1
+  && abs(mean(moments(5))-19.5) < 0.1  ...
+  && abs(trapz(Sqw{1}'.*trapz(Sqw25)/2/3.47/3.47) - 0.47) < 0.01
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
