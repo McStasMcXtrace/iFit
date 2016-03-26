0f5791936a5d933bf928b92538932199efd0527e
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-03-26 20:18:51 +0100

    Models: Sqw: more stuf in sqw_powder, not yet done...

--- Scripts/Models/scattering/dynamic_structure_factors/sqw_powder.m
+++ Scripts/Models/scattering/dynamic_structure_factors/sqw_powder.m
@@ -10,8 +10,27 @@ function r=sqw_powder(a)
 
 % the single x axis is assumed to be in a cubic system (for a start)
 
+% reciprocal vectors from ASE
+
+from atoms:
+atoms.get_reciprocal_cell()
+atoms.get_volume()
+atoms.get_chemical_formula()
+atoms.get_masses()
+atoms.get_cell()
+save all this to a file to be read by matlab
+
 % create a 4D hklw space grid
 qh=x; qk=x; ql=x; 
+f=iData(s,[],qh,qk,ql,w);
+[h,k,l,e]=ndgrid(f{1},f{2},f{3},f{4});
+h=h(:); k=k(:); l=l(:); e=e(:);
+q=sqrt(h.*h+k.*k+l.*l); % should do q=h*as+k*bs+l*cs (vector)
+clear h k l
+S=f{0}; S=S(:);
+p=iData(q,e,S);
+clear q e S
+P=hist(p, 100);
 
 % hkl in rlu
 % Q=as*h+bs*k+cs*l
