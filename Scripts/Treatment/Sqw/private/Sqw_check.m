b08ad60707019a541fb8b0851df3c8d64cf0f1a4
Emmanuel FARHI <farhi@ill.fr>
2016-04-11 17:57:19 +0200

    Treatment: sqw:   dynamic_range: also return the S(phi,w) as this is anyway computed.   gDOS: initial commit with J. Olllivier Pab/gDOS stuff   Sqw_check: initial check of wavelength and phi2q, t2e conversions.

--- Scripts/Treatment/Sqw/private/Sqw_check.m
+++ Scripts/Treatment/Sqw/private/Sqw_check.m
@@ -9,10 +9,14 @@ function s = Sqw_check(s)
 % input:
 %   s: Sqw data set
 %        e.g. 2D data set with w as 1st axis (rows), q as 2nd axis.
+
+  if isempty(s), return; end
   
   % check if the data set is Sqw (2D)
   w_present=0;
   q_present=0;
+  a_present=0;
+  t_present=0;
   if isa(s, 'iData') && ndims(s) == 2
     for index=1:2
       lab = lower(label(s,index));
@@ -21,11 +25,28 @@ function s = Sqw_check(s)
         q_present=index;
       elseif any(strfind(lab, 'energy')) || any(strfind(lab, 'frequency')) || strcmp(strtok(lab), 'w') || any(strfind(lab, 'meV'))
         w_present=index;
+      elseif any(strfind(lab, 'time')) || any(strfind(lab, 'sec')) || strcmp(strtok(lab), 't') || strcmp(strtok(lab), 'tof')
+        t_present=index;
+      elseif any(strfind(lab, 'angle')) || any(strfind(lab, 'deg')) || strcmp(strtok(lab), 't')
+        a_present=index;
       end
     end
   end
+  if ~w_present && t_present
+    % convert from S(xx,t) to S(xx,w): t2e requires L2
+    s = Sqw_t2e(s);
+    s = Sqw_check(s);
+    return
+  end
+  if ~q_present && a_present && w_present
+    % convert from S(phi,w) to S(q,w)
+    s = Sqw_phi2q(s);
+    s = Sqw_check(s);
+    return
+  end
   if ~w_present || ~q_present
-    disp([ mfilename ': WARNING: The data set ' s.Tag ' ' s.Title ' from ' s.Source ' does not seem to be an isotropic S(|q|,w) 2D object. Ignoring.' ]);
+    disp([ mfilename ': WARNING: The data set ' s.Tag ' ' s.Title ' from ' s.Source ]);
+    disp('    does not seem to be an isotropic S(|q|,w) 2D object. Ignoring.');
     s = [];
     return
   end
@@ -161,4 +182,119 @@ function s = Sqw_check(s)
     
   end % energy axis has +/- 
 
+% ------------------------------------------------------------------------------
+function s = Sqw_phi2q(s)
+% convert S(phi,w) to S(q,w)
+
+  [s,lambda] = Sqw_search_lambda(s);
+  if isempty(s), return; end
   
+  disp([ mfilename ': ' s.Tag ' ' s.Title ' Converting Axis 2: angle [deg] to wavevector [Angs-1].' ]);
+  ei    = 81.805/lambda^2;
+  phi   = s{2}; % angle
+  hw    = s{1};
+  q     = sqrt(2*(2*pi/lambda)^2*(1 + 0.5*hw/ei - sqrt(1 + hw/ei).*cos(phi*pi/180)));
+  s{2}  = q;
+  
+% ------------------------------------------------------------------------------
+function s=Sqw_t2e(s, L2)
+
+  [s,lambda] = Sqw_search_lambda(s);
+  if isempty(s), return; end
+
+  disp([ mfilename ': ' s.Tag ' ' s.Title ' Converting Axis 1: time [sec] to energy [meV].' ]);
+  t = s{1};
+  if  all(t> 0 & t < .1)
+    % probably in seconds (usually in the range 0 - 1e-2)
+    % NOP: time is already OK
+  elseif all(t > 0 & t < 100)
+    % probably in milli-seconds
+    t = t/1000;
+  elseif all(t > 0 & t < 100000)
+    % probably in micro-seconds
+    t = t/1e6;
+  else
+    disp([ mfilename ': WARNING: ' s.Tag ' ' s.Title ' the time-of-flight Axis 1 seems odd.' ])
+    disp('    Check that the time-of-flight is defined as the time from the sample to the detector, in seconds.')
+  end
+  
+  % we search for the elastic peak position (EPP)
+  if ~isfield(s, 'Distance')
+    disp([ mfilename ': ' s.Tag ' ' s.Title ' undefined sample-detector distance.' ]);
+    disp('    Define e.g. s.Distance=<sample-detector distance in m>');
+    s = [];
+    return;
+  end
+  
+  L2=get(s, 'Distance');
+  telast = L2 * lambda/3956.035;  % time from sample to detector elastic signal
+  
+  Ei = 81.805/lambda^2;
+  Ef = Ei*(telast./t).^2;
+  dtdE    = t./(2.*Ef)*1e6; % to be consistent with vnorm abs. calc.
+  kikf    = sqrt(Ei./Ef);            % all times above calculated in sec.
+  hw = Ei - Ef;
+  % Average energy scale:
+  hw0 = mean(hw,2);
+
+  dtdEkikf = dtdE.*kikf;
+  s    = s.*dtdEkikf;
+  s{1} = hw0;
+  
+% ------------------------------------------------------------------------------ 
+function [s,lambda] = Sqw_search_lambda(s, lambda)
+  % search for the wavelength in the object
+  if nargin < 2
+    lambda=[];
+  end
+
+  % no wavelength defined: search in object
+  if isempty(lambda)
+    lambda_field=[ findfield(s, 'wavelength') findfield(s, 'lambda')  ];
+    if ~isempty(lambda_field), 
+      if iscell(lambda_field), lambda_field=lambda_field{1}; end
+      lambda = mean(get(s, lambda_field));
+      disp([ mfilename ': ' s.Tag ' ' s.Title ' using incident wavelength=' num2str(lambda) ' [Angs] from ' lambda_field ]);
+    end
+  end
+  if isempty(lambda)
+    momentum_field=[ findfield(s, 'wavevector') findfield(s, 'momentum') ];
+    if ~isempty(momentum_field), 
+      if iscell(momentum_field), momentum_field=momentum_field{1}; end
+      momentum = mean(get(s, momentum_field));
+      lambda=2*pi/momentum;
+      disp([ mfilename ': ' s.Tag ' ' s.Title ' using incident wavevector=' num2str(momentum) ' [Angs-1] from ' momentum_field ]);
+    end
+  end
+  if isempty(lambda)
+    energy_field = [ findfield(s, 'energy') findfield(s, 'meV')  ];
+    if ~isempty(energy_field)
+      if iscell(energy_field), energy_field=energy_field{1}; end
+      energy = mean(get(s, energy_field));
+      disp([ mfilename ': ' s.Tag ' ' s.Title ' using incident energy=' num2str(energy) ' [meV] from ' energy_field ]);
+      lambda = sqrt(81.805/energy);
+      setalias(s, 'IncidentEnergy', energy, 'Incident Energy [meV]');
+    end
+  end
+  if isempty(lambda)
+    disp([ mfilename ': ' s.Tag ' ' s.Title ' undefined incident neutron wavelength/energy.' ]);
+    disp('    Define e.g. s.Wavelength=<lambda in Angs> or s.IncidentEnergy=<energy in meV>');
+    s = [];
+    return
+  end
+  
+  if ~isfield(s, 'Wavelength')
+    setalias(s, 'Wavelength', lambda, 'Incident Wavelength [Angs]');
+  end
+  
+  % search for a sample-to-detector distance
+  distance_field = [ findfield(s, 'distance') ];
+  if ~isempty(distance_field), 
+    if iscell(distance_field), distance_field=distance_field{1}; end
+    distance = get(s, distance_field);
+    disp([ mfilename ': ' s.Tag ' ' s.Title ' using <sample-detector distance> =' num2str(mean(distance(:))) ' [m] from ' distance_field ]);
+    if ~isfield(s, 'Distance')
+      setalias(s, 'Distance', distance, 'Sample-Detector distance [m]');
+    end
+  end
+    
