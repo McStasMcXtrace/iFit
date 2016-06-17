523f5e420b809854e24a76bfc6f562bfa0ff4cb2
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-06-17 17:31:09 +0200

    Tests: Sqw: test export of McStas Sqw file

--- Tests/Integration/Scripts/Treatment/test_Treatment_Sqw.m
+++ Tests/Integration/Scripts/Treatment/test_Treatment_Sqw.m
@@ -6,14 +6,17 @@ function result = test_Treatment_Sqw
   Sqw25 = Sqw_dynamic_range(SqwT, 25);
   moments=Sqw_moments(SqwT, 72.64);
   XS    = Sqw_scatt_xs(SqwT, 25); % 25 meV -> Ki=3.47
+  file  = Sqw_McStas(SqwT);
   
   % total XS should be equal to \int(q S(q))/2Ki^2
   
   if abs(mean(moments(1))-0.45) < 0.01 && abs(mean(moments(2))-1.26) < 0.01 ...
   && abs(mean(moments(3))-23.6) < 1    && abs(mean(moments(4))-5.4) < 0.1 ...
   && abs(mean(moments(5))-19.5) < 0.1  ...
-  && abs(trapz(Sqw.q'.*trapz(Sqw25)/2/3.47/3.47) - 0.47) < 0.01
+  && abs(trapz(Sqw.q'.*trapz(Sqw25)/2/3.47/3.47) - 0.47) < 0.01 ...
+  && ~isempty(file)
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
   end 
+  delete(file);
