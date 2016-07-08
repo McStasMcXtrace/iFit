52ba0836739caa0f95487945b7aeaf95ace56202
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-07-08 10:11:37 +0200

    iData: edit as table: enable plot of selection

--- Objects/@iData/edit.m
+++ Objects/@iData/edit.m
@@ -12,6 +12,8 @@ function edit(a)
 % Version: $Date$
 % See also iData, iData/uminus, iData/abs, iData/real, iData/imag, iData/uplus
 
+% the Selection is stored in the figure UserData
+
   if numel(a) > 1
     parfor index=1:numel(a)
       edit(a(index));
@@ -24,24 +26,31 @@ function edit(a)
       iData_private_warning(mfilename, [ 'Object ' a.Tag ' is too large (numel=' num2str(prod(size(a))) ...
     '.\n\tYou should rebin with e.g. a=a(1:2:end, 1:2:end, ...).' ]);
     end
-    UserData.Monitor = real(get(a,'Monitor'));
-    UserData.Signal  = getaxis(a, 'Signal'); % Signal/Monitor
-    UserData.Selected= [];
+    
+    Signal    = getaxis(a, 'Signal'); % Signal/Monitor
+    
     % opens a Table with the Signal content there-in
     NL = sprintf('\n');
     f = figure('Name', [ 'Edit ' char(a) ]); % raw Signal, no monitor weightening
     p = get(f, 'Position');
-    h = uitable('Data', UserData.Signal);
+    h = uitable('Data', Signal);
+    
+    % UserData.Monitor = real(get(a,'Monitor'));
+    
+    UserData.Selection = [];
+    UserData.handle    = h;
+    UserData.Name      = char(a);
+    
     set(h, 'Position', [0,0,p(3),p(4)]); 
     try
       set(h, 'Tag',[ mfilename '_' a.Tag ]);
       set(h, 'Units','normalized');
       set(h, 'Position', [0,0,1,1]);
-      set(h, 'UserData', UserData);  % contains the selection indices
+      set(f, 'UserData', UserData);  % contains the selection indices
+      set(h, 'CellSelectionCallback', @iData_edit_CellSelectionCallback);
     end
 
 %      'CellEditCallback', @iData_edit_CellEditCallback, , ...
-%      'CellSelectionCallback', @iData_edit_CellSelectionCallback, ...
 %      'RearrangeableColumn','on', 'ColumnEditable', true, ...
 %      'ToolTipString', [ char(a) NL 'You may edit the Signal from the object.' ...
 %         NL 'The Contextual menu enables to plot a selection of the oject' ], ...    
@@ -71,7 +80,7 @@ function edit(a)
         end
       end
     end
-    % uimenu(uicm, 'Separator','on','Label','Plot selection...', 'Callback', @iData_edit_display);
+    uimenu(uicm, 'Separator','on','Label','Plot selection...', 'Callback', @iData_edit_display);
     uimenu(uicm, 'Separator','on','Label', 'About iData', 'Callback',[ 'msgbox(''' version(iData) ''')' ]);
     % attach contexual menu to the table
     try
@@ -85,36 +94,63 @@ function edit(a)
   end
 end
 
-
-
-% function called when the Table is modified: update the initial object
-function iData_edit_CellEditCallback(source, eventdata)
-  % find initial object, and modify its Signal
-  
-  % this may be speed-up by only changing the modified elements
-  Tag  = get(source, 'Tag');
-  Data = get(source, 'Data');
-  UD   = get(source, 'UserData');
-  try
-    Signal = Data.*UserData.Monitor;
-  catch
-    Signal = Data;
-  end
-  [caller, base] = findobj(iData, 'Tag', Tag);
-  if ~isempty(caller), set(caller, 'Signal', Signal); end
-  if ~isempty(base),   set(base,   'Signal', Signal); end
-end
+% ------------------------------------------------------------------------------
+% Events
+% ------------------------------------------------------------------------------
 
 % function called when the Table selection is plotted
-function iData_edit_display
-  h = gcbo; % handle of the Table
-  % get the Selection
+function iData_edit_display(hObject, eventdata, varargin)
+  % hObject is the handle of the context menu item
+  fig = gcbf;
+  UserData  = get(fig, 'UserData');
+  Signal    = get(UserData.handle, 'Data');
+  Selection = UserData.Selection; % as { rows, columns }
   
+  % get the Selection as Signal( rows, columns )
+  if isempty(Selection) || isempty(Selection{1}) || isempty(Selection{2})
+    Selection = Signal;
+    rows    = [ 1 size(Signal,1) ];
+    columns = [ 1 size(Signal,2) ];
+  else
+    rows    = Selection{1};
+    columns = Selection{2};
+    Selection = Signal(Selection{:});
+  end
+  clear Signal
+
   % plot it (no axes here ?)
+  if isscalar(Selection) || ndims(Selection) > 2
+    return;
+  end
+  nfig = figure;
+  if isvector(Selection)
+    plot(Selection);
+    if numel(rows) == 1
+      xlabel(sprintf('(%i,[%i:%i])', rows, min(columns),max(columns)));
+    else
+      xlabel(sprintf('([%i:%i],%i)', min(rows),max(rows), columns));
+    end
+  elseif ndims(Selection) == 2
+    surf(Selection);
+    ylabel(sprintf('[%i:%i]', min(rows),max(rows)));
+    xlabel(sprintf('[%i:%i]', min(columns),max(columns)));
+  end
+  t = sprintf('Edit ([%i:%i,%i:%i]) %s', ...
+        min(rows),max(rows),min(columns),max(columns), UserData.Name);
+  set(nfig, 'Name', t);
+  title(t)
   
 end
 
-function iData_edit_CellSelectionCallback
+function iData_edit_CellSelectionCallback(source, eventdata)
   % copy the selection into the UserData
-  UserData.Selection = '';
+  
+  fig = gcbf; % current UItable figure
+  
+  rows    = unique(eventdata.Indices(:,1)); % rows and columns selected
+  columns = unique(eventdata.Indices(:,2));
+  
+  UserData = get(fig, 'UserData');
+  UserData.Selection = { rows,columns };
+  set(fig, 'UserData',UserData);
 end
