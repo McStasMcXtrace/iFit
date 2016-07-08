361862696dae0b91435ae335c922fd4785fafe3f
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-07-08 12:08:45 +0200

    iData: edit: allow edition of Table, export to iData,   and clipboard copy/paste.

--- Objects/@iData/edit.m
+++ Objects/@iData/edit.m
@@ -1,9 +1,12 @@
-function edit(a)
+function h = edit(a, option)
 % edit(s) : edit iData object Signal/Monitor
 %
 %   @iData/edit function to view the Signal/Monitor of a data set. 
 %     The data appears as a spreadsheet (requires Java to be enabled).
-%     The Signal can not (yet) be modified.
+%
+%   An additional 'option' argument allows to specify what to do with the Table
+%   d=edit(iData, handle)   The given Table handle is exported into an iData object.
+%   edit(iData, 'editable') The Table data can be modified after creation.
 %
 % input:  s: object or array (iData)
 % output: b: object or array (iData)
@@ -13,10 +16,18 @@ function edit(a)
 % See also iData, iData/uminus, iData/abs, iData/real, iData/imag, iData/uplus
 
 % the Selection is stored in the figure UserData
+  if nargin < 2
+    option = '';
+  end
+  
+  if ishandle(option)
+    h = iData_edit_iData(option);
+    return
+  end
 
   if numel(a) > 1
     parfor index=1:numel(a)
-      edit(a(index));
+      edit(a(index), option);
     end
     return
   end
@@ -25,6 +36,12 @@ function edit(a)
     if prod(size(a)) > 1e5
       iData_private_warning(mfilename, [ 'Object ' a.Tag ' is too large (numel=' num2str(prod(size(a))) ...
     '.\n\tYou should rebin with e.g. a=a(1:2:end, 1:2:end, ...).' ]);
+    end
+    
+    if strfind(option, 'editable')
+      editable = true;
+    else
+      editable = false;
     end
     
     Signal    = getaxis(a, 'Signal'); % Signal/Monitor
@@ -39,16 +56,19 @@ function edit(a)
     
     UserData.Selection = [];
     UserData.handle    = h;
+    UserData.figure    = f;
     UserData.Name      = char(a);
+    UserData.Axes      = getaxis(a, []);
     
     set(h, 'Position', [0,0,p(3),p(4)]); 
     try
       set(h, 'Tag',[ mfilename '_' a.Tag ]);
       set(h, 'Units','normalized');
       set(h, 'Position', [0,0,1,1]);
-      set(f, 'UserData', UserData);  % contains the selection indices
       set(h, 'CellSelectionCallback', @iData_edit_CellSelectionCallback);
+      if editable, set(h, 'ColumnEditable', true); end
     end
+    set(f, 'UserData', UserData);  % contains the selection indices
 
 %      'CellEditCallback', @iData_edit_CellEditCallback, , ...
 %      'RearrangeableColumn','on', 'ColumnEditable', true, ...
@@ -80,27 +100,32 @@ function edit(a)
         end
       end
     end
-    uimenu(uicm, 'Separator','on','Label','Plot selection...', 'Callback', @iData_edit_display);
+    uimenu(uicm, 'Separator','on','Label','Copy selection to clipboard', 'Callback', @iData_edit_copy);
+    if editable
+    uimenu(uicm, 'Label','Paste from clipboard', 'Callback', @iData_edit_paste);
+    end
+    uimenu(uicm, 'Label','Export to main workspace...', 'Callback', @iData_edit_export);
+    uimenu(uicm, 'Label','Plot selection...', 'Callback', @iData_edit_display);
     uimenu(uicm, 'Separator','on','Label', 'About iData', 'Callback',[ 'msgbox(''' version(iData) ''')' ]);
     % attach contexual menu to the table
     try
       set(h,   'UIContextMenu', uicm); 
     end
+    
   else
     % open the data file
     try
       edit(a.Source);
     end
   end
-end
+
 
 % ------------------------------------------------------------------------------
 % Events
 % ------------------------------------------------------------------------------
 
-% function called when the Table selection is plotted
-function iData_edit_display(hObject, eventdata, varargin)
-  % hObject is the handle of the context menu item
+function [Selection, rows, columns, UserData] = iData_edit_getselection
+  % get the current selection in the uitable
   fig = gcbf;
   UserData  = get(fig, 'UserData');
   Signal    = get(UserData.handle, 'Data');
@@ -116,8 +141,13 @@ function iData_edit_display(hObject, eventdata, varargin)
     columns = Selection{2};
     Selection = Signal(Selection{:});
   end
-  clear Signal
 
+% function called when the Table selection is plotted
+function iData_edit_display(hObject, eventdata, varargin)
+  % hObject is the handle of the context menu item
+  
+  [Selection, rows, columns] = iData_edit_getselection;
+  
   % plot it (no axes here ?)
   if isscalar(Selection) || ndims(Selection) > 2
     return;
@@ -140,7 +170,6 @@ function iData_edit_display(hObject, eventdata, varargin)
   set(nfig, 'Name', t);
   title(t)
   
-end
 
 function iData_edit_CellSelectionCallback(source, eventdata)
   % copy the selection into the UserData
@@ -153,4 +182,93 @@ function iData_edit_CellSelectionCallback(source, eventdata)
   UserData = get(fig, 'UserData');
   UserData.Selection = { rows,columns };
   set(fig, 'UserData',UserData);
-end
+
+% Clipboard management
+function iData_edit_copy(hObject, eventdata, varargin)
+  % copy the selection to the clipboard
+  
+  [Selection, rows, columns, UserData] = iData_edit_getselection;
+  
+  if isempty(Selection), return; end
+  
+  disp([ mfilename ': copy Data Selection ' mat2str(size(Selection)) ' to the clipboard.' ]);
+  disp(UserData.Name);
+  c = num2str(Selection);
+  c = cellstr(c);
+  c = sprintf('%s\n', c{:});
+  
+  clipboard('copy', c);
+  
+function iData_edit_paste(hObject, eventdata, varargin)
+  % paste from the clipboard at the current location
+  
+  [Selection, rows, columns, UserData] = iData_edit_getselection;
+  
+  Selection = clipboard('paste');
+  if isempty(Selection), return; end
+  
+  % split into lines
+  Selection = regexp(Selection,'\n|\r','split');
+  Selection = Selection(~cellfun(@isempty, Selection));
+  Selection = str2num(str2mat(Selection));  % now a double array
+  
+  if isempty(Selection), return; end
+  
+  fig = gcbf;
+  Data      = get(UserData.handle, 'Data');
+  
+  % test if the selection will replace some data without extending borders
+  sz_Data = size(Data);
+  sz_Sel  = size(Selection);
+  sz_Ins  = [ min(rows) min(columns) ]; % insertion location
+  rows    = sz_Ins(1):(sz_Sel(1)+sz_Ins(1)-1);
+  columns = sz_Ins(2):(sz_Sel(2)+sz_Ins(2)-1);
+  if any(sz_Data < sz_Sel+sz_Ins-1)
+    % the Selection will extend the Data set
+    
+    newData = zeros(sz_Sel+sz_Ins-1);
+    % put the Data in
+    newData(1:sz_Data(1),1:sz_Data(2)) = Data;
+    Data = newData;
+    clear newData
+    % this also invalidates the stored Axes from initial object
+    UserData.Axes = [];
+    set(fig, 'UserData', UserData)
+  end
+  Data( rows, columns ) = Selection;
+  set(UserData.handle, 'Data', Data);
+  
+function iData_edit_export(varargin)
+  % exports the data set to the 'ans' variable in main workspace
+  fig = gcbf;
+  UserData  = get(fig, 'UserData');
+  d=iData_edit_iData(UserData.handle);
+  disp([ mfilename ': export Data ' mat2str(size(d)) ' into iData object ''ans''.' ]);
+  disp(UserData.Name);
+
+  assignin('base','ans',d);
+  % make 'ans' be displayed
+  ans = d
+
+function d=iData_edit_iData(h)
+  % create an iData object from Table
+  if ~ishandle(h) || ~strcmpi(get(h, 'Type'),'uitable')
+    d = [];
+  end
+  
+  % get the Data
+  fig       = get(h,'Parent');
+  UserData  = get(fig, 'UserData');
+  Signal    = get(h, 'Data');
+  
+  % get the Axes
+  Axes      = UserData.Axes;
+  % check axes if there is at least one given
+  if ~isempty(Axes)
+    d = iData(Axes{:},Signal);
+  else
+    d = iData(Signal);
+  end
+  d.Title = [ 'edit(' UserData.Name ')' ];
+  
+  
