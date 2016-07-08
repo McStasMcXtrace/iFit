73bd7519dc4de3efb0698ab8080d323aea6e57fc
Emmanuel FARHI <farhi@ill.fr>
2016-07-08 16:08:05 +0200

    App:MiFit: simpler layout. Start to fill the stack.

--- Applications/miFit/mifit.m
+++ Applications/miFit/mifit.m
@@ -1,28 +1,60 @@
 function out = mifit(varargin)
 
-    out = [];
+
+% notes:
+% saveas: display dialogue to select what to export:
+% * data sets
+% * parameters
+% * fit model
+
+% plot: plot model/dataset/parameters ? -> preferences
+
+% set optimizer configuration -> contextual dialogue
+
+% treat data set with MView type operations
+
+% re-asign data set signal, axes, ... to aliases/new ones
+
+% will add toolbar instead of uicontrols for fast processing
 
     % look if the main window is opened
     fig = mifit_fig;
     if isempty(fig) || ~ishandle(fig)
         fig = feval([ mfilename '_OpeningFcn' ]);
-        out = fig;
     end
+    out = fig;
 
     if nargin == 1 && isa(varargin{1}, 'iFunc')
         % import model: add it to the Models menu, if not already there
     elseif nargin == 1 && isa(varargin{1}, 'iData')
         % import iData object(s) into the List
-        disp('Import into List')
+        disp([ mfilename ': Import into List:' ])
+        disp(char(varargin{1}))
     end
 
-    if ~isempty(varargin) && ischar(varargin{1})
-        % callback with varargin{1} == 'action'
-        if strcmpi(varargin{1},'identify'), return; end
-        try
-          feval(varargin{:});
-        catch
-          disp([ mfilename ': Unknown action "' varargin{1} '"' ])
+    if ~isempty(varargin) 
+        if ischar(varargin{1})          % a function/action to call ?
+            % callback with varargin{1} == 'action'
+            if strcmpi(varargin{1},'identify'), return; end
+            try
+              feval(varargin{:});
+            catch ME
+              disp([ mfilename ': Unknown action "' varargin{1} '"' ])
+              rethrow(ME)
+            end
+        elseif ishandle(varargin{1})    % a CallBack function
+            h = varargin{1};
+            if strcmpi(get(h,'Type'),'uitable')
+                % get data from a UITable
+                try
+                    d =edit(iData, h);
+                catch
+                    d = iData(get(h, 'Data'));
+                    d.Title = get(h, 'Tag');
+                end
+                % now push 'd' into the Stack
+                List_Data_push(d);
+            end
         end
     end
     
@@ -80,6 +112,10 @@ if isempty(fig) || ~ishandle(fig)
         separator = 1;
         % first add the 'Add new Model': from File or ifitmakefunc
         uimenu(hmodels, 'Label','Add new Model...');
+        uimenu(hmodels, 'Label','Plot Model...');
+        uimenu(hmodels, 'Label','View Model Parameters...', 'Separator','on');
+        uimenu(hmodels, 'Label','Plot Model Parameters...');
+        uimenu(hmodels, 'Label','Export Model Parameters...');
         for f=functions
             % each Model is an iFunc object. These should be stored in the
             % Models items 'UserData'
@@ -113,6 +149,10 @@ if isempty(fig) || ~ishandle(fig)
         end
     end
     
+    % create the AppData
+    % Data Stack
+    setappdata(fig, 'Data', []);
+    
     % close welcome image
     delete(h);
 end
@@ -121,232 +161,50 @@ end
 % Callbacks
 % -------------------------------------------------------------------------
 
-% --- Executes on button press in Button_Fit.
-function Button_Fit_Callback(hObject, eventdata, handles)
-% hObject    handle to Button_Fit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+function File_New(handle)
+% File/New menu item
+  d = iData(zeros(5)); % create an empty Data set;
+  disp([ mfilename ': Editing an empty Data set. Close the window to retrieve its content' ]);
+  disp( '    as a new Data set into MiFit. Use the Contextual menu.');
+  handle = edit(d, 'editable');
+  % set a DeletedFcn so that the content can be retrieved into MiFit when
+  % closing.
+  set(handle, 'DeleteFcn', @mifit);
+  
+function File_Open(handle)
+  d = iData('');
+  % push that data onto the List
+  
+function List_Data_push(d)
+% put a new data set at the end of the stack
+  fig = mifit_fig;
+  Data = getappdata(fig, 'Data');
+  
+  % update AppData Stack
+  if numel(d) > 1, d = d(:); end
+  Data = [ Data ; d ];  % a columns of iData set
+  setappdata(fig, 'Data', d);
+  
+  % update the List labels by appending the Name at the end
+  hObject        = mifit_fig('List_Data_Files');
+  list           = get(hObject,'String');
+  index_selected = get(hObject,'Value');
+  for index=1:numel(d)
+      list{end+1} = char(d);
+      index_selected(end+1) = 1;
+  end
+  set(hObject,'String', list, 'Value', index_selected);
+  
 
+function d=List_Data_Files(hObject)
+% get the selected Data List
+  if nargin == 0 || isempty(hObject)
+      hObject = mifit_fig('List_Data_Files');
+  end
+  d = [];
 
-% --- Executes on button press in Button_Plot.
-function Button_Plot_Callback(hObject, eventdata, handles)
-% hObject    handle to Button_Plot (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes during object creation, after setting all properties.
-function Button_Data_File_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to Button_Data_File (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --------------------------------------------------------------------
-function Menu_File_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_File (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Menu_Load_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_Load (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Menu_Models_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_Models (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Menu_Optimizers_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_Optimizers (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Menu_Tools_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_Tools (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Tools_Plot_Callback(hObject, eventdata, handles)
-% hObject    handle to Tools_Plot (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Tools_Fit_Callback(hObject, eventdata, handles)
-% hObject    handle to Tools_Fit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function File_New_Callback(hObject, eventdata, handles)
-% hObject    handle to File_New (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function File_Open_Callback(hObject, eventdata, handles)
-% hObject    handle to File_Open (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-  List_Add_Callback;
-
-
-% --------------------------------------------------------------------
-function File_Save_Callback(hObject, eventdata, handles)
-% hObject    handle to File_Save (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function File_Preferences_Callback(hObject, eventdata, handles)
-% hObject    handle to File_Preferences (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function File_Exit_Callback(hObject, eventdata, handles)
-% hObject    handle to File_Exit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-  disp([ mfilename ': Exiting MiFit, saving Preferences and Data files...' ])
-  disp(datestr(now))
-
-
-% --- Executes on selection change in List_Data_Files.
-function List_Data_Files_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Data_Files (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns List_Data_Files contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from List_Data_Files
-
-
-% --- Executes during object creation, after setting all properties.
-function List_Data_Files_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to List_Data_Files (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: listbox controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on button press in Button_Open.
-function Button_Open_Callback(hObject, eventdata, handles)
-% hObject    handle to Button_Open (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-  List_Add_Callback;
-
-
-% --- Executes on button press in Button_Data_View.
-function Button_Data_View_Callback(hObject, eventdata, handles)
-% hObject    handle to Button_Data_View (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function List_Add_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Add (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-  disp([ mfilename ': Add Data files to the MiFit List' ]);
-  toadd = iData('');
-  mifit(toadd);
-
-% --------------------------------------------------------------------
-function List_Remove_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Remove (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function List_Plot_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Plot (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function List_Fit_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Fit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Tools_View_Callback(hObject, eventdata, handles)
-% hObject    handle to Tools_View (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Tools_Help_Callback(hObject, eventdata, handles)
-% hObject    handle to Tools_Help (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Tools_About_Callback(hObject, eventdata, handles)
-% hObject    handle to Tools_About (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-    h = msgbox('MiFit is a user interface to the iFit software','MiFit: About','help');
-
-% --------------------------------------------------------------------
-function File_Close_Callback(hObject, eventdata, handles)
-% hObject    handle to File_Close (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function Menu_List_Data_Callback(hObject, eventdata, handles)
-% hObject    handle to Menu_List_Data (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function List_Remove_All_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Remove_All (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function List_Select_All_Callback(hObject, eventdata, handles)
-% hObject    handle to List_Select_All (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+  index_selected = get(hObject,'Value');
+  list = get(hObject,'String');
+  if isempty(list), return; end
+  
+  item_selected = list{index_selected};
