4a883c21800c8787e03029d3b00b726ce0fc2e6a
Emmanuel FARHI <farhi@ill.fr>
2016-07-11 10:24:32 +0200

    Process: added comments about new object based on a Timer.

--- Objects/@Process/Process.m
+++ Objects/@Process/Process.m
@@ -91,3 +91,108 @@ else
 end
 
 % ------------------------------------------------------------------------------
+%creation:
+
+
+%t = timer('TimerFcn',@TimerRefreshCmd, 
+%  'StartFcn', { @TimerStartCmd, command },
+%  'StopFcn', @TimerStopCmd,
+%  'ExecutionMode', 'fixedSpacing',
+%  Name, command,
+%  'Period', 10.0);
+
+%function TimerStartCmd(obj, event, command)
+
+%  obj.UserData.process = java.lang.Runtime.getRuntime().exec(command);
+%  obj.UserData.command = command;
+%  obj.UserData.creationDate  = now;
+%  obj.UserData.terminationDate=[];
+%  obj.UserData.isActive  = 1;
+%  obj.UserData.stdout = [];   % stores the stdout (yes!) from the process
+%  obj.UserData.stderr = [];   % stores the stderr from the process
+%  
+%  % I/O from process
+%  obj.UserData.stdinStream   = pid.process.getInputStream;
+%  obj.UserData.stderrStream  = pid.process.getErrorStream;
+%  
+%  % update the Timer to hold the external process information
+%  assignin('caller', inputname(1), obj);
+%  
+%function TimerStopCmd(obj, event)
+
+%  obj.UserData.terminationDate=now;
+%  obj.UserData.process.destroy;
+%  pause(1) % wait a little for process to abort
+%  obj.UserData.isActive  = 0;
+%  obj.UserData.exitValue = obj.UserData.process.exitValue;
+%  
+%  % update the Timer to hold the external process information
+%  assignin('caller', inputname(1), obj);
+%  
+%function TimerRefreshCmd(obj, event)
+
+%  % first check if process is still valid. For this we try to get its exitValue
+%  % which is only possible after completion and raises an error during execution.
+
+%  try
+%    obj.UserData.exitValue = obj.UserData.process.exitValue; % will raise error if process still runs
+%    if isempty(obj.UserData.terminationDate) || ~obj.UserData.terminationDate
+%      obj.UserData.terminationDate=now;
+%    end
+%    obj.UserData.isActive  = 0;
+%    stop(obj);
+%  catch
+%    if isempty(obj.UserData.process) || isempty(obj.UserData.command)
+%      obj.UserData.isActive  = 0;
+%      stop(obj);
+%    else
+%      obj.UserData.isActive  = 1;
+%    end
+%  end
+
+%  % then retrieve any stdout/stderr content (possibly in Buffer after end of process)
+%  obj.UserData.stdout = strcat(obj.UserData.stdout, process_get_output(pid.stdinStream));
+%  obj.UserData.stderr = strcat(obj.UserData.stderr, process_get_output(pid.stderrStream));
+%  
+%  % update the Timer to hold the external process information
+%  assignin('caller', inputname(1), obj);
+%  
+%% private function to read a stream
+%  function out = process_get_output(stream)
+%  % process_get_output: get the stream (InputStream) content[private]
+%  
+%    out = '';
+%    available = 0;
+%    try
+%      available = stream.available;
+%    end
+%    
+%    % return when nothing to read or invalid stream
+%    if available <= 0, return; end
+%    
+%    % Read the content of the stream.
+%    %
+%    % EPIC FAIL 1:
+%    % The following method would be nice, but fails:
+%    %   target_buffer = javaArray('java.lang.Byte', available);
+%    %   stream.read(target_buffer, 0, available);
+%    %
+%    % Indeed, as matlab converts any Java array into a Matlab class, 
+%    % the read method can not be used with additional parameters (signature not found)
+%    % see http://www.mathworks.com/matlabcentral/answers/66227-syntax-for-call-to-java-library-function-with-byte-reference-parameter
+%    %
+%    % EPIC FAIL 2:
+%    % using readLine from a BufferedReader stalls after a few iterations
+%    % Reader       = java.io.InputStreamReader(stream);
+%    % bufferReader = java.io.BufferedReader(Reader);
+%    % readLine(bufferReader)
+%    
+%    % we use a for loop to read all bytes, one by one (sadly).
+%    out = zeros(1, available);
+%    for index=1:available
+%      out(index) = read(stream);
+%    end
+%    
+%    out = strrep(char(out), sprintf('\n\r'), sprintf('\n'));
+
+%      
