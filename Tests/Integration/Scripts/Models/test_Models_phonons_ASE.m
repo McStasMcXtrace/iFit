ba8babd0a1df4d96b61dc6ae6afc669fdfeee03e
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-08-26 16:00:07 +0200

    Tests: faster Model tests for phonon and spinw

--- Tests/Integration/Scripts/Models/test_Models_phonons_ASE.m
+++ Tests/Integration/Scripts/Models/test_Models_phonons_ASE.m
@@ -1,12 +1,12 @@
 function result = test_Models_phonons_ASE
 
   % spin wave
-  s=sqw_phonons;
+  s=sqw_phonons('defaults');
   if isempty(s)
     result = [ 'OK     ' mfilename ' SKIPPED: ASE not available'];
     return
   end
-  qh=linspace(0.01,1.5,50);qk=qh; ql=qh'; w=linspace(0.01,50,50);
+  qh=linspace(0.01,1.5,20);qk=qh; ql=qh'; w=linspace(0.01,50,50);
   f=iData(s,[],qh,qk,ql,w);
   fig=figure; plot3(log(f(:,:,1,:)));
   [w1,c1]=std(f(:,:,1,:),3);
