dcaca6e0ad2248dff6bdb0dba2699661e2f99ee6
Emmanuel FARHI <farhi@ill.fr>
2016-08-26 16:00:07 +0200

    Tests: faster Model tests for phonon and spinw

--- Tests/Integration/Scripts/Models/test_Models_spinw.m
+++ Tests/Integration/Scripts/Models/test_Models_spinw.m
@@ -1,17 +1,17 @@
 function result = test_Models_spinw
 
   % spin wave
-  s=sqw_spinw;
+  s=sqw_spinw('defaults');
   if isempty(s)
     result = [ 'OK     ' mfilename ' SKIPPED: SpinW not available'];
     return
   end
-  qh=linspace(0.01,1.5,50);qk=qh; ql=qh'; w=linspace(0.01,10,50);
+  qh=linspace(0.01,1,20);qk=qh; ql=qh'; w=linspace(0.01,10,50);
   f=iData(s,[],qh,qk,ql,w);
   fig=figure; plot3(log(f(:,:,1,:)));
   [w1,c1]=std(f(:,:,1,:),3);
   
-  if abs(c1-5.33) < 0.01 && abs(w1-2.69) < 0.1 
+  if abs(c1-5.92) < 0.5 && abs(w1-2.5) < 0.5 
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
