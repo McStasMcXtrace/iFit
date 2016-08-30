b55c757e86e2842908701a242ce28b5b5a7b326f
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-08-30 16:32:26 +0200

    iFunc: more relaxed isempty

--- Objects/@iFunc/isempty.m
+++ Objects/@iFunc/isempty.m
@@ -1,7 +1,7 @@
 function y = isempty(s)
 % isempty(s) : true for empty iFunc object
 %
-%   @iFunc/isempty true for empty iFunc object (no value/expression)
+%   @iFunc/isempty true for empty iFunc object (no parameter/expression)
 %
 % input:  s: object or array (iFunc)
 % output: false(0) or true(1) whether Signal is empty in the objects
@@ -16,12 +16,13 @@ elseif ~numel(s), y=1; return;
 end
 for index = 1:numel(s)
   t = s(index);
-  if     isempty(t.Expression), empty = 1;
-  else                          empty = 0; end
+  if     isempty(t.Expression) && isempty(t.Parameters), empty = 1;
+  else empty = 0; end
   y(index) = empty;
 end
 
 y=uint8(y);
 
 
-
+% expression  parameters  empty
+%     no         no         yes
