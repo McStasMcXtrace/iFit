16f2f60359853a5a6f4e87dc959250de5f19ba66
Emmanuel FARHI <farhi@ill.fr>
2016-09-23 17:02:00 +0200

    Models: Sqw: now use templates for HKL and DHO parts.

---
+++ Scripts/Models/Factory/private/sqw_phonons_template_dho.m
@@ -0,0 +1,74 @@
+function [signal, this] = sqw_phonons_template_dho(HKL, t, FREQ, POLAR, is_event, resize_me, Amplitude, Gamma, Bkg, T, u2)
+% sqw_phonon_template_dho: code to build signal from DHO's at each mode FREQ, for all HKL locations
+%   when POLAR is not empty, the phonon intensity (Q.e)^2 form factor is computed
+%   when resize_me is specified [size], the signal is reshaped to it
+%   when u2>0 is specified, the Debye-Waller intensity exp(-u2 Q2) form factor is computed
+% Reference: B. Fak, B. Dorner / Physica B 234-236 (1997) 1107-1108
+
+% size of 'w' is [ numel(x) numel(t) ]. the energy for which we evaluate the model
+if is_event, w = t(:);
+else         w = ones(size(FREQ,1),1) * t(:)'; end
+signal               = zeros(size(w));
+this.UserData.maxFreq= max(FREQ);
+nt = numel(t);
+
+% test for unstable modes
+wrong_w = numel(find(FREQ(:) < 0 | ~isreal(FREQ(:)) | ~isfinite(FREQ(:))));
+if wrong_w, disp([ 'WARNING: found ' num2str(wrong_w) ' negative/imaginary phonon frequencies (' num2str(wrong_w*100/numel(FREQ)) '% of total)' ]); end
+
+% we compute the Q vector in [Angs-1]. Search for the B=rlu2cartesian matrix
+UD = this.UserData; B=[];
+if isfield(UD, 'reciprocal_cell')
+  B = UD.reciprocal_cell;
+elseif isfield(UD, 'properties') && isfield(UD.properties, 'reciprocal_cell')
+  B = UD.properties.reciprocal_cell;
+else
+  B = eye(3); % assume cubic, a=b=c=2*pi, 90 deg, a*=2pi/a=1
+end
+q_cart = B*HKL';
+qx=q_cart(1,:); qy=q_cart(2,:); qz=q_cart(3,:);
+Q=[ qx(:) qy(:) qz(:) ];  % in Angs-1
+clear q_cart qx qy qz
+
+for index=1:size(FREQ,2)  % loop on modes
+  % % size of 'w0' is [ numel(x) numel(t) ]. the energy of the modes (columns) at given x=q (rows)
+  if is_event
+    % event 4D: [ x y z t ]   as vectors, same length, same orientation
+    w0= FREQ(:,index);
+  else  % all other cases (use matrices instead of vectors)         
+    w0= FREQ(:,index) * ones(1,nt); 
+  end
+  % we assume Gamma(w) = Gamma w/w0
+  % W0 is the renormalized phonon frequency W0^2 = w0^2+Gamma^2
+  Gamma2 = Gamma^2+imag(w0).^2; % imaginary frequency goes in the damping
+  w0     = real(w0);
+  W02    = w0.^2+Gamma2;
+  clear w0
+  
+  % the Bose factor is negative for w<0, positive for w>0
+  % (n+1) converges to 0 for w -> -Inf, and to 1 for w-> +Inf. It diverges at w=0
+  if ~isempty(T) && T > 0, 
+    n         = 1./(exp(w/T)-1);
+    n(w == 0) = 0;  % avoid divergence
+  else n=0; end
+  
+  % phonon form factor (intensity) |Q.e|^2
+  if ~isempty(POLAR)
+    ZQ = abs(sum(Q.*squeeze(POLAR(:,index,:)),2)).^2; % one-phonon structure factor in a Bravais lattice
+  else ZQ = 1; end
+  % Debye-Waller form factor exp(-2Wq) ~ exp(-1/6 u2.Q2)
+  if ~isempty(u2) && u2 > 0
+    ZQ = ZQ.*exp(-u2/6*sum(Q.^2,2));
+  end
+  if ~is_event && ~isscalar(ZQ), ZQ = ZQ*ones(1,nt); end
+  % sum-up all contributions to signal
+  dho = (n+1).*ZQ*4.*w.*sqrt(Gamma2)/pi ./ ((w.^2-W02).^2 + 4*w.^2.*Gamma2);
+  signal = signal+dho;
+end % for mode index
+
+% Amplitude is exp(-2W)/2M
+signal = signal*Amplitude + Bkg;
+if ~isempty(resize_me) && prod(resize_me) == numel(signal)
+  signal = reshape(signal, resize_me); % initial 4D cube dimension = [ size(x) numel(t) ]
+end
+
---
+++ Scripts/Models/Factory/private/sqw_phonons_template_hkl.m
@@ -0,0 +1,53 @@
+function [HKL, t, is_event, resize_me] = sqw_phonons_template_hkl(x,y,z,t)
+% sqw_phonons_template_hkl: code to analyze [xyzt] and prepare a list of HKL locations
+% can work in the following logic:
+% event 4D: [ x y z t ]   as vectors, same length, same orientation -> ResLibCal...
+% kpath 2D: [ x y z ]     as vectors, same length, same orientation, [ t ] not same length or orientation
+% map 3D:   [ x y z ]     are all 3D arrays, and [ t ] gives 4D as a vector
+% map 4D:   any other choice. Get unique values, and build ndgrid
+
+ax = {x y z t};
+% check for vectors: 0 or 1 for each axis
+check_vector = cellfun(@isvector, ax);
+% check number of elements: would be equal for 3D grids (xyz) or 4D (xyzt)
+check_numel  = cellfun(@numel, ax);
+% check orientation (axis rank) for vectors. 0 for non vectors
+check_orient = zeros(1,4);
+index        = find(check_vector);
+check_orient(index) = cellfun(@(c)find(size(c)==numel(c)), ax(index));
+
+HKL = []; is_event = false; resize_me = [];
+if all(check_vector) && all(check_numel == check_numel(1)) && all(check_orient == check_orient(1))
+  % event 4D: [ x y z t ]   as vectors, same length, same orientation
+  is_event = true;
+  
+elseif all(check_vector(1:4)) && all(check_numel(1:3) == check_numel(1)) && all(check_orient(1:3) == check_orient(1))
+  % kpath 2D: [ x y z ]     as vectors, same length, same orientation, [ t ] vector, not same length or orientation
+  % this is the SAME test as event 4D, but with xyz only
+
+elseif all(cellfun(@ndims, ax) == 4) && all(check_numel == check_numel(1)) % (X,Y,Z,T) are all 4D, same numel
+  x=x(:,:,:,1); y=y(:,:,:,1); z=z(:,:,:,1); t=t(1,1,1,:);
+  resize_me = size(x);
+
+elseif all(cellfun(@ndims, ax(1:3)) == 3)  && all(check_numel(1:3) == check_numel(1)) % (X,Y,Z) are all 3D
+  % 3D HKL data sets plus energy axis. NOP.
+  resize_me = size(x);
+else
+  % map 4D:   any other choice
+  % any(~check_vector) || any(check_numel ~= check_numel(1)) || any(check_orient ~= check_orient(1))
+  x=unique(x); y=unique(y); z=unique(z); 
+  [x,y,z] = ndgrid(x,y,z);
+  resize_me = size(x);
+end
+clear ax
+HKL      = [ x(:) y(:) z(:) ];      % a clean array (Nx3) of HKL coordinates
+diff_HKL = diff(HKL,1,1);           % difference along rows
+HKL(sum(diff_HKL,2) == 0, :) = [];  % remove consecutive duplicates at same HKL
+t        = unique(t);
+clear diff_HKL
+if numel(resize_me) == 3
+  resize_me = [ resize_me numel(t) ];
+end
+
+% now we compute FREQ (and POLAR when possible) at HKL
+u2 = []; POLAR=[]; T=0; Amplitude=1; Gamma=0.01;  % defaults
