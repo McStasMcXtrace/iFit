6d1fde364fc7d93db02bbfb9dc1f6acaf2c9b635
Emmanuel FARHI <farhi@ill.fr>
2016-10-05 13:45:40 +0200

    Tests: fix to account for recent commits.

--- Tests/Unit/Objects/iFunc/test_iFunc_get.m
+++ Tests/Unit/Objects/iFunc/test_iFunc_get.m
@@ -2,7 +2,7 @@ function result=test_iFunc_get
 
 a=gauss;
 b=get(a, 'Amplitude');
-[p,a]=feval(a,'guess');
+[signal,a]=feval(a,'guess'); p=a.ParameterValues;
 c=get(a, 'Amplitude');
 e=get(a,'Expression');
 if isempty(b) && isscalar(c) && c == a.Amplitude
