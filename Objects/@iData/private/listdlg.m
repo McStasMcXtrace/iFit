04e8df10d336b1e236837a33c20b88c2122cbc20
Emmanuel FARHI <farhi@ill.fr>
2016-10-13 09:31:47 +0200

    iData/listdlg: update our listdlg that can work modal or normal

--- Objects/@iData/private/listdlg.m
+++ Objects/@iData/private/listdlg.m
@@ -1,4 +1,4 @@
-function [fig] = listdlg_nonmodal(varargin)
+function [selection,value] = listdlg_nonmodal(varargin)
 %LISTDLG_nonmodal  List selection dialog box.
 %   [SELECTION,OK] = LISTDLG('ListString',S) creates a modal dialog box
 %   which allows you to select a string or multiple strings from a list.
@@ -27,6 +27,10 @@ function [fig] = listdlg_nonmodal(varargin)
 %                   as text above the list box; defaults to {}.
 %   'OKString'      string for the OK button; defaults to 'OK'.
 %   'CancelString'  string for the Cancel button; defaults to 'Cancel'.
+%   'Resize'        string: can be 'on' (default) or 'off'.
+%   'WindowStyle'   string: can be 'normal' or 'modal' (default).
+%   'FontSize'      scalar: the Font size for texts.
+%   'Interpreter'   string: can be either 'none' (default) or 'tex'.
 %
 %   A 'Select all' button is provided in the multiple selection case.
 %
@@ -40,7 +44,7 @@ function [fig] = listdlg_nonmodal(varargin)
 %  See also DIALOG, ERRORDLG, HELPDLG, INPUTDLG,
 %    MSGBOX, QUESTDLG, WARNDLG.
 %
-% THIS VERSION IS MODIFIED TO USE DefaultUicontrolFontSize and be NonModal/Resizable
+% THIS VERSION IS MODIFIED TO USE DefaultUicontrolFontSize
 
 %   Copyright 1984-2009 The MathWorks, Inc.
 %   $Revision: 1.20.4.10 $  $Date: 2009/10/24 19:19:47 $
@@ -60,7 +64,7 @@ function [fig] = listdlg_nonmodal(varargin)
 
 error(nargchk(1,inf,nargin))
 
-figname = '';
+figname = mfilename;
 smode = 2;   % (multiple)
 promptstring = {};
 liststring = [];
@@ -71,6 +75,11 @@ cancelstring = 'Cancel';
 fus = 8;
 ffs = 8;
 uh = 22;
+resize = 'on';
+windowstyle = 'modal';
+fontsize = get(0,'DefaultUicontrolFontSize');
+interpreter = 'none';
+[~,tag] = fileparts(tempname); tag = [ 'ListDialogAppData_' tag ];
 
 if mod(length(varargin),2) ~= 0
     % input args have not com in pairs, woe is me
@@ -105,6 +114,14 @@ for i=1:2:length(varargin)
       okstring = varargin{i+1};
      case 'cancelstring'
       cancelstring = varargin{i+1};
+     case 'resize'
+      resize = varargin{i+1};
+     case {'windowstyle','createmode'}
+      windowstyle = varargin{i+1};
+     case 'interpreter'
+      interpreter = varargin{i+1};
+     case 'tag'
+      tag = varargin{i+1};
      otherwise
       error('MATLAB:listdlg:UnknownParameter', 'Unknown parameter name passed to LISTDLG.  Name was %s', varargin{i})
     end
@@ -122,7 +139,7 @@ if isempty(liststring)
     error('MATLAB:listdlg:NeedParameter', 'ListString parameter is required.')
 end
 
-ex = get(0,'DefaultUicontrolFontSize')*1.7;  % height extent per line of uicontrol text (approx)
+ex = fontsize*1.7;  % height extent per line of uicontrol text (approx)
 
 fp = get(0,'DefaultFigurePosition');
 w = 2*(fus+ffs)+listsize(1);
@@ -132,10 +149,10 @@ fp = [fp(1) fp(2)+fp(4)-h w h];  % keep upper left corner fixed
 fig_props = { ...
     'name'                   figname ...
     'color'                  get(0,'DefaultUicontrolBackgroundColor') ...
-    'resize'                 'on' ...
+    'resize'                 resize ...
     'numbertitle'            'off' ...
     'menubar'                'none' ...
-    'windowstyle'            'normal' ...
+    'windowstyle'            windowstyle ...
     'visible'                'off' ...
     'createfcn'              ''    ...
     'position'               fp   ...
@@ -150,6 +167,7 @@ fig = figure(fig_props{:});
 if length(promptstring)>0
     prompt_text = uicontrol('style','text','string',promptstring,...
         'horizontalalignment','left',...
+        'fontsize',fontsize, ...
         'position',[ffs+fus fp(4)-(ffs+fus+ex*length(promptstring)) ...
         listsize(1) ex*length(promptstring)]); %#ok
     set(prompt_text,'unit','normalized');
@@ -210,7 +228,35 @@ ad.selection=[];
 ad.selectionString='';
 ad.ok_btn  = ok_btn;
 ad.cancel_btn=cancel_btn;
-setappdata(fig,'ListDialogAppData__',ad);
+ad.tag = tag;
+set(fig, 'UserData', ad);
+setappdata(0,tag, ad);
+
+if strcmp(windowstyle,'modal')
+	try
+		% Give default focus to the listbox *after* the figure is made visible
+		uicontrol(listbox);
+		uiwait(fig);
+	catch
+		if ishghandle(fig)
+		    delete(fig)
+		end
+	end
+
+	if isappdata(0,tag)
+		ad = getappdata(0,tag);
+		selection = ad.selection;
+		value = ad.value;
+		rmappdata(0,tag)
+	else
+		% figure was deleted
+		selection = [];
+		value = 0;
+	end
+else % non-modal, normal
+	selection = fig;
+	value = ad;
+end
 
 %% figure, OK and Cancel KeyPressFcn
 function doKeypress(src, evd, listbox) %#ok
@@ -221,29 +267,33 @@ end
 
 %% OK callback
 function doOK(ok_btn, evd, listbox) %#ok
-if isappdata(gcbf, 'ListDialogAppData__')
-  ad = getappdata(gcbf,'ListDialogAppData__');
+fig = get(listbox, 'Parent');
+ad = get(fig,'UserData');
+if isappdata(0, ad.tag)
+  ad = getappdata(0,ad.tag);
 end
 ad.value = 1;
 ad.selection = get(listbox,'value');
 string = get(listbox,'string');
 ad.selectionString = string(ad.selection);
 ad.button = 'OK';
-setappdata(gcbf,'ListDialogAppData__',ad);
-close(gcbf);
+setappdata(0,ad.tag,ad);
+delete(gcbf);
 
 %% Cancel callback
 function doCancel(cancel_btn, evd, listbox) %#ok
-if isappdata(gcbf, 'ListDialogAppData__')
-  ad = getappdata(gcbf,'ListDialogAppData__');
+fig = get(listbox, 'Parent');
+ad = get(fig,'UserData');
+if isappdata(0, ad.tag)
+  ad = getappdata(0,ad.tag);
 end
 ad.value = 0;
 ad.selection = get(listbox,'value');
 string = get(listbox,'string');
 ad.selectionString = string(ad.selection);
 ad.button = 'Cancel';
-setappdata(gcbf,'ListDialogAppData__',ad)
-close(gcbf);
+setappdata(0,ad.tag,ad)
+delete(gcbf);
 
 %% SelectAll callback
 function doSelectAll(selectall_btn, evd, listbox) %#ok
