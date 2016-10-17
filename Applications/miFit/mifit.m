0f0fae2e0871feb53dad65e905e985e6acca8e8a
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-10-17 10:39:07 +0200

    App: miFit: added Optimizers/Criteria selection menu item

--- Applications/miFit/mifit.m
+++ Applications/miFit/mifit.m
@@ -617,7 +617,7 @@ function mifit_Optimizers_Configure(varargin)
   % change optimizer configuration parameters
   CurrentOptimizer = getappdata(mifit_fig,'CurrentOptimizer');
   mifit_disp([ '[Optimizers_Configure] Configure "' CurrentOptimizer '"' ]);
-  options    = getappdata(mifit_fig,'CurrentOptimizerConfig');
+  options    = getappdata(mifit_fig, 'CurrentOptimizerConfig');
   config     = getappdata(mifit_fig, 'Preferences');
   o.FontSize = config.FontSize;
   o.Name     = [ mfilename ': Optimizer "' CurrentOptimizer '" configuration' ];
@@ -642,7 +642,32 @@ function mifit_Optimizers_Configure(varargin)
   options = options1;
   setappdata(mifit_fig,'CurrentOptimizerConfig', options);
   
+
+function mifit_Optimizers_Criteria(varargin)
+% Optimizers/Criteria: open dialogue to change the fit criteria
   
+  crit = {'least_square = (|Signal-Model|/Error)^2 [non-robust]', ...
+    'least_absolute = |Signal-Model|/Error [robust]',...
+    'least_median = median(|Signal-Model|/Error) [robust]',...
+    'least_mean = mean(|Signal-Model|/Error) [robust]',...
+    'least_max = max(|Signal-Model|/Error) [non-robust]',...
+    'least_rfactor = (|Signal-Model|/Error)^2/(Signal/Error)^2 [non-robust]',...
+    'max_likelihood =  (|Signal-Model|/σ)^2 + 1/2 log(2πσ) [robust]', ...
+    'max_corrcoef = 1-corrcoef(Signal, Model)'};
+  selection = [];
+  if isappdata(mifit_fig,'CurrentOptimizerCriteria')
+    selection = find(strcmp(getappdata(mifit_fig,'CurrentOptimizerCriteria'), strtok(crit)),1);
+  end
+  if isempty(selection), selection = 1; end
+  % show operator selection dialogue
+  [selection, ok] = listdlg('ListString', crit, 'ListSize',[400 160], ...
+    'Name',[ 'miFit: Select the Fit Criteria ti use' ], ...
+      'PromptString', 'The Fit criteria is the quantity which is to be minimized. It is a function of the Data set Signal, and the Model value. It is then divided by the number of degrees of freedom.', ...
+      'InitialValue',selection);
+  if isempty(selection) || ok ~= 1, return; end
+  crit = strtok(crit{selection});
+  setappdata(mifit_fig,'CurrentOptimizerCriteria', crit);
+
 function selected = mifit_Optimizers_Set(varargin)
 % [internal] mifit_Optimizers_Set: set current optimizer, and check its label in the menu
   % set the optimizer to use
@@ -671,7 +696,7 @@ function selected = mifit_Optimizers_Set(varargin)
   set(hoptims,'Checked','off');
   set(hoptims(index),'Checked','on');
   
-% Tools menu *******************************************************************
+% Help menu *******************************************************************
 
 function mifit_Help_Main(varargin)
 % Help/Main: open web page for miFit
@@ -689,6 +714,10 @@ function mifit_Help_Models(varargin)
 function mifit_Help_Optimizers(varargin)
 % Help/Optimizers: open web page Optimizers
   doc(iData,'Optimizers');
+  
+function mifit_Help_Math(varargin)
+% Help/Math: open web page Math
+  doc(iData,'Math');
   
 % List Data and Stack management ***********************************************
 
