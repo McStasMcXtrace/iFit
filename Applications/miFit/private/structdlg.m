80cf9fbb27ee28141934964ba4a42243133d4fe9
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2016-12-08 16:25:32 +0100

    structdlg: better uitable display with comments in resizable columns

--- Applications/miFit/private/structdlg.m
+++ Applications/miFit/private/structdlg.m
@@ -83,31 +83,13 @@ function structure = structdlg(structure,options)
   % create a uitable from the structure fields and values
   f = figure('Name',options.Name, 'MenuBar','none', ...
       'NextPlot','new', ...
-      'Tag', options.Tag, ...
+      'Tag', options.Tag, 'Units','pixels', ...
       'CloseRequestFcn', ...
         [ 'setappdata(0,''' tmp_storage ''', get(findobj(gcbf,''Tag'',''' options.Tag '_Table' '''),''Data'')); delete(gcbf)' ]);
   
   % override default mechanism for Table update when closing
   if isfield(options, 'CloseRequestFcn') && ~strcmp(options.CreateMode, 'modal')
     set(f, 'CloseRequestFcn', options.CloseRequestFcn);
-  end
-  
-  % create the Table content to display, handle array of structures
-  % check if the structure values are numeric, logical, or char
-  Data0       = cell(numel(fields), numel(structure)+1);  % first column=fieldnames
-  fields_type = cell(size(Data0));
-  Data0(:,1)  = fields(:);
-  for index_s = 1:numel(structure)
-    Data0(:,index_s+1) = struct2cell(structure(index_s));
-    for index=1:numel(fields)
-      item = Data0{index,index_s+1};
-      % uitable only support char or scalar numeric/logical
-      flag = ischar(item) || (numel(item) ==1 && (isnumeric(item) || islogical(item)));
-      if ~flag
-        fields_type{index,index_s+1} = class(item);
-        Data0{index,index_s+1} = class2str('', Data0{index,index_s+1}, 'eval');
-      end
-    end
   end
   
   % sort ListString so that it matches the fields
@@ -124,13 +106,32 @@ function structure = structdlg(structure,options)
   end
   options.ListString = ListString;
   
+  % create the Table content to display, handle array of structures
+  % check if the structure values are numeric, logical, or char
+  Data0       = cell(numel(fields), numel(structure)+2);  % 1st column=ListString, % 2nd column=fieldnames
+  fields_type = cell(size(Data0));
+  Data0(:,1)  = ListString(:);
+  Data0(:,2)  = fields(:);
+  for index_s = 1:numel(structure)
+    Data0(:,index_s+2) = struct2cell(structure(index_s));
+    for index=1:numel(fields)
+      item = Data0{index,index_s+2};
+      % uitable only support char or scalar numeric/logical
+      flag = ischar(item) || (numel(item) ==1 && (isnumeric(item) || islogical(item)));
+      if ~flag
+        fields_type{index,index_s+2} = class(item);
+        Data0{index,index_s+2} = class2str('', Data0{index,index_s+2}, 'eval');
+      end
+    end
+  end
+
   % determine the window size to show
   TextWidth = 12;
   TextHeight= 30;
   % height is given by the number of fields
   height = (numel(options.ListString)+3)*TextHeight;
   % width is given by the length of the longest RowName
-  width = max(cellfun(@numel,options.ListString))*TextWidth + 10*options.FontSize;
+  width = max(cellfun(@numel,options.ListString))*TextWidth + numel(structure)*options.FontSize*5;
   % compare to current window size
   p = get(f, 'Position');
   p(3) = width;
@@ -138,19 +139,26 @@ function structure = structdlg(structure,options)
   set(f, 'Position',p);
   
   % set ColumnName
-  ColumnName = 'Field';
-  ColumnName = [ ColumnName num2cell(1:numel(structure)) ];
+  ColumnName = 'Description';
+  ColumnName = [ ColumnName 'Field' num2cell(1:numel(structure)) ];
   ColumnFormat='char';
-  ColumnFormat=[ ColumnFormat cell(1,numel(structure)) ];
+  ColumnFormat=[ ColumnFormat 'char' cell(1,numel(structure)) ];
+  ColumnEditable = [ false true ];
+  ColumnWidth = {max(cellfun(@numel,options.ListString))*options.FontSize/2, ...
+    max(cellfun(@numel,fields))*options.FontSize/2};
+  for index_s = 1:numel(structure)
+    ColumnEditable(end+1) = true;
+    ColumnWidth{end+1}    = 'auto';
+  end
 
   % create the table
   t = uitable('Parent',f, ...
-    'RowName',options.ListString, 'Data', Data0, ...
-    'ColumnEditable',true, ...
+    'Data', Data0, ...
+    'ColumnEditable',ColumnEditable, ...
     'Tag', [ options.Tag '_Table' ], ...
     'FontSize',options.FontSize, ...
     'Units','normalized', 'Position', [0.05 0.2 .9 .7 ], ...
-    'ColumnWidth','auto','TooltipString',options.TooltipString, ...
+    'ColumnWidth',ColumnWidth,'TooltipString',options.TooltipString, ...
     'ColumnName',ColumnName);
     
   % add context menu to add a field
@@ -181,9 +189,9 @@ function structure = structdlg(structure,options)
     
     if isempty(Data), return; end
     % get new field names
-    fields = genvarname(strtok(Data(:,1)));
-    Data   = Data(:,2:end);
-    fields_type = fields_type(:,2:end);
+    fields = genvarname(strtok(Data(:,2)));
+    Data   = Data(:,3:end);
+    fields_type = fields_type(:,3:end);
     % restore initial structure field type when set
     if prod(ad.size) == numel(Data)
       Data = reshape(Data, ad.size);
@@ -220,7 +228,8 @@ function structure = structdlg(structure,options)
     % append a line
     Data(end+1,:) = cell(1,size(Data,2));
     fields_type(end+1,:) = cell(1,size(Data,2));
-    Data{end,1}   = 'New'; 
+    Data{end,1}   = 'New field'; 
+    Data{end,2}   = 'New'; 
     set(ad.table, 'Data',Data);
   end
   
