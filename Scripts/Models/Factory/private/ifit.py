df930d6ceeaca3837bf714e90bff9858e14d8095
Emmanuel FARHI <farhi@ill.fr>
2017-02-27 11:48:00 +0100

    Models: Phonons: ifit.py: use spglib when installed.

--- Scripts/Models/Factory/private/ifit.py
+++ Scripts/Models/Factory/private/ifit.py
@@ -21,9 +21,9 @@
 
 # we need spacegroup from ASE
 try:
-    from ase.spacegroup import Spacegroup
+    from ase.spacegroup import Spacegroup         # For ASE version 3.10 or later
 except ImportError:
-    from ase.lattice.spacegroup import Spacegroup
+    from ase.lattice.spacegroup import Spacegroup # For ASE version 3.9 or before
 
 import sys
 import pickle
@@ -33,6 +33,19 @@ import warnings
 from ase.atoms import Atoms
 from ase.utils import opencew
 from ase.parallel import rank, barrier
+
+# check if we have access to get_spacegroup from spglib
+# https://atztogo.github.io/spglib/
+has_spglib = False
+try:
+    import spglib                   # For version 1.9 or later
+    has_spglib = True
+except ImportError:
+    try:
+        from pyspglib import spglib # For versions 1.8.x or before
+        has_spglib = True
+    except ImportError:
+        pass
 
 # ------------------------------------------------------------------------------
 def get_spacegroup(atoms, symprec=1e-5):
@@ -54,13 +67,26 @@ def get_spacegroup(atoms, symprec=1e-5):
     >>> atoms = bulk("Cu", "fcc", a=3.6, cubic=True)
     >>> sg = get_spacegroup.get_spacegroup(atoms)
     """ 
-    #
-    # we try all available spacegroups from 1 to 230
-    # a Space group is the collection of all symmetry operations which let the 
+    
+    # use spglib when it is available (and return)
+    if has_spglib:
+        sg    = spglib.get_spacegroup(atoms)
+        sg_no = int(sg[sg.find("(")+1:sg.find(")")])
+        atoms.info["spacegroup"] = Spacegroup(sg_no)
+        return atoms.info["spacegroup"]
+    
+    # no spglib, we use our own spacegroup finder. Not as robust as spglib.
+    
+    # we try all available spacegroups from 1 to 230, backwards
+    # a Space group is the collection of all symmetry operations which lets the 
     # unit cell invariant.
     found    = None
     positions  = atoms.get_scaled_positions(wrap=True)  # in the lattice frame
-    positions -= positions[0] # make sure we are insensitive to translation
+    
+    # make sure we are insensitive to translation. this choice is arbitrary and 
+    # could lead to a 'slightly' wrong guess for the Space group, e.g. do not  
+    # guess centro-symmetry.
+    positions -= positions[0] 
     
     # search space groups from the highest symmetry to the lowest
     # retain the first match
@@ -147,6 +173,8 @@ def equivalent_sites(sg, scaled_positions, onduplicates='keep',
         scaled = numpy.array(scaled_positions, ndmin=2)
         for kind, pos in enumerate(scaled):
             for rot, trans in sg.get_symop():
+                # check if R.x + T = x' is in the atoms list (equivalent site)
+                # x is expressed in fractional/scaled/direct/lattice coordinates
                 site = numpy.mod(numpy.dot(rot, pos) + trans, 1.)
                 if not sites:
                     sites.append(site)
@@ -228,9 +256,9 @@ def phonons_run(phonon):
 
                 # Update atomic positions. Shift is applied to Cartesian coordinates.
                 disp    = [0,0,0]
-                disp[i] = sign * phonon.delta
+                disp[i] = sign * phonon.delta # in Angs, cartesian
                 atoms_N.positions[offset + a, i] = \
-                    pos[a, i] + sign * phonon.delta
+                    pos[a, i] + disp[i]
                 
                 # Call derived class implementation of __call__
                 output = phonon.__call__(atoms_N)
@@ -266,7 +294,9 @@ def _phonons_run_symforce(phonon, atoms, disp, force, a):
     # equivalent_sites uses the scaled positions, so we must convert the 'disp'
     # from Cartesian to lattice frame
     disp_lat = numpy.dot(invL, disp)
+    disp_lat = numpy.mod(disp_lat, 1.)
     
+    # search for all [disp_lat*rot+trans] equivalents
     disps_lat,kinds,rot,trans = equivalent_sites(atoms.info["spacegroup"], \
                 disp_lat, onduplicates='keep')
     
@@ -275,8 +305,11 @@ def _phonons_run_symforce(phonon, atoms, disp, force, a):
     #   fractions in the cell. The cell frame may not be ortho-normal. This frame
     #   is also labeled as 'Direct'.
     #   atoms.scaled_positions is using the Direct/lattice frame
+    #       fractional = np.linalg.solve(self.cell.T, self.positions.T).T
+    #                  = inv(self.cell) * positions
     # Cartesian coordinates use an ortho-normal frame.
     #   atoms.positions is using the cartesian frame
+    #       positions = np.dot(scaled_positions, self._cell)
     #   displacements in ASE are in the cartesian frame
     
     # **** Transformations *****************************************************
@@ -296,20 +329,24 @@ def _phonons_run_symforce(phonon, atoms, disp, force, a):
             
             # compute the 'planned' displacements (atom, xyz, sign)
             new_disp    = [0,0,0]
-            new_disp[i] = sign * phonon.delta
+            new_disp[i] = sign * phonon.delta # in Angs, cartesian
             # convert this displacement from cartesian to lattice frame
             new_disp_lat= numpy.dot(invL, new_disp)
-            # modulo in the cell, Direct
+            # modulo in the cell, Direct/scaled/lattice/fractional
             new_disp_lat= numpy.mod(new_disp_lat, 1.)
             
             # does this move belongs to the equivalent displacements ?
             if new_disp_lat in disps_lat:
                 # if found in sites, we apply rotation to forces
                 # get the index of the matching site for rot and trans
-                index = numpy.where((disps_lat == new_disp_lat).all(axis=1))
-                try:
-                    index=index[0][0]
-                except IndexError:
+                index = numpy.where(numpy.isclose(disps_lat,new_disp_lat).all(axis=1))
+                if len(index):
+                    index = index[0]
+                if len(index) > 1:
+                    print "WARNING: ifit:phonons:run_symforce: multiple equivalent sites ! Using first out of ", index
+                if len(index):                    
+                    index=index[0]
+                else:
                     continue  # no symmetry operator found
                     
                 # skip if the pickle already exists
