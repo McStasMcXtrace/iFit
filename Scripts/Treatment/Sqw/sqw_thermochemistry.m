807918efb8f0317bbee6136a9efee65d59bf826e
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2017-05-10 14:12:09 +0200

    Treat: Phonons: DOS and thermo chem stuff now in sqw_thermo script

--- Scripts/Treatment/Sqw/sqw_thermochemistry.m
+++ Scripts/Treatment/Sqw/sqw_thermochemistry.m
@@ -1,18 +1,28 @@
-function [s,f]=sqw_phonons_dos(s, T)
-% sqw_phonons_dos: compute the vDOS and thermodynamic quantities
+function [s,f]=sqw_thermochemistry(s, T)
+% sqw_thermochemistry: compute the vDOS and thermodynamic quantities.
 %
-% compute the vDOS from the evaluation of the 4D sqw_phonons object
-% then compute thermodynamics quantities.
+% Compute the phonon (vibrational) density of states (vDOS) from e.g. the evaluation
+% of the 4D sqw_phonons object. Then compute thermodynamics quantities.
+%
 % The input 4D model should be e.g. a 4D phonon one, with axes qh,qk,ql,energy
-% in [rlu^3,meV]. The evaluation of he model should provide the bare excitation
-% frequencies into the UserData.FREQ, in [meV].
+% in [rlu^3,meV]. The evaluation of the model should provide the density of states
+% in model.UserData.DOS as an iData object.
+%
+% the function returns an array of iData objects:
+%   DOS 
+%   pDOS (one per mode)
+%   entropy                           S [eV/K/cell]
+%   internal_energy                   U [eV/cell]
+%   helmholtz_energy                  F [eV/cell]
+%   heat_capacity at constant volume Cv [eV/K/cell]
 %
 % input:
-%   s: a 4D sqw_phonons model (iFunc)
+%   s: a 4D sqw_phonons model [K,H,L,Energy] (iFunc)
+%   T: temperature range. When not given, T=1:500 [K]
 %
 % output:
-%   s: enriched model
-%   f: iData holding the 
+%   s: [DOS, partial_DOS, S U F Cv ]
+%   f: iData holding the 4D evaluation of the model.
 
 f = []; 
 % test the input object
@@ -35,39 +45,32 @@ end
 
 % first get a quick estimate of the max frequency
 if ~isfield(s.UserData,'maxFreq')
-  qh=linspace(0.01,.5,10);qk=qh; ql=qh; w=linspace(0.01,50,11);
+  qh=linspace(-.5,.5,10);qk=qh; ql=qh; w=linspace(0.01,50,11);
   f=iData(s,[],qh,qk,ql',w);
   s.UserData.maxFreq = max(s.UserData.FREQ(:));
   disp([ mfilename ': maximum phonon energy ' num2str(s.UserData.maxFreq) ' [meV] in ' s.Name ]);
 end 
 
-% re-evaluate the 4D model onto a mesh filling the Brillouin zone [-0.5:0.5]
+% evaluate the 4D model onto a mesh filling the Brillouin zone [-0.5:0.5 ]
+s.UserData.DOS     = [];  % make sure we re-evaluate again on a finer grid
 s.UserData.maxFreq = max(s.UserData.maxFreq(:));
 qh=linspace(-0.5,.5,30);qk=qh; ql=qh; w=linspace(0.01,s.UserData.maxFreq*1.2,51);
 f=iData(s,[],qh,qk,ql',w);
 
 % then get back the bare frequencies
-if ~isfield(s.UserData,'FREQ')
-  disp([ mfilename ': The object should have the bare frequencies available as UserData.FREQ [meV].' ]);
+if ~isfield(s.UserData,'DOS')
+  disp([ mfilename ': The object has no density of states available. Can not compute thermo-chemistry.' ]);
   return
 end
-FREQ = s.UserData.FREQ; % in meV
 
-% compute the DOS
-index= find(FREQ > 0);
-[dos_e,omega_e]=hist(FREQ(index),50);
-Escaling = findfield(s, 'Energy_scaling', 'first');
-if ~isempty(Escaling), Escaling = get(s, Escaling); end
-if isnumeric(Escaling) && Escaling > 0, omega_e=omega_e*Escaling(1); end
-DOS=iData(omega_e,dos_e./sum(dos_e));
-DOS.Title = [ 'Total DOS ' strtok(s.Name) ]; 
-xlabel(DOS,'Energy [meV]'); 
-ylabel(DOS,'Total DOS');
-UD = s.UserData;
-DOS.Error=0; UD.DOS=DOS;
+% get the DOS
+DOS     = s.UserData.DOS;
+omega_e = DOS{1};
+dos_e   = DOS{0};
 
-% clean up the energy axis
-omega_e = omega_e/1000; % eV
+% renormalize the dos in eV
+omega_e = omega_e/1000; % meV -> eV
+dos_e   = dos_e*1000;
 
 % get the temperature
 if nargin < 2
@@ -89,7 +92,7 @@ if isfield(s.UserData,'properties') && isfield(s.UserData.properties,'potential_
   potential_energy = s.UserData.properties.potential_energy;
 else potential_energy = 0; end
 
-U = get_internal_energy(omega_e, dos_e, T, potential_energy);
+U = get_internal_energy(omega_e/1000, dos_e, T, potential_energy);
 internal_energy = iData(T, U);
 internal_energy.Title=[ 'Internal energy U [eV/cell] ' strtok(s.Name) ]; 
 xlabel(internal_energy,'Temperature [K]'); ylabel(internal_energy,'U [eV/cell]');
@@ -110,34 +113,20 @@ Cv.Error=0; UD.heat_capacity = Cv;
 
 s.UserData = UD;
 
-% update the object
-if nargout == 0 && ~isempty(inputname(1))
+if ~isempty(inputname(1))
   assignin('caller',inputname(1),s);
 end
 
-function D = get_total_dos(s, npts, delta)
-  % compute the total Phonon Density of States
-  if nargin < 2, npts = []; end
-  if nargin < 3, delta= []; end
-  if isempty(npts), npts=1000; end
-  if isempty(delta), delta = 0.5; end
-
-  omega_kl = s.UserData.FREQ; % in meV
-  omega_e  = linspace(0,max(FREQ(:))*1.2, npts);
-  dos_e    = zeros(size(omega_e));
-  delta    = 0.05; % meV smoothing
-
-  omega_el = repmat(omega_e, [3 1])';
-
-  for index=1:size(omega_kl,1)  % along q values
-    diff_el = ( omega_el - repmat(omega_kl(index,:),[npts 1]) ).^2;
-    dos_el  = 1./ (diff_el + (0.5*delta^2)); 
-    dos_e   = dos_e + sum(dos_el,2)';
-  end
+% return all
+s = [s.UserData.DOS s.UserData.pDOS ...
+    s.UserData.entropy s.UserData.internal_energy s.UserData.helmholtz_energy ...
+    s.UserData.heat_capacity ];
 
 % ------------------------------------------------------------------------------
 function U = get_internal_energy(omega_e, dos_e, T, potential_energy)
   % get the internal energy U in eV/unit cell
+  % 
+  % omega_e: in eV
   
   if nargin < 3, T=[]; end
   if nargin < 4, potential_energy=0; end
@@ -153,19 +142,21 @@ function U = get_internal_energy(omega_e, dos_e, T, potential_energy)
     
   U = potential_energy;
   % zero-point-energy
-  zpe = trapz(omega_e.*dos_e/2, omega_e);
+  zpe = trapz(omega_e, omega_e.*dos_e/2);
 
   T2E       = (1/11.6045);           % Kelvin to meV = 1000*K_B/e
-  kT        = T*T2E;
+  kT        = T*T2E/1000;
   hw_kT     = omega_e./kT;           % hbar omega / kT
     
   U         = U + zpe;
   E_vib     = omega_e ./ (exp(hw_kT) - 1);
-  E_phonon  = trapz(E_vib .* dos_e, omega_e);
+  E_phonon  = trapz(omega_e, E_vib .* dos_e);
   U         = U + E_phonon;
 
 function S = get_entropy(omega_e, dos_e, T)
   % get the entropy in eV/K/unit cell
+  % 
+  % omega_e: in eV
   
   if nargin < 3, T=[]; end
   if isempty(T), T=300; end
@@ -179,7 +170,7 @@ function S = get_entropy(omega_e, dos_e, T)
   end
   
   T2E       = (1/11.6045);    % Kelvin to meV = 1000*K_B/e
-  kT        = T*T2E;
+  kT        = T*T2E/1000;
   hw_kT     = omega_e./kT;    % hbar omega / kT
   k = 1.380658e-23;           % Boltzmann constant, J/K
   e = 1.60217733e-19;         % elementary charge,  C
@@ -187,5 +178,5 @@ function S = get_entropy(omega_e, dos_e, T)
   
   S_vib = (omega_e ./ (T * (exp(hw_kT) - 1.)) - kB * log(1. - exp(-hw_kT)));
                  
-  S = trapz(S_vib .* dos_e, omega_e);
+  S = trapz(omega_e, S_vib .* dos_e);
 
