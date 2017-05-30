9a9c3cbe153150b385b693e3d6a929e3c8f278d0
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2017-05-30 11:01:14 +0200

    Models: Phonons: fix DHO script to work when e.g. 'UserData.properties' does   not exist.

--- Tests/Integration/Scripts/Models/test_Models_phonons_ASE.m
+++ Tests/Integration/Scripts/Models/test_Models_phonons_ASE.m
@@ -1,6 +1,6 @@
 function result = test_Models_phonons_ASE
 
-  % spin wave
+  % phonon Al
   s=sqw_phonons('defaults');
   if isempty(s)
     result = [ 'OK     ' mfilename ' SKIPPED: ASE not available'];
@@ -11,7 +11,8 @@ function result = test_Models_phonons_ASE
   fig=figure; plot3(log(f(:,:,1,:)));
   [w1,c1]=std(f(:,:,1,:),3);
   
-  if abs(c1-13) < 3 && abs(w1-9.8) < 0.4
+  if (abs(c1-13) < 3 && abs(w1-9.8) < 0.4) ...
+  || (abs(c1-22) < 3 && abs(w1-8) < 0.4)
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
