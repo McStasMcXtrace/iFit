5b7807a24c5547260fe1dc828e2c77ea9d63b496
Emmanuel Farhi <farhi@ill.fr>
2017-06-09 14:56:15 +0200

    Loaders: NPY: fix magic key

---
+++ Libraries/Loaders/read_npy.m
@@ -0,0 +1,52 @@
+function s = read_npy(filename)
+% data = read_npy(filename)
+%
+% Read a Python Numpy array '.npy' file.
+%   can handle: 'uint8','uint16','uint32','uint64
+%               'int8','int16','int32','int64'
+%               'single','double', 'logical'
+%
+% References:
+% format specification: https://docs.scipy.org/doc/numpy/neps/npy-format.html
+% contrib: https://github.com/kwikteam/npy-matlab, C. Rossant 2016
+
+
+% Function to read NPY files into matlab. 
+% *** Only reads a subset of all possible NPY files, specifically N-D arrays of certain data types. 
+% See https://github.com/kwikteam/npy-matlab/blob/master/npy.ipynb for
+% more. 
+%
+s = [];
+
+% in private dir.
+[s.shape, s.dataType, s.fortranOrder, s.littleEndian, s.totalHeaderLength, s.npyVersion] = readNPYheader(filename);
+
+if s.littleEndian
+    fid = fopen(filename, 'r', 'l');
+else
+    fid = fopen(filename, 'r', 'b');
+end
+
+try
+    
+    [~] = fread(fid, s.totalHeaderLength, 'uint8');
+
+    % read the data
+    data = fread(fid, prod(s.shape), [s.dataType '=>' s.dataType]);
+    
+    if length(s.shape)>1 && ~s.fortranOrder
+        data = reshape(data, s.shape(end:-1:1));
+        data = permute(data, [length(s.shape):-1:1]);
+    elseif length(s.shape)>1
+        data = reshape(data, s.shape);
+    end
+    s.data = data;
+    fclose(fid);
+    
+catch me
+    s = [];
+    fclose(fid);
+    rethrow(me);
+end
+
+
---
+++ Scripts/Load/opennpy.m
@@ -0,0 +1,29 @@
+function out = opennpy(filename)
+%OPENEDF Open an python Numpy NPY Format file, display it
+%        and set the 'ans' variable to an iData object with its content
+% (c) E.Farhi, ILL. License: EUPL.
+
+
+if ~isa(filename,'iData')
+  out = iData(filename,'NPY');
+else
+  out = filename;
+end
+clear filename;
+
+if numel(out) > 1
+  % handle input iData arrays
+  for index=1:numel(out)
+    out(index) = feval(mfilename, out(index));
+  end
+end
+
+if ~nargout
+  figure; subplot(out);
+  
+  if ~isdeployed
+    assignin('base','ans',out);
+    ans = out
+  end
+end
+
