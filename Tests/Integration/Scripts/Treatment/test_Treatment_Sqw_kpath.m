056cef44ed97af0f77ab8bd242962499a6d66594
Emmanuel FARHI <farhi@ill.fr>
2017-06-21 12:32:19 +0200

    Trest: Sqw kpath: added test.

---
+++ Tests/Integration/Scripts/Treatment/test_Treatment_Sqw_kpath.m
@@ -0,0 +1,12 @@
+function result = test_Treatment_Sqw_kpath
+
+  Sqw   = sqw_cubic_monoatomic('defaults');
+  [d,k, fig] = sqw_kpath(Sqw, '','','plot THz');
+  close(fig);
+  
+  if abs(std(d,2)-1.5) < .2
+    result = [ 'OK     ' mfilename ];
+  else
+    result = [ 'FAILED ' mfilename ];
+  end 
+ 
