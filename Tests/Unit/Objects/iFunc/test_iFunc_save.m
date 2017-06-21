7dbe1bfb3bc07dccb170a241faa6a12d99d46e32
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2017-06-21 12:31:45 +0200

    Test: iFunc: save: also test for restore of the object.

--- Tests/Unit/Objects/iFunc/test_iFunc_save.m
+++ Tests/Unit/Objects/iFunc/test_iFunc_save.m
@@ -2,7 +2,8 @@ function result=test_iFunc_save
 
 a=iFunc('x*p(1)+p(2)'); 
 h = save(a);
-if ischar(h)
+b = load(iFunc,h);
+if ischar(h) && isa(b, 'iFunc') && strcmp(a.Expression, b.Expression)
   result = [ 'OK     ' mfilename ];
   delete(h)
 else
