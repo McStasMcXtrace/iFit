0a0b8b3e9ee0c32caa99ad9ba3f95af06470bf43
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2017-06-27 13:53:20 +0200

    Treat: Sqw: thermo: add plot option

--- Scripts/Treatment/Sqw/sqw_thermochemistry.m
+++ Scripts/Treatment/Sqw/sqw_thermochemistry.m
@@ -1,4 +1,4 @@
-function t=sqw_thermochemistry(s, T)
+function t=sqw_thermochemistry(s, T, options)
 % sqw_thermochemistry: compute thermodynamic quantities.
 %
 % compute thermodynamics quantities.
@@ -20,6 +20,7 @@ function t=sqw_thermochemistry(s, T)
 % input:
 %   s: a 4D sqw_phonons model [K,H,L,Energy] (iFunc)
 %   T: temperature range. When not given, T=1:500 [K]
+%   options: can be 'plot' to display the results.
 %
 % output:
 %   t: structure with [DOS, DOS_partials, S U F Cv ]
@@ -27,6 +28,8 @@ function t=sqw_thermochemistry(s, T)
 t=[];
 % test the input object
 if nargin == 0, return; end
+if nargin < 2, T=[]; end
+if nargin < 3, options=[]; end
 
 % must be 2D or 4D iFunc/iData.
 if (~isa(s,'iFunc') && ~isa(s,'iData')) || ~any(ndims(s) == [2 4])
@@ -35,14 +38,15 @@ if (~isa(s,'iFunc') && ~isa(s,'iData')) || ~any(ndims(s) == [2 4])
 end
 
 % get the temperature
-if nargin < 2
-  T = 1:500;  % default
-  try
-    T = Sqw_getT(s);  % a value is defined in the Parameters ?
-    if isscalar(T)
-      disp([ mfilename ': found temperature T=' num2str(T) ' [K] in ' s.Name ]);
-    end
+if isempty(T)
+
+  T = Sqw_getT(s);  % a value is defined in the Parameters ?
+  if isempty(T), 
+    T = 1:500;  % default
+  elseif isscalar(T)
+    disp([ mfilename ': found temperature T=' num2str(T) ' [K] in ' s.Name ]);
   end
+  
 end
 
 DOS     = sqw_phonon_dos(s);
@@ -106,6 +110,34 @@ t.entropy         =s.UserData.entropy;            % S
 t.internal_energy =s.UserData.internal_energy;    % U
 t.helmholtz_energy=s.UserData.helmholtz_energy;   % F = U-TS
 t.heat_capacity   =s.UserData.heat_capacity;      % Cv = dU/dT
+
+if nargout == 0 || ~isempty(strfind(options, 'plot'))
+  fig = figure;
+  subplot([ t.entropy t.internal_energy t.helmholtz_energy t.heat_capacity ], [3 2]);
+  hold on
+  if isfield(s.UserData,'DOS') && ~isempty(s.UserData.DOS)
+      subplot(3,2,5);
+      DOS = s.UserData.DOS;
+      DOS{1} = DOS{1}; % change energy unit
+      xlabel(DOS,[ 'Energy [meV]' ]);
+      % plot any partials first
+      if isfield(s.UserData,'DOS_partials') && numel(s.UserData.DOS_partials) > 0
+        d=s.UserData.DOS_partials;
+        for index=1:numel(d)
+          this_pDOS=d(index);
+          this_pDOS{1} = this_pDOS{1};
+          d(index) = this_pDOS;
+        end
+        h=plot(d);
+        if iscell(h), h=cell2mat(h); end
+        set(h,'LineStyle','--');
+        hold on
+      end
+      % plot total DOS and rotate
+      h=plot(DOS); set(h,'LineWidth',2);
+    end
+end
+
 
 % ------------------------------------------------------------------------------
 function U = get_internal_energy(omega_e, dos_e, T, potential_energy)
