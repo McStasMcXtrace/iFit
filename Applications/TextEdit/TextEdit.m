e900411f71fc3f094fae24fcc8eaa1746ab848a3
Emmanuel FARHI <farhi@ill.fr>
2017-07-12 22:07:54 +0200

    TextEdit: clean code

--- Applications/TextEdit/TextEdit.m
+++ Applications/TextEdit/TextEdit.m
@@ -9,12 +9,6 @@ function hF = TextEdit(filename, options)
 %    Display the string inside a new Editor window
 %  TextEdit(..., name)
 %    Give the TextEdit a name
-%  TextEdit(filename|string, options)
-%    with options as a structure:
-%      name:    the name of the TextEdit
-%      command: when 1, displays a command line, in which case the main pane is not
-%               editable. Commands entered in this area are echoed in the main 
-%               pane and executed.
 %
 % $ Initially from: Jorge De Los Santos $
 % $ E-mail: delossantosmfq@gmail.com $
@@ -34,9 +28,6 @@ if ischar(options) name = options; options=''; options.name = name; end
 
 if ~isstruct(options) || ~isfield(options, 'name') || isempty(options.name), 
   options.name    = mfilename;
-end
-if ~isfield(options, 'command') || isempty(options.command)
-  options.command = 0;
 end
 
 % build the main window
@@ -90,27 +81,8 @@ try
     hTxt = jCodePane;
     % set the editor panel to whole figure, normalised.
     set(hContainer,'Units',   'normalized')
- 
-    if options.command
-      set(hContainer,'Position',[0 0.1 1 0.9])
-      % we create an other ScrollPane with highlight syntax and a 'Run' button
-      jCodePane2 = com.mathworks.widgets.SyntaxTextPane;
-      codeType = jCodePane2.M_MIME_TYPE;  % jCodePane.contentType='text/m-MATLAB'
-      jCodePane2.setContentType(codeType)
-      str = ['% enter Matlab commands here\n' ];
-      str = sprintf(strrep(str,'%','%%'));
-      jCodePane2.setText(str)
-      jScrollPane2 = com.mathworks.mwswing.MJScrollPane(jCodePane2);
-      [jhPanel2,hContainer2] = javacomponent(jScrollPane2,[10,10,300,100],hF);
-      set(hContainer2,'Units',   'normalized')
-      set(hContainer2,'Position',[0 0 1 0.1]);
-      options.command_pane_java = jCodePane2;
-      options.command_pane_uicontrol = hContainer2;
-    else
-      set(hContainer,'Position',[0 0 1 1])
-      options.command_pane = [];
-      options.command_pane_uicontrol = [];
-    end
+    set(hContainer,'Position',[0 0 1 1])
+
     options.display_pane = jCodePane;
     options.display_pane_uicontrol = hContainer;
     
@@ -129,26 +101,10 @@ if use_fallback
     'BackgroundColor','w','Horizontal','left',...
     'Tag','TextEdit_Text', ...
     'Max',1000,'FontSize',10,'FontName','Arial');
-    
-  % Align menu
-  h_AT=uimenu(hME,'Label','Align');
-  uimenu(h_AT,'Label','Left','Callback',@textedit_align);
-  uimenu(h_AT,'Label','Right','Callback',@textedit_align);
-  uimenu(h_AT,'Label','Center','Callback',@textedit_align);
-
-  % Theme selection
-  hMT=uimenu(hF,'Label','Layout');
-  uimenu(hMT,'Label','Blue','Callback',@textedit_theme);
-  uimenu(hMT,'Label','Classic','Callback',@textedit_theme);
-  uimenu(hMT,'Label','Cool','Callback',@textedit_theme);
-  uimenu(hMT,'Label','Dark','Callback',@textedit_theme);
-  uimenu(hMT,'Label','Silver','Callback',@textedit_theme);
   
   % more in Edit menu
-  uimenu(hME,'Label','Change Font Color...','Callback',@textedit_fontcolor, 'Separator','on');
   uimenu(hME,'Label','Change Font...','Callback',@textedit_font);
   options.display_pane = hTxt;
-  options.command = 0;
 end
 
 % protect figure from over-plotting
@@ -287,14 +243,6 @@ end
         textedit_setText(options.display_pane, '');
     end
 
-% Select font color
-    function textedit_fontcolor(~,~)
-      if ~isa(options.display_pane, 'com.mathworks.widgets.SyntaxTextPane')
-        clr=uisetcolor([1 1 1]);
-        set(options.display_pane,'ForegroundColor',clr);
-      end
-    end
-
 % Select Font
     function textedit_font(~,~)
         if ~isa(options.display_pane, 'com.mathworks.widgets.SyntaxTextPane')
@@ -310,29 +258,6 @@ end
         end
     end
 
-% Align text
-    function textedit_align(src,~)
-      if ~isa(options.display_pane, 'com.mathworks.widgets.SyntaxTextPane')
-        alin=get(src,'Label');
-        set(options.display_pane,'Horizontal',alin);
-      end
-    end
-
-% Modify the Theme/color set
-    function textedit_theme(src,~)
-        tipo=get(src,'Label');
-        MTEM={'Blue',[0.2 0.2 0.95],[1 1 1];
-            'Classic',[1 1 1],[0 0 1];
-            'Cool',[0.9 0.5 0.9],[1 1 0];
-            'Dark',[0 0 0],[1 1 1];
-            'Silver',[0.6 0.6 0.6],[1 1 1]};
-        k=strfind(MTEM(:,1),tipo);
-        k=cellfun(@isempty,k);
-        if ~isa(options.display_pane, 'com.mathworks.widgets.SyntaxTextPane')
-          set(options.display_pane,'BackgroundColor',MTEM{find(~k),2});
-          set(options.display_pane,'ForegroundColor',MTEM{find(~k),3});
-        end
-    end
 
 % About
     function textedit_about(~,~)
@@ -340,7 +265,7 @@ end
         devel='By: Jorge De Los Santos; Improved by E. Farhi, ILL, 2017.';
         e_mail='E-mail: delossantosmfq@gmail.com; farhi@ill.fr';
         blog='Blog: http://matlab-typ.blogspot.mx ; http://ifit.mccode.org';
-        nvrs='TextEdit 1.0';
+        nvrs='TextEdit 2.0';
         
         str = { devel, e_mail, blog, nvrs };
         helpdlg(str, nvrs);
