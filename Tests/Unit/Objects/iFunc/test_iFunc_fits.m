df06e2432b303df2a2f8ba21fd925093f8dbf66e
Emmanuel Farhi <farhi@ill.fr>
2017-07-27 11:41:52 +0200

    Tests: more tests for iFunc/Optimizers

--- Tests/Unit/Objects/iFunc/test_iFunc_fits.m
+++ Tests/Unit/Objects/iFunc/test_iFunc_fits.m
@@ -3,7 +3,10 @@ function result = test_iFunc_fits
   a=load(iData, [ ifitpath 'Data/sv1850.scn' ]);
   p=fits(a,'','','fminimfil');
   
-  if abs(max(abs([ 0.61         1.0008      0.0035         0.0001 ])-abs(p))) < 0.01
+  p2 = fits(a,'',struct(),'fminimfil');
+  
+  if abs(max(abs([ 0.61 1.0008 0.0035 0.0001 ])-abs(p))) < 0.1 ...
+  && isstruct(p2) && abs(max(abs([ 0.61 1.0008 0.0035 0.0001 ])-abs(cell2mat(struct2cell(p2)')))) < 0.1
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
