fb543302b3e28c4b09700267853431a8bd191928
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2017-12-05 16:11:07 +0100

    Neutron scatt: search for Sqw parameters extends from T to other physical quantities.

--- Objects/iData_subclasses/@iData_Sqw2D/private/Sqw_getT.m
+++ Objects/iData_subclasses/@iData_Sqw2D/private/Sqw_getT.m
@@ -1,37 +1,45 @@
-function  T = Sqw_getT(s)
-% Sqw_getT: search for a Temperature value in a data set
+function  T = Sqw_getT(s, prop)
+% Sqw_getT: search for a property value in a data set
 %
 % input:
-%   s: any iData object, including S(q,w) ones.
+%   s: any iData object, including S(q,w) and DOS ones.
   
   T = [];
+  if nargin < 2,    prop = []; end
+  if isempty(prop), prop={'Temperature','T'}; end
+  if ~iscellstr(prop), prop = { prop }; end
   
   % handle arrays
   if numel(s) > 1
     for index=1:numel(s)
-      t = Sqw_getT(s(index));
+      t = Sqw_getT(s(index), prop);
       if isempty(t), t=nan; end
       T = [ T t ];
     end
     return
   end
   
-  if isfield(s,'temperature'), T=getfield(s,'temperature'); end
-  
-  if isempty(T)
-    f = findfield(s,{'Temperature','T'},'exact numeric');
-    if ~isempty(f), T = s.(f{1}); end
-  end
-
-
-  if isempty(T)
-    f = findfield(s,{'Temperature','T'},'numeric cache');
-    for index=1:numel(f)
-        try
-            T = s.(f{index});
-            break
-        end
+  for field=prop
+    if isfield(s,field{1}), T=get(s,field{1}); end
+    
+    if isempty(T) || all(T(:)<0) || ~isnumeric(T) || ~isvector(T)
+      f = findfield(s,field{1},'exact numeric');
+      if ~isempty(f), T = get(s,f{1}); end
     end
+
+    if isempty(T) || all(T(:)<0) || ~isnumeric(T) || ~isvector(T)
+      f = findfield(s,field{1},'numeric cache');
+      for index=1:numel(f)
+        try
+          T = get(s,f{1});
+          if ~isempty(T) && isnumeric(T) && isvector(T) && all(T(:)>0), break; end
+        end
+      end
+    end
+
+    if ~isempty(T) && isnumeric(T) && isvector(T) && all(T(:)>0), break; end
   end
   
   if isvector(T), T = mean(T(:)); else T=[]; end
+  
+ 
