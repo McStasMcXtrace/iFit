f89a3aba39c97012ba2fc0c73248136fade3aa9b
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-08-06 15:14:44 +0200

    iData_Sqw2D: check/get Temperature can be obtained from Bose.

--- Objects/iData_subclasses/@iData_Sqw2D/private/Sqw_getT.m
+++ Objects/iData_subclasses/@iData_Sqw2D/private/Sqw_getT.m
@@ -3,6 +3,9 @@ function  T = Sqw_getT(s, prop, raw)
 %
 %   T = Sqw_getT(s, prop, raw)
 %
+%   T = Sqw_getT(s, prop, 'Bose')
+%     compute the temperature from the Bose factor (detailed balance).
+%
 % input:
 %   s: any iData object, including S(q,w) and DOS ones.
 %   prop: a list of equivalent properties (numeric) to search for.
@@ -11,10 +14,20 @@ function  T = Sqw_getT(s, prop, raw)
 % output:
 %   T: the property value (mean value when 'raw' not specified)
 
-  if nargin < 3, raw = false; else raw = true; end
+  if nargin < 2,    prop = []; end
+  if nargin ==3 && strcmpi(raw, 'Bose')
+    raw = 'Bose';
+  elseif nargin < 3
+    raw = false; 
+  else raw = true; end
   
   T = [];
-  if nargin < 2,    prop = []; end
+  if strcmp(raw, 'Bose')
+    prop= {'Temperature','T'};
+    T   = Sqw_getT_Bose(s);
+    if ~isempty(T), return; end
+  end
+  
   if isempty(prop), prop={'Temperature','T'}; end
   if ~iscellstr(prop), prop = { prop }; end
   
@@ -56,4 +69,51 @@ function  T = Sqw_getT(s, prop, raw)
     if isvector(T), T = mean(T(:)); else T=[]; end % also for scalars
   end
   
+% ------------------------------------------------------------------------------
+  
+function T = Sqw_getT_Bose(s)
+  % Compute the temperature from the Bose factor
+  
+  w = getaxis(s,1);
+  T = [];
+  
+  if any(w(:) < 0) && any(w(:) > 0)
+    % restrict the energy axis to the common +/- range
+    w1 = max(w(:)); w2 = max(-w(:)); w_max = min([w1 w2]);
+
+    if w1 ~= w_max || w2 ~= w_max
+      s_res  = ylim(s, [-w_max w_max]); % restricted to [-w:w] range
+    else
+      s_res = s;
+    end
+    % get axes
+    w = getaxis(s_res,1);
+    
+    % we compare the s(q,w) and s(q,-w)
+    s_opp = setaxis(s_res, 1, -w);
+    s_opp = sum(s_opp,2); s_opp = sort(s_opp, 1);
+
+    s_res = sum(s_res,2); s_res = sort(s_res, 1);
+
+    % the ratio should be S(q,w)/S(q,-w) = exp(hw/kT)
+    % so log(S(q,w)) - log(S(q,-w)) = hw/kT
+    wrn = warning;
+    warning off;
+    log_s_ratio = log(s_res) - log(s_opp);
+    warning(wrn);
+    w = getaxis(log_s_ratio,1);
+    clear s_res s_opp
+    
+    % mean_log_ratio = mean(log_s_ratio,0);
+    % std_log_ratio  = std(log_s_ratio,0);
+    
+    % compute the temperature from the Data
+    % log_s_ratio should be a constant if S(q,w) contains Bose
+    % then kT = w./log_s_ratio
+    T         = w./log_s_ratio*11.6045; % 1 meV = 11.6045 K
+    T         = getaxis(T,0);
+    if any(isfinite(T)) && (all(T(~isnan(T))>0.1) || all(T(~isnan(T))<0.1))
+      T         = mean(real(T(~isnan(T))));
+    end 
+  end
  
