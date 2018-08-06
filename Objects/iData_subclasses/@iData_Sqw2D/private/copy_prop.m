3d7f460aa996dbf1ae7b27c2f87c21d5d88cb0d4
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-08-06 16:38:17 +0200

    iData_Sqw2D: remove warnings

--- Objects/iData_subclasses/@iData_Sqw2D/private/copy_prop.m
+++ Objects/iData_subclasses/@iData_Sqw2D/private/copy_prop.m
@@ -2,12 +2,14 @@ function obj = copy_prop(obj0, m)
   % copy the properties from 'm' in the instantiated object obj0 (single)
   % handle arrays by copying the obj0
   obj = [];
+  w = warning;
+  warning off;
   for index=1:numel(m)
     this = m(index);
 
     % transfer properties
     % this is a safe way to instantiate a subclass
-    warning off MATLAB:structOnObject
+    
     this = struct(this);
     for p = fieldnames(this)'
       obj0.(p{1}) = this.(p{1});
@@ -20,3 +22,4 @@ function obj = copy_prop(obj0, m)
       obj = [ obj obj0 ];
     end
   end
+  warning(w);
