bd331ceb2b3f9e0050f26980a60f08ce2a18df8d
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-10-16 19:12:41 +0200

    Test: getframe: use opengl software to avoid crash

--- Tests/Unit/Objects/iData/test_iData_getframe.m
+++ Tests/Unit/Objects/iData/test_iData_getframe.m
@@ -2,8 +2,11 @@ function result=test_iData_getframe
 
   a=iData(peaks);
   
+  gl=opengl;
+  opengl software
   if isstruct(getframe(a))
     result = [ 'OK     ' mfilename ];
   else
     result = [ 'FAILED ' mfilename ];
   end
+  opengl(gl);
