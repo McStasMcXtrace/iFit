6a2fe92e139a8fb25c80cc4b8e064276baae0277
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-10-22 19:01:38 +0200

    Test: iFunc: Sqw4D: also test for export to McStas files.

--- Tests/Unit/Objects/iFunc_Sqw4D/test_iFunc_Sqw4D.m
+++ Tests/Unit/Objects/iFunc_Sqw4D/test_iFunc_Sqw4D.m
@@ -8,10 +8,20 @@ function result=test_iFunc_Sqw4D
   q=0.01:0.25:3; w=0:.25:7;
   p=powder(s);
   d=iData(p, [], q, w);
+  
+  % export to Sqw2D
+  file = saveas(d, tempname, 'sqw');
+  
+  % export to Sqw4D
+  d4 = iData(s);
+  file4 = saveas(d4, tempname, 'sqw');
 
   % check value
-  if abs(std(d,1) - 0.8) > .1
+  if abs(std(d,1) - 0.8) > .1 && exist(file) && exist(file4)
     result = [ 'FAILED ' mfilename ];
   else
     result = [ 'OK     ' mfilename ];
   end
+  
+  if exist(file),  delete(file);  end
+  if exist(file4), delete(file4); end
