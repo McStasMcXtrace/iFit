eb9370485e31c1c85c9f94cb7313d028971cbe26
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-10-23 15:09:13 +0200

    iFunc_Sqw4D/max: fix to avoid default=100

--- Objects/iFunc_subclasses/@iFunc_Sqw4D/max.m
+++ Objects/iFunc_subclasses/@iFunc_Sqw4D/max.m
@@ -20,14 +20,21 @@ function [m, DOS] = max(s)
     || (nargout > 1 && isempty(s.UserData.FREQ))
     qh=linspace(-.5,.5,10);qk=qh; ql=qh; w=linspace(0.01,50,11);
     f=iData(s,[],qh,qk,ql',w);
-    if isfield(s.UserData, 'FREQ') && ~isempty(s.UserData.FREQ)
-      s.UserData.maxFreq = max(s.UserData.FREQ(:));
-      disp([ mfilename ': maximum phonon energy ' num2str(max(s.UserData.maxFreq)) ' [meV] in ' s.Name ]);
+    if isfield(f.UserData, 'FREQ') && ~isempty(f.UserData.FREQ)
+      m = max(f.UserData.FREQ(:));
+    elseif isfield(s.UserData, 'FREQ') && ~isempty(s.UserData.FREQ)
+      m = max(s.UserData.FREQ(:));
     end
-    if ~isfield(s.UserData, 'maxFreq') || isempty(s.UserData.maxFreq) ...
-      || ~isfinite(s.UserData.maxFreq) || s.UserData.maxFreq <= 0
-      s.UserData.maxFreq = 100;
+    
+    if ~isempty(m)
+      disp([ mfilename ': maximum phonon energy ' num2str(max(m)) ' [meV] in ' s.Name ]);
     end
+    if isempty(m)
+      m=100;
+    end
+    
+    s.UserData.maxFreq = m;
+    f.UserData.maxFreq = m;
     
     if ~isempty(inputname(1))
       assignin('caller',inputname(1),s); % update in original object
