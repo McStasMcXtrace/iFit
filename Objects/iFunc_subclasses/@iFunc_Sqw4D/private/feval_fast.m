c2d99ddca510b0e797728be42a47719ac3d950ec
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-10-23 15:11:10 +0200

    iFunc_Sqw4D: faster 'feval fast'

--- Objects/iFunc_subclasses/@iFunc_Sqw4D/private/feval_fast.m
+++ Objects/iFunc_subclasses/@iFunc_Sqw4D/private/feval_fast.m
@@ -1,8 +1,8 @@
 function f = feval_fast(s)
   % iFunc_Sqw4D: feval_fast: quickly evaluate a 4D Sqw(h=0)
   maxFreq = max(s);
-  % evaluate the 4D model onto a mesh filling the Brillouin zone [0:0.5 ]
-  qk=linspace(0,0.95,50); qh=0; ql=qk; 
+  % evaluate the 4D model onto a coarse mesh filling the Brillouin zone [0:0.5 ]
+  qk=linspace(0,0.95,15); qh=0; ql=qk; 
   w =linspace(0.01,maxFreq*1.2,51);
   f =iData(s,[],qh,qk,ql',w);
   f = log(squeeze(f(1,:, :,:)));
@@ -10,3 +10,4 @@ function f = feval_fast(s)
   ylabel(f, 'QL [rlu]');
   zlabel(f, 'Energy [meV]');
   title(f, s.Name);
+
