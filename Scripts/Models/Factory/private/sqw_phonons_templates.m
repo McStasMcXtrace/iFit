40805b54990f6432ffbe3fcae3ed1e3e9d5e4499
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-01-29 15:09:22 +0100

    Sqw4D: templates: only get 'dho' snippet when requested

--- Scripts/Models/Factory/private/sqw_phonons_templates.m
+++ Scripts/Models/Factory/private/sqw_phonons_templates.m
@@ -8,7 +8,9 @@ function [script_hkl, script_dho] = sqw_phonons_templates
   script_hkl(strncmp(deblank(script_hkl), 'function', 8)) = []; % get rid of 1st line 'function'
 
   % get code to convolve DHO line shapes for all excitations
-  script_dho = fileread(which('sqw_phonons_template_dho.txt'));
-  script_dho = textscan(script_dho,'%s','delimiter','\n','whitespace',''); % read all lines
-  script_dho = script_dho{1};
-  script_dho(strncmp(deblank(script_dho), 'function', 8)) = []; % get rid of 1st line 'function'
+  if nargout > 1
+    script_dho = fileread(which('sqw_phonons_template_dho.txt'));
+    script_dho = textscan(script_dho,'%s','delimiter','\n','whitespace',''); % read all lines
+    script_dho = script_dho{1};
+    script_dho(strncmp(deblank(script_dho), 'function', 8)) = []; % get rid of 1st line 'function'
+  end
