ee37854a8c51870f6c0bdac5a93c7de78c528107
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-02-04 16:24:31 +0100

    struct: avoid calling struct/set for graphics handles

--- Objects/@struct/set.m
+++ Objects/@struct/set.m
@@ -1,12 +1,18 @@
-function s = set(s, field, value)
+function s = set(s, varargin)
 % SET    Set structure properties.
 %    V = SET(S,'PropertyName','Value') set the value of the specified
 %    property/field in the structure.  
 % 
 %    SET(S) displays all structure field names.
 
-  if nargin == 1, field=''; end
-  if nargin < 2, value=[]; end
+  if ~isstruct(s)
+    builtin('set', s, varargin{:});
+    return
+  end
+  field='';
+  value=[];
+  if nargin <=2,  field=varargin{1}; end
+  if nargin <=3,  value=varargin{3}; end
   if isempty(field), s = fieldnames(s); return; end
   
   % handle array of struct
