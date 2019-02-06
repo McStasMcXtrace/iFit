16f392a6132dd7b2f75e2ab06e852cf83a8268d7
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-02-06 17:53:15 +0100

    iFunc_Sqw2D: new method Sqw -> DDCS (*Kf/Ki)

---
+++ Objects/iFunc_subclasses/@iFunc_Sqw2D/Sqw2ddcs.m
@@ -0,0 +1,40 @@
+function self = Sqw2ddcs(s)
+  % iFunc_Sqw2D: Sqw2ddcs: convert a S(q,w) into a double differential cross-section (q,w)
+  %   i.e. multiply by Kf/Ki
+  %
+  % convert: iFunc_Sqw2D S(q,w) -> d2(sigma)/dOmega/dE = N.sigma Kf/Ki S(q,w)
+  %
+  %   s = Sqw2ddcs(s)
+  %
+  % New model parameters:
+  %       Ei     Incident neutron energy [meV]
+  %
+  % input:
+  %   s:      Sqw 2D model with q as 1st axis (Angs-1), w as 2nd axis (meV).
+  %
+  % output:
+  %   s:      double differential cross-section [Kf/Ki S(q,w)]
+  
+  self = copyobj(s);
+  index=numel(self.Parameters)+1;
+  self.Parameters{end+1} = 'Ei Incident neutron energy [meV]';
+  
+  if isvector(self.Guess) && isnumeric(self.Guess)
+    self.Guess = [ self.Guess(:)' 14.8 ];  % typical
+  else
+    if ~iscell(self.Guess), self.Guess = { self.Guess }; end
+    self.Guess{end+1} = [ 14.8 ];
+  end
+
+  self.Expression{end+1} = [ 'q = x; w = y; Ei = p(' num2str(index) ');' ];
+  self.Expression{end+1} = 'Ef = Ei-w;';
+  self.Expression{end+1} = 'kfki = sqrt(Ef./Ei); kfki(Ef<0)=0;';
+  self.Expression{end+1} = 'signal = signal .* kfki; % actually set the dynamic range';
+  
+  self = iFunc_Sqw2D(self); % check
+
+  if nargout == 0 && ~isempty(inputname(1))
+    assignin('caller',inputname(1),self);
+  end
+
+end % Sqw2ddcs
