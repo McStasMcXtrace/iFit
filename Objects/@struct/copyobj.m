3b0a2eef4cbfd24b2567a39f7ff1edc1192c03b8
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-02-21 15:13:26 +0100

    struct: add deep copy for struct and later derived class, including handles.

---
+++ Objects/@struct/copyobj.m
@@ -0,0 +1,53 @@
+function new = copyobj(self, org)
+  % COPYOBJ makes a deep copy of initial object
+  %
+  %   new = copyobj(self)
+  %     creates a deep copy of the initial object 'self'
+  %   new = copyobj(self, content)
+  %     creates an empty object and fill it with 'content' (struct/object)
+  %
+  % copy the properties from 'self' into the instantiated object 'new'.
+  
+  % handle arrays by copying the new0
+  if nargin == 1,  org=''; end
+  if isempty(org), org=self; end
+  if ~isstruct(org) || ~isobject(org)
+    new = self; % this may not be a deep copy
+    return
+  end
+  wrn_flag = true;
+  
+  % handle array input
+  if numel(self) > 1
+    new = [];
+    for index=1:numel(self)
+      new = [ new copyobj(self(index), org) ];
+    end
+    return
+  end
+  if numel(org) > 1
+    new = [];
+    for index=1:numel(org)
+      new = [ new copyobj(self, org(index)) ];
+    end
+    return
+  end
+
+  % single deep copy
+  new = feval(class(self)); % new empty object of same class
+  % transfer properties: this is a safe way to instantiate a subclass
+  if isstruct(self)
+    new = cell2struct(struct2cell(org),fieldnames(org),2);
+  else
+    for p = properties(org)'
+      try
+        new.(p{1}) = org.(p{1});  % may fail when copying from enriched object
+      catch ME
+        if wrn_flag
+          disp(getReport(ME))
+          warning([ mfilename ': can not copy property ' p{1} ...
+                    ' from class ' class(org) ' into ' class(self) ]);
+        end
+      end
+    end
+  end
