7f5a17f48c19797c6f7a6f19ba9e3aa141e08a6d
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-03-08 14:33:50 +0100

    estruct: removed sort, as dynamic props can not be sorted... (random order)

--- Objects/@estruct/sort.m
+++
@@ -1,7 +0,0 @@
-function c = sort(s)
-%  SORT Order fields of a structure array.
-%  
-%  SNEW = SORT(S1) orders the fields in S1 so the new structure array
-%  SNEW has field names in ASCII dictionary order.
-
-c = orderfields(s);
