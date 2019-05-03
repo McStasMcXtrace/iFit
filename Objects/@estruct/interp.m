6f965f626370e0b7b5673295074c98435817d2a2
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-05-03 18:43:16 +0200

    estruct: initial import of interp/meshgrid stuff

---
+++ Objects/@estruct/interp.m
@@ -0,0 +1,383 @@
+function b = interp(a, varargin)
+% [b...] = interp(s, ...) : interpolate object
+%
+%   @iData/interp function to interpolate data sets.
+%
+%   The multidimensional interpolation is based on a Delauney tessellation 
+%     using the Computational Geometry Algorithms Library, CGAL.
+%   This function computes the values of the object 's' interpolated
+%   on a new axis grid, which may be specified from an other object, as independent axes,
+%   or as a rebinning of the original axes.
+%     b=interp(s)    rebin/check 's' on a regular grid.
+%     b=interp(s, d) where 'd' is an iData object computes 's' on the 'd' axes.
+%     b=interp(s, X1,X2, ... Xn) where 'Xn' are vectors or matrices as obtained 
+%                    from ndgrid computes 's' on these axes.
+%     b=interp(s, {X1,X2, ... Xn}) is similar to the previous syntax
+%     b=interp(s, ..., ntimes) where 'ntimes' is an integer computes new axes for 
+%                    interpolation by sub-dividing the original axes ntimes.
+%                    we also recommend the 'resize' method (much faster).
+%     b=interp(s, ..., 'method') uses specified method for interpolation as one of
+%                    linear (default), spline, cubic, or nearest
+%     b=interp(s, ..., 'grid') uses meshgrid/ndgrid to determine new axes as arrays
+%   Extrapolated data is set to NaN for the Signal, Error and Monitor. Also, in some
+%     cases, the triangulation interpolant creates fake 'flat' area, especially 
+%     when the axes area is concave. We then recommend you try the 'hist' method.
+%   For Event data sets, we recommend to use the 'hist' method which is much faster.
+%
+% input:  s: object or array (iData)
+%         d: single object from which interpolation axes are extracted (iData)
+%            or a cell containing axes d={X1,X2, ... Xn}               (cell)
+%         X1...Xn: scalar, vectors or matrices specifying axis for 
+%            dimensions 1 to ndims(s) (double scalar/vector/matrix)
+%         ntimes: original axis sub-division (integer)
+% output: b: object or array (iData)
+% ex:     a=iData(peaks); b=interp(a, 'grid'); c=interp(a, 2);
+%
+% Version: $Date$ $Version$ $Author$
+% See also iData, interp1, interpn, ndgrid, iData/setaxis, iData/getaxis, 
+%          iData/hist, iData/resize, iData/reshape
+
+% private_interp and private_meshgrid are in private
+
+% input: option: linear, spline, cubic, nearest
+% axes are defined as rank of matrix dimensions
+% plot function is plot(y,x,Signal)
+% rand(10,20) 10 rows, 20 columns
+% pcolor/surf with view(2) shows x=1:20, y=1:10
+
+% handle input arrays
+if numel(a) > 1
+  b = zeros(estruct, numel(a), 1);
+  for index=1:numel(a)
+    b(index) = interp(a(index), varargin{:});
+  end
+  b = reshape(b, size(a));
+  return
+end
+
+% build new iData object to hold the result
+b = copyobj(a);
+
+% object check
+if ndims(a) == 0
+  return
+end
+
+% default axes/parameters
+i_axes = cell(1,ndims(a)); i_labels=i_axes;
+for index=1:ndims(a)
+  [i_axes{index}, i_labels{index}] = getaxis(a, index);  % loads object axes, or 1:end if not defined 
+end
+for index=ndims(a):length(a.Alias.Axis)
+  [~, i_labels{index}] = getaxis(a, index);  % additional inactive axes labels (used to create new axes)
+end
+method='linear';
+ntimes=0;
+
+% interpolation axes
+f_axes           = i_axes;
+requires_meshgrid= 0;
+
+% parse varargin to overload defaults and set manually the axes ----------------
+axis_arg_index   = 0;
+for index=1:length(varargin)
+  c = varargin{index};
+  if ischar(c) && ~isempty(strfind(c,'grid')) 
+    requires_meshgrid=1;
+  elseif ischar(c)                      % method (char)
+    method = c;
+  elseif isa(varargin{index}, 'estruct')  % object axes
+    if length(c) > 1
+      warning([ mfilename ': Can not interpolate onto all axes of input argument ' num2str(index) ' which is an array of ' num2str(numel(c)) ' elements. Using first element only.']);
+      c = c(1);
+    end
+    for j1 = 1:ndims(c)
+      axis_arg_index = axis_arg_index+1;
+      [f_axes{axis_arg_index}, lab] = getaxis(c, j1);
+      if ~isempty(lab) && axis_arg_index < length(i_labels) && isempty(i_labels{axis_arg_index})
+        i_labels{axis_arg_index} = lab;
+      end
+    end
+  elseif isnumeric(c) && length(c) ~= 1  % vector/matrix axes
+    axis_arg_index = axis_arg_index+1;
+    if ~isempty(c), f_axes{axis_arg_index} = c; end
+  elseif isnumeric(c) && length(c) == 1  % ntimes rebinning (max 10 times)
+    ntimes=c;
+    if abs(ntimes) > 10, ntimes=10; end
+  elseif iscell(c)                      % cell(vector/matrix) axes
+    for j1 = 1:length(c(:))
+      axis_arg_index = axis_arg_index+1;
+      if ~isempty(c{j1}), f_axes{axis_arg_index} = c{j1}; end
+    end
+  elseif ~isempty(c)
+    warning([ mfilename ': Input argument ' num2str(index) ' of class ' class(c) ' size [' num2str(size(c)) '] is not supported. Ignoring.']);
+  end
+  clear c
+end % input arguments parsing
+
+b = history(b, mfilename, a, varargin{:});
+cmd=b.Command;
+clear varargin a
+
+% check for method to be valid
+if ~any(strcmp(method, {'linear','cubic','spline','nearest','v4'}))
+  warning([ mfilename ': Interpolation method ' method ' is not supported. Use: linear, cubic, spline, nearest, v4. Defaulting to linear.']);
+  method = 'linear';
+end
+
+% check/determine output axes for interpolation --------------------------------
+
+% test axes and decide to call meshgrid if necessary
+
+if isvector(b) >=2 % event data set: redirect to hist method (accumarray)
+  f_axes = private_meshgrid(f_axes, s_dims, 'vector'); % private function
+  b = hist(b, f_axes{:});
+  return
+end
+
+% check final axes
+s_dims = size(b); % Signal/object dimensions
+myisvector = @(c)length(c) == numel(c);
+
+% do we have mixed vector and matrix f_axes ?
+has_vector = 0;
+has_matrix = 0;
+
+for index=1:ndims(b)
+  v = f_axes{index}; 
+  if isempty(v), v= i_axes{index}; end % no axis specified, use the initial one
+
+  % compute the initial axis length
+  if myisvector(v), a_len = numel(v); has_vector=1;
+  else            a_len = size( v, index); has_matrix=1;
+  end
+  if isvector(b) >= 2 && a_len > prod(size(b))^(1/ndims(b))*2 % event data set
+    a_len = prod(size(b))^(1/ndims(b))*2;
+  end
+  if ntimes > 0 % expand it if requested
+    a_len = a_len*ntimes;
+  end
+  if a_len == 1, a_len = 2; end
+  s_dims(index) = a_len;
+
+end
+if has_vector && has_matrix, requires_meshgrid=1; end
+
+% check if interpolation is indeed required ------------------------------------
+% do we need to recompute the final axes ?
+if length(f_axes) > 1 && (requires_meshgrid || ntimes)
+  f_axes = private_meshgrid(f_axes, s_dims, method); % private function
+end
+
+% test if interpolation axes have changed w.r.t input object (for possible quick exit)
+has_changed = 0;
+
+for index=1:ndims(b)  
+  this_i = i_axes{index}; if myisvector(this_i), this_i=this_i(:); end
+  this_f = f_axes{index}; if myisvector(this_f), this_f=this_f(:); end
+  if ~isequal(this_i, this_f)
+    % length changed ?
+    if length(this_i) ~= length(this_f)
+      % not same length
+      has_changed=1;
+    elseif prod(size(this_i)) ~= prod(size(this_f)) % nb of elements has changed, including matrix axes ?
+      has_changed=2; 
+    elseif all(abs(this_i(:) - this_f(:)) > 1e-4*abs(this_i(:) + this_f(:))/2)
+      % or axis variation bigger than 0.01 percent anywhere
+      has_changed=3;
+    end
+  end
+  clear this_i this_f
+end
+
+% get Signal, error and monitor.
+i_signal = subsref(b,struct('type','.','subs','Signal'));
+
+% quick exit check based on the Signal
+if any(isnan(i_signal(:))), has_changed=1; end
+if ~has_changed && ~requires_meshgrid 
+  return; 
+end
+
+if isvector(i_signal) % force axes to be vectors
+  for index=1:length(i_axes)
+    x=i_axes{index}; x=x(:); i_axes{index}=x;
+  end
+  clear x
+end
+
+% prepare interpolation Signal, Error, Monitor ---------------------------------
+i_class    = class(i_signal); i_signal = double(i_signal);
+
+i_error = getalias(b, 'Error');
+if ~isempty(i_error),   
+  % check if Error is sqrt(Signal) or a constant
+  if strcmp(i_error, 'sqrt(this.Signal)')
+    i_error=[];
+  elseif isnumeric(i_error) && isscalar(i_error) == 1
+    % keep that as a constant value
+  else
+    % else get the value
+    i_error  = subsref(b,struct('type','.','subs','Error'));
+  end
+  i_error    = double(i_error);
+end
+
+i_monitor = getalias(b, 'Monitor');
+if ~isempty(i_monitor),   
+  % check if Monitor is 1 or a constant
+  if isnumeric(i_monitor) && isscalar(i_monitor) == 1
+    % keep that as a constant value
+  else
+    % else get the value
+    i_monitor  =subsref(b,struct('type','.','subs','Monitor'));
+  end
+  i_monitor    = double(i_monitor);
+end
+
+% check f_axes vector orientation
+for index=1:ndims(b)
+  i_axes{index} = double(i_axes{index});
+  f_axes{index} = double(f_axes{index});
+  if myisvector(f_axes{index})
+    % orient the vector along the dimension
+    n = ones(1,ndims(b));
+    n(index) = numel(f_axes{index});
+    if length(n) == 1, n=[ n 1]; end
+    f_axes{index}=reshape(f_axes{index},n);
+  end
+end
+
+% make sure input axes are monotonic. output axes should be OK ------------
+i_nonmonotonic=0;
+for index=1:ndims(b)
+  dif = diff(i_axes{index},1,index);
+  if all(dif(:) < 0) || all(dif(:) >= 0), continue; end
+  i_nonmonotonic=index; break;
+end
+
+if i_nonmonotonic && length(i_axes) > 1 
+  % transform the initial data into individual points, then interpolate on
+  % a regular grid
+  i_axes_new  = private_meshgrid(i_axes, size(b));
+  % must make sure initial axes given as vector have same length as signal
+  flag_ndgrid_needed = 0;
+  for index=1:length(i_axes)
+    x=i_axes{index}; x=x(:);
+    % make sure length(x) == numel(i_signal) for interpolation to work
+    if length(x) < numel(i_signal)
+      if isempty(length(x) == size(i_signal))
+        error('%s: The axis rank %d of length=%d does not match the object %s Signal dimensions %s\n', ...
+          mfilename, index, length(x), a.Tag, mat2str(size(i_signal)) ...
+        );
+      else
+        flag_ndgrid_needed = 1;
+      end
+    end
+    i_axes{index} = x;  % now a vector...
+  end
+  % signal is a grid but axes are vectors, axes should also be...
+  if flag_ndgrid_needed
+    [i_axes{:}] = ndgrid(i_axes{:});
+    for index=1:length(i_axes)
+        i_axes{index} = i_axes{index}(:);
+    end
+  end
+
+  % interpolate initial data on monotonic initial axes
+  i_signal    = private_interp(i_axes, i_signal(:),  i_axes_new, method);
+
+  if isnumeric(i_error) && length(i_error) > 1, 
+    i_error   = private_interp(i_axes, i_error(:),   i_axes_new, method); 
+  end
+  if isnumeric(i_monitor) && length(i_monitor) > 1, 
+    i_monitor = private_interp(i_axes, i_monitor(:), i_axes_new, method); 
+  end
+  i_axes = i_axes_new;
+  clear i_axes_new
+end
+
+% last test to check if axes have changed ---------------------------------
+has_changed = 0;
+for index=1:ndims(b)    % change to double before interpolation
+  i_axes{index}=double(i_axes{index});
+  f_axes{index}=double(f_axes{index});
+end
+for index=1:ndims(b)
+  x = i_axes{index}; x=x(:)';
+  if ~isequal(i_axes{index}, f_axes{index})
+    has_changed = 1;
+    break
+  end
+end
+if ~has_changed, 
+  return; 
+end
+
+% interpolation takes place here ------------------------------------------
+[f_signal, meth] = private_interp(i_axes, i_signal, f_axes, method);
+if isscalar(f_signal), return; end % single scalar value
+
+if isnumeric(i_error) && length(i_error) > 1, 
+     f_error = private_interp(i_axes, i_error,  f_axes, method); 
+else f_error = i_error; end
+clear i_error
+if isnumeric(i_monitor) && length(i_monitor) > 1, 
+     f_monitor = private_interp(i_axes, i_monitor,f_axes, method);
+else f_monitor = i_monitor; end
+clear i_monitor
+clear i_axes
+
+% get back to original Signal class
+if ~strcmp(i_class, 'double')
+  f_signal = feval(i_class, f_signal);
+  f_error  = feval(i_class, f_error);
+  f_monitor= feval(i_class, f_monitor);
+end
+
+if isvector(i_signal) && size(i_signal,1)==1
+    f_signal = transpose(f_signal);
+    f_error  = transpose(f_error);
+    f_monitor= transpose(f_monitor);
+end
+clear i_signal
+
+% transfer Data and Axes --------------------------------------------------
+b.Data.Signal =f_signal;  clear f_signal
+b.Data.Error  =f_error;   clear f_error
+b.Data.Monitor=f_monitor; clear f_monitor
+for index=1:length(f_axes)
+  b.Data.([ 'axis' num2str(index) ]) = f_axes{index};
+end
+
+% update new aliases, but remove old axes which are numeric (to free memory)
+g = getalias(b);
+to_remove=[];
+for index=4:length(g)
+  if any(strcmp(g{index}, b.Alias.Axis))
+    if isnumeric(b.Alias.Values{index})
+      to_remove=[ to_remove index ];
+    end
+  end
+end
+b.Alias.Values(to_remove) = [];
+b.Alias.Names(to_remove)  = [];
+b.Alias.Labels(to_remove) = [];
+setalias(b,'Signal', 'Data.Signal');
+setalias(b,'Error',  'Data.Error');
+setalias(b,'Monitor','Data.Monitor');
+setalias(b,'Interpolation',meth,'Interpolation method used');
+
+% clear axes
+rmaxis (b);
+
+for index=1:length(f_axes)
+  if index <= length(i_labels)
+    b=setalias(b,[ 'axis' num2str(index) ], [ 'Data.axis' num2str(index) ], i_labels{index});
+  else
+    b=setalias(b,[ 'axis' num2str(index) ], [ 'Data.axis' num2str(index) ]);
+  end
+  b=setaxis (b, index, [ 'axis' num2str(index) ]);
+end
+b.Command=cmd; 
+
---
+++ Objects/@estruct/private/private_interp.m
@@ -0,0 +1,267 @@
+function [f_signal, method] = private_interp(i_axes, i_signal, f_axes, method)
+% PRIVATE_INTERP private function for interpolation
+%   interpolates i_signal(i_axes{}) onto f_axes{} and returns the f_signal
+%
+% arguments:
+%  i_axes:   cell array of initial axes
+%  i_signal: initial signal (double array or vector)
+%  f_axes:   desired new axes for interpolation (cell array)
+%  method:   method used in interpolation
+% output:
+%  f_signal: interpolated new signal (double array)
+%  method:   the interpolation method used
+
+% This file is used by: interp
+
+f_signal=[]; 
+if nargin < 4, method=''; end
+if length(i_signal) <= 1
+  f_signal = i_signal; return; 
+end
+
+% interpolation is required
+if length(i_axes) == 1    % case: 1D
+
+  X=i_axes{1};
+  Y=i_signal;
+  [X,I] = unique(X); Y=Y(I);
+  f_signal = interp1(X,   Y, f_axes{1},   method, NaN);
+  
+elseif isvector(i_signal)  % case: long vector nD 'event' Data set
+  
+  if length(i_axes) == 2    % 2D event (i.e. 2 vector axes)
+    if ~any(strcmp(method,{'linear','nearest','cubic','v4','natural'})), method='linear'; end
+    i_signal(~isfinite(i_signal)) = 0;
+    for index=1:length(i_axes)
+        x = i_axes{index}; x(~isfinite(x)) = 0; i_axes{index} = x;
+        x = f_axes{index}; x(~isfinite(x)) = 0; f_axes{index} = x;
+    end
+    warning('off','MATLAB:griddata:DuplicateDataPoints');
+    f_signal = griddata(i_axes{[2 1]}, i_signal, f_axes{[2 1]}, method);
+    method = 'griddata with vector signal';
+  else                       % nD event: method: linear or nearest
+    if ~any(strcmp(method,{'linear','nearest'})), method='linear'; end
+    % i_axes and f_axes must be columns, and cell2mat append them for
+    % griddatan
+    for index=1:length(i_axes)
+      x = i_axes{index}; i_axes{index}=x(:); 
+      x = f_axes{index}; f_axes{index}=x(:); clear x;
+    end
+    warning('off','MATLAB:griddatan:DuplicateDataPoints');
+    f_signal = griddatan(cell2mat(i_axes), i_signal, cell2mat(f_axes), method);
+    method = 'griddatan with vector signal';
+  end
+    
+else % normal nD grids/axes
+  
+    % f_axes must be an ndgrid result, and monotonic
+    if ~any(strcmp(method,{'linear','nearest','cubic','spline'})), method='linear'; end
+    any_vector  = 0;
+    any_matrix  = 0;
+    for i=1:length(i_axes); 
+        v=i_axes{i}; 
+        if numel(v) == length(v)  % isvector
+          sz = ones(size(i_axes));
+          sz(i) = numel(v);
+          i_axes{i}=reshape(v, sz); 
+          any_vector = 1; 
+        else any_matrix =1; end; 
+    end
+    % check if we have mixed axes as vector/matrices -> all go to matrices
+    % with repmat. First get the 'matrix' size, which is the one from
+    % Signal.
+    sz = size(i_signal);
+    if any_vector && any_matrix
+        for i=1:length(i_axes)
+            v=i_axes{i}; 
+            if numel(v) == length(v) % isvector
+                % orient axis
+                this_sz=ones(size(sz)); this_sz(i) = numel(v);
+                v=reshape(v, this_sz);
+                % replicate it to form a matrix
+                this_sz=sz; this_sz(i) = 1;
+                v=repmat(v, this_sz);
+                i_axes{i} = v;
+            end
+        end
+    end
+    
+    interpolants = {'interp_interpn','interp_TriScatteredInterp', ...
+        'interp_scatteredInterpolant','interp_griddata'};
+    f_signals ={};
+    f_methods ={};
+    f_nans    =[];
+    % input number of NaN's
+    nb_i_nans  = numel(find(isnan(i_signal(:))));
+    for index = 1:numel(interpolants)
+      try
+        [f_signal, f_method] = feval(interpolants{index}, i_axes, i_signal, f_axes, method);
+        if ~isempty(f_signal)
+          f_signals{end+1}  = f_signal;
+          f_methods{end+1}  = f_method;
+          f_nans(end+1)     = numel(find(isnan(f_signal(:))));
+          if f_nans(end) == 0, break; end % perfect method/no NaN's, we assume it is good
+          % less than twice as many NaN's as original data set, or 1%: this is acceptable
+          if f_nans(end)/numel(f_signal) < 2*nb_i_nans/numel(i_signal) || f_nans(end)/numel(f_signal) < 1e-2
+            break
+          end
+        end
+      catch ME
+        % warning([ mfilename ': WARNING: failed interpolation with ' interpolants{index} '. Going on.' ]);
+        % getReport(ME)
+      end
+    end
+    
+    f_signal = []; method = []; f_nan = Inf;
+    % now we get the best method, which has fewer NaN's
+    [~,best] = min(f_nans);
+    if isempty(best) || numel(f_signals) == 1
+        best = 1;
+    end
+    if isempty(f_signals)
+        error([ mfilename ': failed all interpolation methods.' ]);
+    end
+    f_signal = f_signals{best};
+    method   = f_methods{best};
+    f_nan    = f_nans(best);
+  end
+end
+
+% ------------------------------------------------------------------------------
+
+function [f_signal, method] = interp_interpn(i_axes, i_signal, f_axes, method)
+  % interpn is faster than griddatan, but requires that i_axes are monotonic
+  % as obtained from ndgrid.
+  switch length(i_axes)
+  case 1
+    f_signal  = interp1(i_axes{1}, i_signal, f_axes{1}, method, NaN);
+  case 2
+    f_signal  = interp2(i_axes{[2 1]}, i_signal, f_axes{[2 1]}, method, NaN);
+  case 3
+    f_signal  = interp3(i_axes{[2 1 3]}, i_signal, f_axes{[2 1 3]}, method, NaN);
+  otherwise
+    f_signal  = interpn(i_axes{:}, i_signal, f_axes{:}, method, NaN);
+  end
+  method    = [ method ' interpn' ];
+  
+function [f_signal, method] = interp_TriScatteredInterp(i_axes, i_signal, f_axes, method)
+  % TriScatteredInterp is recommended in R2010a up to R2013a
+  if ~exist('TriScatteredInterp') || length(i_axes) > 3
+      error('TriScatteredInterp is not available');
+  end
+  
+  % make sure we do not have nan/inf, and use vectors
+  i_signal(~isfinite(i_signal)) = 0; i_signal=i_signal(:);
+  for index=1:length(i_axes)
+      x = i_axes{index}; x(~isfinite(x)) = 0; i_axes{index} = x(:);
+      x = f_axes{index}; x(~isfinite(x)) = 0; f_axes{index} = x(:);
+  end
+  
+  switch length(i_axes)
+  case 1
+    F = TriScatteredInterp(i_axes{1}, i_signal);
+    f_signal = F(f_axes{1});
+  case 2
+    F = TriScatteredInterp(i_axes{[2 1]}, i_signal);
+    f_signal = F(f_axes{[2 1]});
+  case 3
+    F = TriScatteredInterp(i_axes{[2 1 3]}, i_signal);
+    f_signal = F(f_axes{[2 1 3]});
+  end
+  method = 'TriScatteredInterp';
+  
+  
+function [f_signal, method] = interp_scatteredInterpolant(i_axes, i_signal, f_axes, method)
+  % scatteredInterpolant is recommended above R2013a
+  if ~exist('scatteredInterpolant') || ~any(length(i_axes) == [2 3])
+      error('scatteredInterpolant is not available');
+  end
+  
+  % make sure we do not have nan/inf, and use vectors
+  i_signal(~isfinite(i_signal)) = 0; i_signal=i_signal(:);
+  for index=1:length(i_axes)
+      x = i_axes{index}; x(~isfinite(x)) = 0; i_axes{index} = x(:);
+      x = f_axes{index}; x(~isfinite(x)) = 0; f_axes{index} = x(:);
+  end
+  
+  switch length(i_axes)
+  case 2
+    F = scatteredInterpolant(i_axes{[2 1]}, i_signal);
+    f_signal = F(f_axes{[2 1]});
+  case 3
+    F = scatteredInterpolant(i_axes{[2 1 3]}, i_signal);
+    f_signal = F(f_axes{[2 1 3]});
+  end
+  method = 'scatteredInterpolant';
+
+function [f_signal, method] = interp_griddata(i_axes, i_signal, f_axes, method)
+  % griddata is much slower than interpn. 
+  % Also, it uses triangulation, which creates fake area when using 
+  %   non-meshgrid concave initial data.
+  % In order to minimize this effet, we extend the initial data with zeros around
+  %
+  % NOTE: TriScatteredInterp provides the same results
+  
+  % pad with 0 around to avoid strange Tri interpolant in concave sets
+  [i_axes, i_signal] = interp_safeboxes(i_axes, i_signal);
+  
+  % make sure we do not have nan/inf
+  i_signal(~isfinite(i_signal)) = 0;
+  for index=1:length(i_axes)
+      x = i_axes{index}; x(~isfinite(x)) = 0; i_axes{index} = x;
+      x = f_axes{index}; x(~isfinite(x)) = 0; f_axes{index} = x;
+  end
+  
+  if length(i_axes) == 2
+    if ~any(strcmp(method,{'linear','nearest','cubic','v4','natural'})), method='linear'; end
+    warning('off','MATLAB:griddata:DuplicateDataPoints');
+    f_signal = griddata(i_axes{[2 1]}, i_signal, f_axes{[2 1]}, method);
+    method = [ method ' griddata'];
+    
+  elseif length(i_axes) == 3 && exist('griddata3')
+    if ~any(strcmp(method,{'linear','nearest'})), method='linear'; end
+    warning('off','MATLAB:griddata3:DuplicateDataPoints');
+    f_signal = griddata3(i_axes{[2 1]}, i_signal, f_axes{[2 1]}, method);
+    method = [ method ' griddata3'];
+    
+  else
+    % try with griddatan, requires to assemble new X,Y and XI
+    if numel(i_signal) ~= length(i_signal)
+      if all(cellfun(@(c)numel(c)==length(c), i_axes))
+        [ i_axes{:} ] = ndgrid(i_axes{:});
+      end
+      if all(cellfun(@(c)numel(c)==length(c), f_axes))
+        [ f_axes{:} ] = ndgrid(f_axes{:});
+      end
+    end
+
+    % i_axes and f_axes must be columns, and cell2mat append them for
+    % griddatan
+    sz = size(f_axes{1});
+    for index=1:length(i_axes)
+      x = i_axes{index}; i_axes{index}=x(:); 
+      x = f_axes{index}; f_axes{index}=x(:); clear x;
+    end
+    warning('off','MATLAB:griddatan:DuplicateDataPoints');
+    f_signal = griddatan(cell2mat(i_axes), i_signal, cell2mat(f_axes), method);
+    f_signal = reshape(f_signal, sz);
+    method = [ method ' griddatan'];
+  end
+  
+% ------------------------------------------------------------------------------
+function [i_axes, i_signal] = interp_safeboxes(i_axes, i_signal)
+  i_signal = interp_safebox(i_signal);
+  for index=1:numel(i_axes)
+    i_axes{index} = interp_safebox(i_axes{index});
+  end
+  
+function [x] = interp_safebox(in)
+  % surrounds a matrix with a 0's
+  x = in;
+  s = num2cell(2+size(x));  % this adds 1 every where.
+  x(s{:}) = 0;          % fill with NaN's
+  if numel(x) == length(x)  % a vector
+    x = circshift(x, 1);
+  else
+    x = circshift(x, ones(1, ndims(x)));
+  end
