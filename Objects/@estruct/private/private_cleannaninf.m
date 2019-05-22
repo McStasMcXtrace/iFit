abd5859968bf2e1a9e44b6baa5133839ccc10fe9
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-05-22 16:36:34 +0200

    estruct: add cleannaninf function which is used in many places

---
+++ Objects/@estruct/private/private_cleannaninf.m
@@ -0,0 +1,38 @@
+function s=private_cleannaninf(s, ratio)
+% private_cleannaninf: clean NaNs and Infs from a numerical field
+%
+
+if nargin < 2, ratio=10; end
+if isa(s,'estruct')
+
+  if numel(s) > 1
+    a = [];
+    for index=1:numel(s)
+      a = [ a ; feval(mfilename, s(index), ratio) ];
+    end
+    s = a;
+  else
+    s=set(s,'Signal',private_cleannaninf(get(s,'Signal'), ratio));
+    s=set(s,'Error', private_cleannaninf(get(s,'Error'), ratio));
+  end
+
+elseif isnumeric(s)
+
+  S = s(:);
+  if all(isfinite(S)), return; end
+  index_ok     = find(isfinite(S));
+
+  maxs = max(S(index_ok));
+  mins = min(S(index_ok));
+
+  S(isnan(S)) = 0;
+  if ~isempty(mins)
+    if mins<0, S(find(S == -Inf)) = mins*ratio;
+    else       S(find(S == -Inf)) = mins/ratio; end
+  end
+  if ~isempty(maxs)
+    if maxs>0, S(find(S == +Inf)) = maxs*ratio;
+    else       S(find(S == +Inf)) = maxs/ratio; end
+  end
+  s = double(reshape(S, size(s)));
+end
