04e79719ba3bf46d758148d1dfdd2770b264438b
Emmanuel FARHI <farhi.rivasseau@gmail.com>
2020-02-04 19:18:07 +0100

    Contrib from Jiri Novak <novak@physics.muni.cz> and Karolína Pěčková <437271@mail.muni.cz> report on bugs regarding the parameters uncertainties 2020-01-28 11:11 Dear Emmanuel, we are writing regarding a series of bugs in iFit concerning uncertainties output and calculations.
    The bugs concern:
    1) the output of the the parameters uncertainties
    2) the calculation of uncertainties from Hessian matrix
    3) the calculation of uncertainties from parameters and criterion
    trajectory (history)
    
    Please, find attached a detailed report (the pdf) on the reported bugs
    and their demonstration,
    example matlab scripts and functions and results of tests
    (example_uncertainty_output_bug.m and uncertainty_estimation.zip)
    and proposed fixes of the iFit functions (fixes.zip).
    
    Regarding the calculation of the parameters uncertainties from the
    parameters and criterion trajectory,
    we are not sure about a correct fix. We wanted to ask you whether the
    formula implemented in fmin_private_wrapper.m
    (which is actually different from that in the iFit manual) is based on
    some particular textbook or paper on statistical
    data analysis? If it is so, please, could you kindly provide a
    reference? Also, please, did you or somebody from the users
    do some (thorough) testing on uncertainty estimators?
    
    Thank you in advance for your answer.
    
    Answer: thanks for contribution. Jiri and Karolina did not manage to push a PR.
    I thus commit on their behalf.

--- Libraries/Optimizers/private/inline_estimate_uncertainty.m
+++ Libraries/Optimizers/private/inline_estimate_uncertainty.m
@@ -1,4 +1,4 @@
-function [dp,covp,corp,jac,hessian] = inline_estimate_uncertainty(fun, pars, options, varargin)
+function [dp,covp,corp,jac,hessian] = inline_estimate_uncertainty(fun, pars, pars_all, options, constraints, varargin) %KP modified
 % [dp,covp,corp] = inline_estimate_uncertainty(fun, pars, options)
 % 
 % Estimates the uncertainty around an optimization solution using
@@ -13,12 +13,16 @@ function [dp,covp,corp,jac,hessian] = inline_estimate_uncertainty(fun, pars, opt
 %  function, which may be of any type, and needn't be continuous. It does,
 %  however, need to return a single value.
 %
-%  PARS is a vector with initial guess parameters. You must input an
+%  PARS is a vector with initial guess of fitted parameters. You must input an
 %  initial guess.
+%
+%  PARS_ALL is a vector with inicial guess of all parameters.
 %
 %  OPTIONS is a structure with settings for the simulated annealing, 
 %  compliant with optimset. Default options may be obtained with
 %     o=fmin_private_wrapper(optimizer,'defaults')
+%
+%  CONSTRAINTS is a vector specifying fixed parameters.
 %
 % Output:
 %  DP is the gaussian uncertainty around PARS
@@ -27,52 +31,68 @@ function [dp,covp,corp,jac,hessian] = inline_estimate_uncertainty(fun, pars, opt
 %  JAC  is the Jacobian
 %  HESSIAN is the Hessian
 
+  %DOSTAT NA VSTUP OBOJE - PARS A PARS_ALL, TAKY CONSTRAINTS
 
-  n=length(pars);
+  %vector of indices of fitted parameters %KP modified
+  ind = find(constraints.fixed == 0);
+  
+  n = length(pars);
   if nargin < 3, options=[]; end
   if nargin < 4, args={}; end
-  if isfield(options,'TolX') TolX = options.TolX; 
-  else TolX = 0; end
+  % if isfield(options,'TolX') TolX = options.TolX; 
+  % else TolX = 0; end
 
   % initialize the curvature matrix alpha = '1/2 d2 Chi2/dpi/dpj' (half Hessian)
   alpha= zeros(n);
   dp   = zeros(size(pars));
-  chisq= sum(feval(fun, pars, varargin{:}));
+  val=feval(fun, pars_all, varargin{:}); %val=feval(fun, pars, varargin{:}); %KP modified
+  mm=length(val); %number of measurement points 
+  chisq= sum(val);
   
   covp = [];
   corp = [];
   jac  = [];
   hessian = [];
-  if TolX <= 0, 
-    TolX = 0.01*pars;
-  end
-  if length(TolX) == 1
-    dp   = TolX*ones(size(pars));
-  else
-    dp   = TolX;
-  end
-  dp(find(dp == 0)) = 1e-5;
+  %if TolX <= 0, 
+  %  TolX = 0.01*pars;
+  %end
+  %if length(TolX) == 1
+  %  dp   = TolX*ones(size(pars));
+  %else
+  %  dp   = TolX;
+  %end
+  %dp(find(dp == 0)) = 1e-5;
+  dp=ones(size(pars_all))*1e-6;
 
   % we now build the error matrix 'alpha' and the Jacobian
   jac = zeros(n,1);
-  for i=1:n
-    p    = pars; p(i) = p(i)+dp(i); chi1 = sum(feval(fun, p, varargin{:}));
-    p    = pars; p(i) = p(i)-dp(i); chi2 = sum(feval(fun, p, varargin{:}));
-    alpha(i,i) = (chi1-2*chisq+chi2)/2/dp(i)/dp(i); % diagonal terms
-    jac(i) = (chi1-chisq)/dp(i);
+  
+  for i=1:n %KP modified
+    ii = ind(i);
+    p = pars_all; p(ii) = p(ii)+dp(ii); chi1 = sum(feval(fun, p, varargin{:}));
+    p = pars_all; p(ii) = p(ii)-dp(ii); chi2 = sum(feval(fun, p, varargin{:}));
+    %p = pars; p(i) = p(i)+dp(i); chi1 = sum(feval(fun, p, varargin{:}));
+    %p = pars; p(i) = p(i)-dp(i); chi2 = sum(feval(fun, p, varargin{:}));
+    alpha(i,i) = (chi1-2*chisq+chi2)/2/dp(ii)/dp(ii); % diagonal terms
+    jac(i) = (chi1-chisq)/dp(ii);
     
     for j=i+1:n
-      p=pars; p(i)=p(i)+dp(i); p(j)=p(j)+dp(j); chi1=sum(feval(fun,p, varargin{:}));
-      p=pars; p(i)=p(i)+dp(i); p(j)=p(j)-dp(j); chi2=sum(feval(fun,p, varargin{:}));
-      p=pars; p(i)=p(i)-dp(i); p(j)=p(j)+dp(j); chi3=sum(feval(fun,p, varargin{:}));
-      p=pars; p(i)=p(i)-dp(i); p(j)=p(j)-dp(j); chi4=sum(feval(fun,p, varargin{:}));
+      jj = ind(j);
+      p = pars_all; p(ii) = p(ii)+dp(ii); p(jj) = p(jj)+dp(jj); chi1 = sum(feval(fun,p, varargin{:}));
+      p=pars_all; p(ii)=p(ii)+dp(ii); p(jj)=p(jj)-dp(jj); chi2=sum(feval(fun,p, varargin{:}));
+      p=pars_all; p(ii)=p(ii)-dp(ii); p(jj)=p(jj)+dp(jj); chi3=sum(feval(fun,p, varargin{:}));
+      p=pars_all; p(ii)=p(ii)-dp(ii); p(jj)=p(jj)-dp(jj); chi4=sum(feval(fun,p, varargin{:}));
+      %p=pars; p(i)=p(i)+dp(i); p(j)=p(j)+dp(j); chi1=sum(feval(fun,p, varargin{:}));
+      %p=pars; p(i)=p(i)+dp(i); p(j)=p(j)-dp(j); chi2=sum(feval(fun,p, varargin{:}));
+      %p=pars; p(i)=p(i)-dp(i); p(j)=p(j)+dp(j); chi3=sum(feval(fun,p, varargin{:}));
+      %p=pars; p(i)=p(i)-dp(i); p(j)=p(j)-dp(j); chi4=sum(feval(fun,p, varargin{:}));
       alpha(i,j)=(chi1-chi2-chi3+chi4)/8/dp(i)/dp(j);
       alpha(j,i)=alpha(i,j); % off diagonal terms (symmetric)
     end
   end
   if any(isnan(alpha(:))), return; end 
   hessian=2*alpha;
-  alpha = alpha/chisq;      % normalized error matrix
+  alpha = alpha/chisq*(mm-n);      % normalized error matrix
   covp  = pinv(alpha);       % COV MATRIX
   dp    = sqrt(abs(diag(covp))); % uncertainty on parameters
   corp  = covp./(dp*dp');   % correlation matrix
