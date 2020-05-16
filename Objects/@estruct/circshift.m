5105422e685db7f6fa739e97328cf8c82a359dc9
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2020-05-16 16:35:53 +0200

    estruct: add moveaxis method aliases

---
+++ Objects/@estruct/circshift.m
@@ -0,0 +1,10 @@
+function a=circshift(a, varargin)
+% CIRCSHIFT Shifts an object axis of specified rank by a value.
+%   A=CIRCSHIFT(A, RANK, SHIFT) shifts object axes RANK by SHIFT.
+%   This is equivalent to A{RANK} = A{RANK}+SHIFT;
+%
+% Example: a=estruct(peaks); circshift(a, 1, 5); max(getaxis(a,1))==54
+% Version: $Date$ $Version$ $Author$
+% See also  estruct/getaxis, estruct/setaxis
+
+a = moveaxis(a, varargin{:});
