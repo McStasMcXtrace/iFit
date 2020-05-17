16c5c03e582c6b9d792ba646f89c65ad9bdb40b7
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2020-05-17 17:55:16 +0200

    estruct: add morphing stuff.

---
+++ Objects/@estruct/colon.m
@@ -0,0 +1,35 @@
+function c = colon(a,d,b)
+% :  Colon (linear morphing).
+%   C = J:K = COLON(J,K) is the same as [J, J+1, ..., K], i.e. creates a vector
+%   of objects that goes from J to K, using LINSPACE.
+%
+% Example: b=estruct(peaks); a=-b; c=a:.5:b; numel(c) == 4
+% Version: $Date$ $Version$ $Author$
+% See also estruct, estruct/floor, estruct/ceil, estruct/round, estruct/combine
+
+if nargin == 1
+  b = []; d=0;
+elseif nargin == 2
+  b = d;
+  d = 0;
+end
+if isempty(b)
+  b=a;
+end
+if numel(a) > 1, a=a(1); end
+if numel(b) > 1, b=b(end); end
+if isempty(d), d=0; end
+
+if isa(a, 'estruct'), as = getaxis(a(1),'Signal'); as = as(:); else as = a; end
+if isa(b, 'estruct'), bs = getaxis(b(1),'Signal'); bs = bs(:); else bs = b; end
+
+as = floor(mean(as));
+bs = ceil( mean(bs));
+if d    , n = abs((bs-as)/d);
+else      n = abs( bs-as); end
+
+if n <= 1, c = a; return; end
+if n < 0,  c = a; return; end
+
+c = linspace(a,b,n);
+
---
+++ Objects/@estruct/logspace.m
@@ -0,0 +1,56 @@
+function v = logspace(a,b,n)
+% LOGSPACE Logarithmically spaced objects.
+%   LOGSPACE(X1, X2) generates a row vector of 10 logarithmically
+%   equally spaced objects between X1 and X2.
+%   This corresponds to a 'morphing' from X1 to X2 in N steps.
+%
+%   LOGSPACE(X1,X2,N) generates N objects between X1 and X2.
+%
+% Example: b=estruct(peaks); a=b+10; c=logspace(a,b); numel(c) == 10
+% Version: $Date$ $Version$ $Author$
+% See also estruct, estruct/max, estruct/min, estruct/colon, estruct/linspace
+
+if nargin <= 2
+  n = [];
+end
+if nargin == 1, b=a; end
+
+if isempty(n) || n <=0
+  n=10;
+end
+
+if isa(a, 'estruct') && numel(a) > 1, a=a(1); end
+if isa(b, 'estruct') && numel(b) > 1, b=b(end); end
+
+% get scalar value (if specified as number of 1x1 object)
+if     isempty(a), a=0; 
+elseif isempty(b), b=0; 
+elseif isscalar(a) & isa(a, 'estruct'), a=get(a,'Signal');
+elseif isscalar(b) & isa(b, 'estruct'), b=get(b,'Signal');
+end
+
+% one of (a,b) must be an estruct else fallback to the usual linspace
+if ~isa(a,'estruct') && ~isa(b,'estruct')
+  v=logspace(a,b,n);
+  return
+end
+
+% create constant objects from scalar input, using the other object as template
+if ~isa(a, 'estruct') & isnumeric(a) & ~isempty(b)
+  s=mean(a(:))*ones(size(get(b,'Signal'))); a=copyobj(b); set(a,'Signal', s);
+elseif ~isa(b, 'estruct') & isnumeric(b) & ~isempty(a)
+  s=mean(b(:))*ones(size(get(a,'Signal'))); b=copyobj(a); set(b,'Signal', s);
+else
+  [a,b] = intersect(a,b); % get intersection for operation (not needed when using copyobj)
+end
+
+xa = logspace(1,0,n);
+xa = (xa-1); xa=xa/max(xa);
+
+v = zeros(estruct, n, 1);
+for index=1:n
+  this = a.*xa(index) + b.*(1-xa(index));
+  this = setalias(this,'ratio',[ xa(index) 1-xa(index) ]);
+  v(index) = this;
+end
+
