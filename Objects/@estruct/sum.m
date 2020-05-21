91fc0539abf365631271d67c004b01dcaed5a107
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2020-05-21 15:04:43 +0200

    estruct: sum now uses same private function as trapz/prod/cum*

--- Objects/@estruct/sum.m
+++ Objects/@estruct/sum.m
@@ -1,8 +1,8 @@
-function b = sum(a, varargin)
+function [b,sigma] = sum(a, dim)
 %  SUM Sum of in object Signal.
 %    S = SUM(X) is the sum of the Signal of the object X. If
 %    X is a N-D object, SUM(X) operates along the first
-%    non-singleton dimension.
+%    dimension.
 %    If X is floating point, that is double or single, S is
 %    accumulated natively, that is in the same class as X,
 %    and S has the same class as X. If X is not floating point,
@@ -10,21 +10,20 @@ function b = sum(a, varargin)
 %
 %    S = SUM(X,DIM) sums along the dimension DIM. 
 %
-%    S = SUM(X,'double') and S = SUM(X,DIM,'double') accumulate
-%    S in double and S has class double, even if X is single.
-%
-%    S = SUM(X,'native') and S = SUM(X,DIM,'native') accumulate
-%    S natively and S has the same class as X.
-%
-%    S = SUM(X, 0) does the same as above, but returns the total sum per object.
+%    [S,sigma] = SUM(X, 0) does the same as above, but returns the total sum 
+%    per object. In this case, a second output argument holds the error bar.
 %
 % Example: s=estruct(-10:10); sum(s,0) == 0
 % Version: $Date$ $Version$ $Author$
 % See also estruct, estruct/uminus, estruct/abs, estruct/real, estruct/imag, estruct/uplus
 
-b = unary(a, mfilename, varargin{:});
+if nargin < 2, dim=1; end
 
-if nargin > 1 && isequal(varargin{1},0)
+[b,sigma] = private_sumtrapzproj(a,dim, 'sum');
+
+% b = unary(a, mfilename, varargin{:});
+
+if nargin > 1 && isequal(dim,0)
   if iscell(b)
     b = cell2mat(b);
   end
