8377d9b15829c8b8471ce795bee7be8419fd6e53
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2020-05-23 19:54:06 +0200

    estruct: findpeaks now worls on nD data

--- Objects/@estruct/findpeaks.m
+++ Objects/@estruct/findpeaks.m
@@ -1,11 +1,12 @@
-function [pks,locs,w,extra] = findpeaks(a, dim, varargin)
+function [pks,locs,extra] = findpeaks(a, dim, varargin)
 % FINDPEAKS Find local maxima.
 %   PKS = FINDPEAKS(A) returns a vector with the local maxima (peaks) of the  
-%   input object along first dimension.
+%   input object.
 %
-%   FINDPEAKS(A, DIM) does the same along dimension DIM.
+%   FINDPEAKS(A, DIM) does the same along dimension DIM with the object 
+%   projection. Specifying DIM=0 works on all dimensions (default).
 %
-%   FINDPEAKS(A, DIM, 'PARAM1',VALUE1,...) specifies additional parameters for the 
+%   FINDPEAKS(A,DIM,'PARAM1',VALUE1,...) specifies additional parameters for the 
 %   search:
 %     MinPeakHeight:   Minimum peak height (positive scalar, 2*std(A))
 %     MinPeakDistance: Minimum separation between (positive integer, 4 pts)
@@ -13,28 +14,34 @@ function [pks,locs,w,extra] = findpeaks(a, dim, varargin)
 %     DoubleSided:     True if data takes positive and negative values
 %                      (boolean, true).
 %
-%   [PKS,LOCS,W,EXTRA] = FINDPEAKS(A,...) returns the peak heights PKS, locations 
-%   LOCS, peak widths W and additional information in structure EXTRA.
+%   [PKS,LOCS,EXTRA] = FINDPEAKS(A,...) returns the peak heights PKS, locations 
+%   LOCS, and additional information in structure EXTRA.
 %     EXTRA.sign:     1 for maximum, -1 for minimum.
 %     EXTRA.height:   peak height (signal value at peak), same as PKS.
-%     EXTRA.width:    peak width, same as W.
 %     EXTRA.indices:  indices of peak locations in the signal.
 %     EXTRA.baseline: baseline below the signal (when noisy, sharp peaks).
+%   LOCS is a single vector for 1D objects or when DIM is specified, and a cell 
+%   array of axis values for nD objects.
 %
 % References: Slavic, NIM 112 (1973) 253 ; M. Morhac, NIM A 600 (2009) 478 
 %
 % Example: t = 2*pi*linspace(0,1,1024)'; ...
 %   y = sin(3.14*t) + 0.5*cos(6.09*t) + 0.1*sin(10.11*t+1/6) + ...
-%   0.1*sin(15.3*t+1/3); a=estruct(t,y); ...
-%   [pks x w extra] = findpeaks(a); ...
-%   plot(a); hold on; plot(x, pks, 'ro'); delete(gcf); numel(pks)==6
+%   0.1*sin(15.3*t+1/3); a=estruct(y); a{1}=t; ...
+%   [pks x extra] = findpeaks(a); ...
+%   plot(a,'hide-errorbars'); hold on; plot(x, pks, 'ro'); ...
+%   delete(gcf); numel(pks)==6
+% Example: a=estruct(peaks); ...
+%   [pks x extra] = findpeaks(a); ...
+%   plot(a); hold on; plot3(x{2},x{1},ones(size(x{1})),'ro'); delete(gcf); 1
 % Version: $Date$ $Version$ $Author$
 % See also estruct, estruct/median, estruct/mean, estruct/std
 
 % inline functions: BaseLine, PeakWidth
 % private: findpeaks_octave, from Octave.
 
-  if nargin < 2, dim=1; end
+  if nargin < 2,   dim=0; end
+  if isempty(dim), dim=0; end
   if numel(a) > 1
     pks = cell(size(a)); locs = pks; w = pks; extra = pks;
     for index=1:numel(a)
@@ -47,33 +54,49 @@ function [pks,locs,w,extra] = findpeaks(a, dim, varargin)
     return
   end
 
-  if dim == 0 || abs(dim) > prod(ndims(a))
+  if abs(dim) > prod(ndims(a))
     dim = 1;
   end
 
   % we first compute projection of estruct on the selected dimension
-  if ndims(a) > 1 && ~isvector(a)
-    b = camproj(a, abs(dim));
+  b = copyobj(a); set(b,'Error',0);
+  if dim > 0 && ndims(a) > 1 && ~isvector(a)
+    b = camproj(b, dim);
   else
-    b = copyobj(a);
+    if dim <= 0
+      b = subsref(b, substruct('()',':'));
+    end
   end
   
   % first we make sure the axis is regularly sampled
   b = unique(b); % unique and sorted axes
   signal    = private_cleannaninf(get(b,'Signal')); 
-  x         = getaxis(b,1);
+  % if dim <= 0, signal = signal(:); end
   
   warning('off','MATLAB:polyfit:RepeatedPointsOrRescale');
   [pks,locs,extra] = findpeaks_octave(signal, varargin{:});
   
   extra.indices   = locs;
-  extra.baseline  = BaseLine(signal, 3);
-  extra.width     = PeakWidth(signal-extra.baseline, 3)*min(diff(x));
+  extra.baseline  = BaseLine(signal, 5);
+  extra,width     = PeakWidth(signal, 5); 
   extra.sign      = findpeaks_slavic(signal);
   extra.sign      = extra.sign(locs);
-  extra.width     = extra.width(locs);
-  w               = extra.width;
-  locs            = x(locs); % now as X values, not indices.
+
+  % compute axes values 
+  if dim, c=b; else c=a; end
+  x=cell(1,ndims(c));
+  [x{:}] = ind2sub(size(c),locs); % [ I, J, K, ..] indices in A signal
+  for index=1:ndims(c)
+    ax = getaxis(c,index);
+    if numel(ax) == size(c,index)
+      x{index} = ax(x{index});
+    elseif numel(ax) == prod(size(a))
+      x{index} = ax(locs);
+    end
+  end
+  locs = x;
+  if numel(locs) == 1, locs = locs{1}; end
+
 end
 
 % ==============================================================================
